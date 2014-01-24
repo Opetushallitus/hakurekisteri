b0b4700841508ee1c959d899e29d15a5d22bc6fd
Verneri Åberg <verneri.aberg@siili.fi>
2014-01-24 13:46:00 +0200

    clarify build

--- project/build.scala
+++ project/build.scala
@@ -12,42 +12,53 @@ object HakuJaValintarekisteriBuild extends Build {
   val ScalaVersion = "2.10.3"
   val ScalatraVersion = "2.2.2"
 
-  lazy val project = Project (
-    "hakurekisteri",
-    file("."),
-    settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+  val ScalatraStack = Seq(
+    "org.scalatra" %% "scalatra",
+    "org.scalatra" %% "scalatra-scalate",
+    "org.scalatra" %% "scalatra-json",
+    "org.scalatra" %% "scalatra-swagger")
 
-      organization := Organization,
-      name := Name,
-      version := Version,
-      scalaVersion := ScalaVersion,
-      resolvers += Classpaths.typesafeReleases,
-      libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
-        "org.scalatra" %% "scalatra-json" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-swagger" % ScalatraVersion,
-        "com.wordnik" % "swagger-core_2.10.0" % "1.2.0",
-        "org.json4s"   %% "json4s-jackson" % "3.2.5",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
-      ),
-      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
-        Seq(
-          TemplateConfig(
-            base / "webapp" / "WEB-INF" / "templates",
-            Seq.empty,  /* default imports should be added here */
-            Seq(
-              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
-            ),  /* add extra bindings here */
-            Some("templates")
-          )
-        )
-      }
-    )
+  val dependencies = Seq(
+    "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
+    "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
+    "org.json4s" %% "json4s-jackson" % "3.2.5"
   )
+
+  val testDependencies = Seq("org.scalatra" %% "scalatra-scalatest" % ScalatraVersion)
+
+
+  lazy val project = {
+
+    Project(
+      "hakurekisteri",
+      file("."),
+      settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+
+        organization := Organization,
+        name := Name,
+        version := Version,
+        scalaVersion := ScalaVersion,
+        resolvers += Classpaths.typesafeReleases,
+        libraryDependencies ++= Seq("org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")))
+          ++ ScalatraStack.map((a) => a % ScalatraVersion)
+          ++ dependencies
+          ++ testDependencies.map((m) => m % "test"),
+        scalateTemplateConfig in Compile <<= (sourceDirectory in Compile) {
+          base =>
+            Seq(
+              TemplateConfig(
+                base / "webapp" / "WEB-INF" / "templates",
+                Seq.empty, /* default imports should be added here */
+                Seq(
+                  Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
+                ), /* add extra bindings here */
+                Some("templates")
+              )
+            )
+        }
+      )
+    )
+  }
 }
 
 
