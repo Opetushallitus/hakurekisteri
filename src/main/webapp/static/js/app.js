3ef408f530a4dbfb3568eb1b41f9e4e11acad1ed
tomikat <tomi.katajanheimo@gmail.com>
2014-02-04 17:32:14 +0200

    TJR-77 suoritusrekisterin gui

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -3,8 +3,8 @@
 // Declare app level module which depends on filters, and services
 var app = angular.module('myApp', ['ngRoute', 'ngResource', 'ui.bootstrap'])
     .config(function ($locationProvider, $routeProvider) {
-        $routeProvider.when('/suoritukset', {templateUrl: '/templates/suoritukset', controller: SuorituksetCtrl});
-        $routeProvider.when('/muokkaa/:henkilo', {templateUrl: '/templates/muokkaa', controller: MuokkaaCtrl});
+        $routeProvider.when('/suoritukset', {templateUrl: 'templates/suoritukset', controller: SuorituksetCtrl});
+        $routeProvider.when('/muokkaa/:henkilo', {templateUrl: 'templates/muokkaa', controller: MuokkaaCtrl});
         $routeProvider.otherwise({redirectTo: '/suoritukset'});
         $locationProvider.html5Mode(false);
     });
