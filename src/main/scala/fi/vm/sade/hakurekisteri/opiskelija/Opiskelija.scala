018ac1c8a0b89da3d9e6a73e51b7c2b0278c3a89
Verneri Åberg <verneri.aberg@siili.fi>
2014-02-06 16:43:19 +0200

    remove unnecessary method

--- src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/Opiskelija.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/Opiskelija.scala
@@ -8,10 +8,6 @@ case class Opiskelija(oppilaitosOid: String, luokkataso: String, luokka: String,
 
 object Opiskelija{
 
-  def apply(o:Opiskelija, identity:UUID): (Opiskelija, UUID) = {
-    (Opiskelija(o.oppilaitosOid, o.luokkataso, o.luokka, o.henkiloOid, o.alkuPaiva, o.loppuPaiva), identity)
-
-  }
 
   def identify(o:Opiskelija, identity:UUID) = {
     new Opiskelija(o.oppilaitosOid, o.luokkataso, o.luokka, o.henkiloOid, o.alkuPaiva, o.loppuPaiva) with Identified{
