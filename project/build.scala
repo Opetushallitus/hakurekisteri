1b741834f960dfe748e2878efee629b1f5bcad08
Verneri Åberg <verneri.aberg@siili.fi>
2014-02-13 11:42:54 +0200

    include jgroups-ehcache

--- project/build.scala
+++ project/build.scala
@@ -30,8 +30,10 @@ object HakuJaValintarekisteriBuild extends Build {
   val SecurityStack = SpringStack.map(_ % SpringVersion) ++
     Seq("net.sf.ehcache" % "ehcache-core" % "2.4.8",
     "fi.vm.sade.generic" % "generic-common" % "9.0-SNAPSHOT",
-    "org.jgroups"  % "jgroups" % "2.10.0.GA"
+    "org.jgroups"  % "jgroups" % "2.10.0.GA",
+    "net.sf.ehcache" % "ehcache-jgroupsreplication" % "1.5"
     )
+
   val dependencies = Seq(
     "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
     "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
