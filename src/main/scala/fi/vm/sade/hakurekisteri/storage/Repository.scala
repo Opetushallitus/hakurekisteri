a1ee7140ba67aa53815037dd775559fd565af65d
Verneri Åberg <verneri.aberg@siili.fi>
2014-02-14 20:04:17 +0200

    add in mem journaled repository

--- src/main/scala/fi/vm/sade/hakurekisteri/storage/Repository.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/storage/Repository.scala
@@ -10,15 +10,43 @@ trait Repository[T] {
 
 }
 
+trait JournaledRepository[T] extends InMemRepository[T] {
+
+  private[this] var _journal:Seq[T with Identified] = Seq()
+  protected def journal:Seq[T with Identified] = _journal
+
+  loadJournal()
+
+  def loadJournal() {
+    store = journal.map((f: T with Identified) => f.id -> f).toMap
+  }
+
+  def addModification(o: T with Identified) {
+    _journal = _journal :+ o
+  }
+
+  override def saveIdentified(o: T with Identified): T with Identified  = {
+    addModification(o)
+    super.saveIdentified(o)
+  }
+
+
+}
+
+
 trait InMemRepository[T] extends Repository[T] {
+
   var store:Map[UUID,T with Identified] = Map()
 
   def save(o: T ): T with Identified = {
     val oid = identify(o)
+    saveIdentified(oid)
+  }
+
+  protected def saveIdentified(oid: T with Identified) = {
     store = store + (oid.id -> oid)
     oid
   }
-
 
   def identify(o: T): T with Identified
 
