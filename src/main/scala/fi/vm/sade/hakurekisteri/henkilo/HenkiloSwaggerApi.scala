de4a22afb4233819bfcbe1ee7b4e6fdaf565addb
tomikat <tomi.katajanheimo@gmail.com>
2014-02-14 16:30:53 +0200

    henkilot resource

---
+++ src/main/scala/fi/vm/sade/hakurekisteri/henkilo/HenkiloSwaggerApi.scala
@@ -0,0 +1,29 @@
+package fi.vm.sade.hakurekisteri.henkilo
+
+import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriResource
+import scala.Some
+import java.util.Date
+
+trait HenkiloSwaggerApi
+    { this: HakurekisteriResource[Henkilo] =>
+
+  override protected val applicationName = Some("henkilot")
+  protected val applicationDescription = "henkilötietojen rajapinta."
+
+  read(apiOperation[Seq[Henkilo]]("henkilot")
+    summary "Näytä kaikki opiskelijatiedot"
+    notes "Näyttää kaikki opiskelijatiedot. Voit myös hakea eri parametreillä."
+    parameter queryParam[Option[String]]("henkilo").description("haetun henkilon oid")
+    parameter queryParam[Option[String]]("kausi").description("kausi jonka tietoja haetaan").allowableValues("S", "K")
+    parameter queryParam[Option[String]]("vuosi").description("vuosi jonka tietoja haetaan")
+    parameter queryParam[Option[Date]]("paiva").description("päivä jonka tietoja haetaan")
+    parameter queryParam[Option[String]]("oppilaitosOid").description("haetun oppilaitoksen oid")
+    parameter queryParam[Option[String]]("luokka").description("haetun luokan nimi")
+  )
+
+  create(apiOperation[Henkilo]("lisääOpiskelija")
+    .parameter(bodyParam[Henkilo]("lisääOpiskelija").description("Uusi opiskelija").required)
+    .summary("luo Opiskelijan ja palauttaa sen tiedot"))
+
+
+}
