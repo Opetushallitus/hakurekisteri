932c28b8c74d8591624eda1806743b48c0879203
Verneri Åberg <verneri.aberg@siili.fi>
2014-02-14 10:58:39 +0200

    remove null pointer exception when there is no authentication

--- src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/OpiskelijaQuery.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/OpiskelijaQuery.scala
@@ -4,6 +4,7 @@ import fi.vm.sade.hakurekisteri.rest.support.{Kausi, Query}
 import fi.vm.sade.hakurekisteri.rest.support.Kausi.Kausi
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
+import scala.util.Try
 
 case class OpiskelijaQuery(henkilo: Option[String],
                            kausi: Option[Kausi],
@@ -17,6 +18,9 @@ object OpiskelijaQuery{
 
 
   def apply(params: Map[String,String]): OpiskelijaQuery = {
-    OpiskelijaQuery(params.get("henkilo"), params.get("kausi").map(Kausi.withName), params.get("vuosi"), params.get("paiva").map(DateTime.parse(_,ISODateTimeFormat.basicDateTimeNoMillis())), params.get("oppilaitosOid"), params.get("luokka"))
+
+    def extractDate(s:String) = Try(DateTime.parse(s,ISODateTimeFormat.dateTimeNoMillis())).
+      recoverWith{case _ : Exception => Try(DateTime.parse(s, ISODateTimeFormat.basicDateTimeNoMillis()))}.get
+    OpiskelijaQuery(params.get("henkilo"), params.get("kausi").map(Kausi.withName), params.get("vuosi"), params.get("paiva").map(extractDate), params.get("oppilaitosOid"), params.get("luokka"))
   }
 }
