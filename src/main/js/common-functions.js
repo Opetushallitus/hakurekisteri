c52bdebf8bed804830285a69f2311d213b3ddd8c
tomikat <tomi.katajanheimo@gmail.com>
2014-02-17 16:35:23 +0200

    console missing fix

--- src/main/js/common-functions.js
+++ src/main/js/common-functions.js
@@ -63,7 +63,25 @@ function authenticateToAuthenticationService($http, successCallback, errorCallba
         .error(errorCallback);
 }
 
-// IE9 hack
-if (!console) {
-    console = {}; console.log = function() {};
-}
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
