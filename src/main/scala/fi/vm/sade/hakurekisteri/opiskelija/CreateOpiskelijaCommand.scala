2afbdeffd3829cb185bc84ff8879d215b6049b2a
Verneri Åberg <verneri.aberg@siili.fi>
2014-02-18 10:43:31 +0200

    add Datetime validation

--- src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/CreateOpiskelijaCommand.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/CreateOpiskelijaCommand.scala
@@ -6,6 +6,11 @@ import org.scalatra.{util, DefaultValue}
 import org.scalatra.util.conversion.TypeConverter
 import org.joda.time.format.{ISODateTimeFormat, DateTimeFormat}
 import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import scalaz.Failure
+import org.scalatra.validation.{Validators, ValidationError}
+import org.scalatra.validation.Validators.PredicateValidator
+import scala.util.Try
 
 
 class CreateOpiskelijaCommand extends HakurekisteriCommand[Opiskelija] {
@@ -16,9 +21,10 @@ class CreateOpiskelijaCommand extends HakurekisteriCommand[Opiskelija] {
   val luokkataso: Field[String]  = asString("luokkataso").notBlank()
   val luokka: Field[String]  = asType[String]("luokka").notBlank()
   val henkiloOid: Field[String]  = asType[String]("henkiloOid").notBlank()
-  val alkuPaiva: Field[DateTime] = asType[DateTime]("alkuPaiva").required
-  val loppuPaiva: Field[DateTime] = asType[DateTime]("loppuPaiva").optional
+  val alkuPaiva: Field[String] = asType[String]("alkuPaiva").notBlank().validate(jsonFormats.dateFormat.parse(_).isDefined)
+  val loppuPaiva: Field[String] = asType[String]("loppuPaiva").optional.validate(jsonFormats.dateFormat.parse(_).isDefined)
 
-  override def toResource: Opiskelija = Opiskelija(oppilaitosOid.value.get,luokkataso.value.get,luokka.value.get,
-    henkiloOid.value.get, alkuPaiva.value.get, loppuPaiva.original.asInstanceOf[Option[DateTime]])
+  override def toResource: Opiskelija = {
+    Opiskelija(oppilaitosOid.value.get,luokkataso.value.get,luokka.value.get,
+    henkiloOid.value.get, JString(alkuPaiva.value.get).extract[DateTime], loppuPaiva.value.flatMap((s) => Try(JString(s).extract[DateTime]).toOption))}
 }
