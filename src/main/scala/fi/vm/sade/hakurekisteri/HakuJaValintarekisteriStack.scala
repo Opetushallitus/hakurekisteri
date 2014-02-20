759aeef1faec9637a5890ea8e85d14e0624f76af
tomikat <tomi.katajanheimo@gmail.com>
2014-02-20 17:29:56 +0200

    add flashmap support

--- src/main/scala/fi/vm/sade/hakurekisteri/HakuJaValintarekisteriStack.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/HakuJaValintarekisteriStack.scala
@@ -3,10 +3,8 @@ package fi.vm.sade.hakurekisteri
 import org.scalatra._
 import org.slf4j.LoggerFactory
 
-trait HakuJaValintarekisteriStack extends ScalatraServlet {
+trait HakuJaValintarekisteriStack extends ScalatraServlet with FlashMapSupport {
 
   val logger = LoggerFactory.getLogger(getClass)
-
-
 
 }
