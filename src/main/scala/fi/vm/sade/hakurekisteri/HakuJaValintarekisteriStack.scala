f30afc76b38d1c31bb6563768d9e8f68b8cb3f1d
Verneri Åberg <verneri.aberg@siili.fi>
2014-02-21 20:34:02 +0200

    cors support

--- src/main/scala/fi/vm/sade/hakurekisteri/HakuJaValintarekisteriStack.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/HakuJaValintarekisteriStack.scala
@@ -3,8 +3,13 @@ package fi.vm.sade.hakurekisteri
 import org.scalatra._
 import org.slf4j.LoggerFactory
 
-trait HakuJaValintarekisteriStack extends ScalatraServlet with FlashMapSupport {
+trait HakuJaValintarekisteriStack extends ScalatraServlet with CorsSupport {
 
   val logger = LoggerFactory.getLogger(getClass)
+
+  options("/*"){
+    response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"))
+
+  }
 
 }
