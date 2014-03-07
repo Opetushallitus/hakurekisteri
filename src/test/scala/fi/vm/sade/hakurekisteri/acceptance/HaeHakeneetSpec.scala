eaf79105f4a49bb56851bb2a0c2c83fef0d10dbf
tomikat <tomi.katajanheimo@gmail.com>
2014-03-07 09:55:25 +0200

    TJR-6 hakeneet - initial acceptance test

--- src/test/scala/fi/vm/sade/hakurekisteri/acceptance/HaeHakeneetSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/acceptance/HaeHakeneetSpec.scala
@@ -11,32 +11,37 @@ class HaeHakeneetSpec extends ScalatraFeatureSpec with GivenWhenThen {
   info("että voin alkaa tekemään valmisteluja tulevaa varten")
 
 
-  feature("Muodosta hakeneet ja valitut siirtotiedosto")
+  feature("Muodosta hakeneet ja valitut siirtotiedosto") {
 
-    scenario("Organisaation X hakijat")
+    scenario("Organisaation X hakijat") {
       Given("N henkilöä täyttää hakemuksen; osa kohdistuu organisaatioon X tai sen lapsiin")
       When("rajaan muodostusta valitsemalla organisaation X")
       Then("saan siirtotiedoston, jossa on organisaatioon X tai sen lapsiin hakeneet")
+    }
 
-    scenario("Haussa Y hakeneet")
+    scenario("Haussa Y hakeneet") {
       Given("N henkilöä täyttää hakemuksen; viisi kohdistuu hakuun Y")
       When("rajaan muodostusta valitsemalla haun Y")
       Then("saan siirtotiedoston, jossa on kyseiset viisi hakijaa")
+    }
 
-    scenario("Hakukohdekoodi")
+    scenario("Hakukohdekoodi") {
       Given("N henkilöä täyttää hakemuksen; osa kohdistuu hakukohteisiin tyyppiä Z")
       When("rajaan muodostusta syöttämällä hakukohdekoodin Z")
       Then("saan siirtotiedoston, jossa on hakijat hakukohteisiin tyyppiä Z")
+    }
 
-    scenario("XML tiedosto")
+    scenario("XML tiedosto") {
       Given("N henkilöä täyttää hakemuksen")
       When("rajaan muodostusta valitsemalla tiedostotyypiksi 'XML'")
       Then("saan siirtotiedoston, joka on XML-muodossa")
+    }
 
-    scenario("Excel tiedosto")
+    scenario("Excel tiedosto") {
       Given("N henkilöä täyttää hakemuksen")
       When("rajaan muodostusta valitsemalla tiedostotyypiksi 'Excel'")
       Then("saan siirtotiedoston, joka on Excel-muodossa")
+    }
 
 
 
@@ -44,19 +49,23 @@ class HaeHakeneetSpec extends ScalatraFeatureSpec with GivenWhenThen {
 
     // Myöhemmin nämä
 
-    scenario("Kaikki hakeneet")
+    scenario("Kaikki hakeneet") {
       Given("Kaikkiaan viisi henkilöä täyttää hakemuksen")
       When("rajaan muodostusta valitsemalla 'Kaikki hakeneet'")
       Then("saan siirtotiedoston, jossa on kaikki viisi hakijaa")
+    }
 
-    scenario("Hyväksytyt hakijat")
+    scenario("Hyväksytyt hakijat") {
       Given("N henkilöä täyttää hakemuksen")
       When("rajaan muodostusta valitsemalla 'Hyväksytyt hakijat'")
       Then("saan siirtotiedoston, jossa on vain hyväksytyt hakijat")
+    }
 
-    scenario("Paikan vastaanottaneet hakijat")
+    scenario("Paikan vastaanottaneet hakijat") {
       Given("N henkilöä täyttää hakemuksen")
       When("rajaan muodostusta valitsemalla 'Paikan vastaanottaneet'")
       Then("saan siirtotiedoston, jossa on vain paikan vastaanottaneet hakijat")
+    }
 
+  }
 }
