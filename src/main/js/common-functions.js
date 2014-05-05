73c73fc89618256d217f22993cbc851ae1604c71
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-05-05 15:30:57 +0300

    js clean up

--- src/main/js/common-functions.js
+++ src/main/js/common-functions.js
@@ -4,7 +4,6 @@ var msgCategory = "suoritusrekisteri";
 
 var henkiloServiceUrl = "/authentication-service";
 var organisaatioServiceUrl = "/organisaatio-service";
-var koodistoServiceUrl = "/koodisto-service";
 var hakuAppServiceUrl = "/haku-app";
 
 Array.prototype.diff = function(a) {
@@ -27,34 +26,6 @@ function getOrganisaatio($http, organisaatioOid, successCallback, errorCallback)
     $http.get(organisaatioServiceUrl + '/rest/organisaatio/' + encodeURIComponent(organisaatioOid), {cache: true})
         .success(successCallback)
         .error(errorCallback);
-}
-
-function getPostitoimipaikka($http, postinumero, successCallback, errorCallback) {
-    $http.get(koodistoServiceUrl + '/rest/json/posti/koodi/posti_' + encodeURIComponent(postinumero), {cache: true})
-        .success(successCallback)
-        .error(errorCallback);
-}
-
-function getKoodistoAsOptionArray($http, koodisto, kielikoodi, options) {
-    $http.get(koodistoServiceUrl + '/rest/json/' + encodeURIComponent(koodisto) + '/koodi', {cache: true})
-        .success(function(koodisto) {
-            angular.forEach(koodisto, function(koodi) {
-                metas: for (var j = 0; j < koodi.metadata.length; j++) {
-                    var meta = koodi.metadata[j];
-                    if (meta.kieli.toLowerCase() === kielikoodi.toLowerCase()) {
-                        options.push({
-                            value: koodi.koodiArvo,
-                            text: meta.nimi
-                        });
-                        break metas;
-                    }
-                }
-            });
-            options.sort(function(a, b) {
-                if (a.text === b.text) return 0;
-                return a.text < b.text ? -1 : 1;
-            });
-        });
 }
 
 function authenticateToAuthenticationService($http, successCallback, errorCallback) {
