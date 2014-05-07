2dff52cb42b5c3ff07df180436d1dd578c7ebafd
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-05-07 12:53:56 +0300

    TJR-2 arvosanat

--- src/main/scala/fi/vm/sade/hakurekisteri/arvosana/CreateArvosanaCommand.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/arvosana/CreateArvosanaCommand.scala
@@ -3,36 +3,18 @@ package fi.vm.sade.hakurekisteri.arvosana
 import org.scalatra.commands._
 import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriCommand
 import java.util.UUID
-import org.scalatra.{util, DefaultValue}
-import org.joda.time.{DateTime, LocalDate}
-import org.scalatra.util.conversion.TypeConverter
-import org.joda.time.format.DateTimeFormat
-import org.json4s._
 
 
 class CreateArvosanaCommand extends HakurekisteriCommand[Arvosana] {
 
-  val defaultUUID: UUID = UUID.fromString("12345678-1234-1234-1234-123456789012")
-
-
-  implicit def UUIDDefaultValue: DefaultValue[UUID] = org.scalatra.DefaultValueMethods.default(defaultUUID)
-  implicit def ArvioDefault: DefaultValue[Arvio] = org.scalatra.DefaultValueMethods.default(Arvio.NA)
-
-  import util.RicherString._
-  implicit val stringToUUID: TypeConverter[String, UUID] = safeOption((in:String)=>in.blankOption.map(UUID.fromString(_)))
-  implicit val jsonToUUID: TypeConverter[JValue, UUID] = safeOption(_.extractOpt[UUID])
-
-
-
-
   val arvio: Field[String] = asType[String]("arvio.arvosana").notBlank
   val asteikko: Field[String] = asType[String]("arvio.asteikko").required.allowableValues(Arvio.asteikot:_*)
-  val suoritus: Field[UUID] = asType[UUID]("suoritus").required
+  val suoritus: Field[String] = asType[String]("suoritus").required.validForFormat("([a-f\\d]{8}(-[a-f\\d]{4}){3}-[a-f\\d]{12}?)".r,"%s is not a valid UUID")
   val aine: Field[String] = asType[String]("aine").notBlank
   val lisatieto: Field[String] = asType[String]("lisatieto").optional
   val valinnainen: Field[Boolean] = asType[Boolean]("valinnainen").optional
 
-  override def toResource: Arvosana = Arvosana(suoritus.value.get, Arvio(arvio.value.get, asteikko.value.get), aine.value.get, lisatieto.value, valinnainen.value.get)
+  override def toResource: Arvosana = Arvosana(suoritus.value.map(UUID.fromString(_)).get, Arvio(arvio.value.get, asteikko.value.get), aine.value.get, lisatieto.value, valinnainen.value.getOrElse(false))
 }
 
 
