0dced14d1b05fc04e70e33f9b2a2e51bcf465772
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-05-15 09:21:30 +0300

    delete suoritus

--- src/main/scala/fi/vm/sade/hakurekisteri/suoritus/SuoritusJournal.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/suoritus/SuoritusJournal.scala
@@ -3,17 +3,27 @@ package fi.vm.sade.hakurekisteri.suoritus
 import scala.slick.driver.JdbcDriver.simple._
 import fi.vm.sade.hakurekisteri.storage.repository.JDBCJournal
 import scala.slick.lifted.ColumnOrdered
+import scala.slick.lifted.ExtensionMethodConversions
 import fi.vm.sade.hakurekisteri.storage.Identified
 import org.joda.time.LocalDate
 import scala.slick.jdbc.meta.MTable
 import scala.slick.driver.JdbcDriver
 import java.util.UUID
+import scala.compat.Platform
 
 class SuoritusJournal(database: Database) extends JDBCJournal[Suoritus, SuoritusTable, ColumnOrdered[Long]] {
   override def toResource(row: SuoritusTable#TableElementType): Suoritus with Identified = Suoritus(row._2, row._3, row._4, LocalDate.parse(row._5), row._6, yksilollistaminen.withName(row._7), row._8).identify(UUID.fromString(row._1))
-  override def update(o: Suoritus with Identified): SuoritusTable#TableElementType = (o.id.toString, o.komo, o.myontaja, o.tila, o.valmistuminen.toString, o.henkiloOid, o.yksilollistaminen.toString, o.suoritusKieli, System.currentTimeMillis())
-  override def delete(id:UUID) = ???
+  override def update(o: Suoritus with Identified): SuoritusTable#TableElementType = (o.id.toString, o.komo, o.myontaja, o.tila, o.valmistuminen.toString, o.henkiloOid, o.yksilollistaminen.toString, o.suoritusKieli, Platform.currentTime, false)
+  override def delete(id:UUID) = currentState(id) match
+  { case (resourceId, komo, myontaja, tila, valmistuminen, henkiloOid, yksilollistaminen, suoritusKieli, _, _) =>
+      (resourceId, komo, myontaja, tila, valmistuminen, henkiloOid, yksilollistaminen, suoritusKieli, Platform.currentTime,true)}
 
+
+  def currentState(id: UUID): (String, String, String, String, String, String, String, String, Long, Boolean) = {
+    database withSession(
+      implicit session =>
+        opiskelijat.filter(_.resourceId === id.toString).sortBy(_.inserted.desc).take(1).list().head)
+  }
 
   val opiskelijat = TableQuery[SuoritusTable]
   database withSession(
@@ -28,7 +38,7 @@ class SuoritusJournal(database: Database) extends JDBCJournal[Suoritus, Suoritus
   override val journalSort = (o: SuoritusTable) => o.inserted.asc
 }
 
-class SuoritusTable(tag: Tag) extends Table[(String, String, String, String, String, String, String, String, Long)](tag, "suoritus") {
+class SuoritusTable(tag: Tag) extends Table[(String, String, String, String, String, String, String, String, Long, Boolean)](tag, "suoritus") {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def resourceId = column[String]("resource_id")
   def komo = column[String]("komo")
@@ -39,6 +49,7 @@ class SuoritusTable(tag: Tag) extends Table[(String, String, String, String, Str
   def yksilollistaminen = column[String]("yksilollistaminen")
   def suoritusKieli = column[String]("suoritus_kieli")
   def inserted = column[Long]("inserted")
+  def deleted = column[Boolean]("deleted")
   // Every table needs a * projection with the same type as the table's type parameter
-  def * = (resourceId, komo, myontaja, tila, valmistuminen, henkiloOid, yksilollistaminen, suoritusKieli, inserted)
+  def * = (resourceId, komo, myontaja, tila, valmistuminen, henkiloOid, yksilollistaminen, suoritusKieli, inserted, deleted)
 }
