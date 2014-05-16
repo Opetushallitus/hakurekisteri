94cb78a645fe38982c233ede9904a0ce8021286d
Verneri Åberg <verneri.aberg@gmail.com>
2014-05-16 16:15:19 +0300

    add logging to debug problems

--- src/main/scala/fi/vm/sade/hakurekisteri/audit/AuditLog.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/audit/AuditLog.scala
@@ -1,15 +1,16 @@
 package fi.vm.sade.hakurekisteri.audit
 
 import fi.vm.sade.log.model.{Tapahtuma, LogEvent}
-import java.io.ByteArrayOutputStream
-import java.beans.XMLEncoder
+import java.io.{StringReader, ByteArrayOutputStream}
+import java.beans.{XMLDecoder, XMLEncoder}
 import akka.camel.{Producer, CamelMessage}
 import akka.actor.Actor
 import fi.vm.sade.hakurekisteri.storage.{DeleteResource, Identified}
 import fi.vm.sade.hakurekisteri.organization.{AuthorizedRead, AuthorizedQuery}
 import fi.vm.sade.hakurekisteri.rest.support.Query
-import java.util.UUID
+import java.util.{Date, UUID}
 import akka.event.Logging
+import org.xml.sax.InputSource
 
 
 sealed trait AuditMessage[T] {
@@ -44,6 +45,17 @@ object ReadEvent extends AuditMessage[UUID] {
   override def tapahtuma(resource: String,original: UUID, user:String): Tapahtuma =  createREAD("hakurekisteri", user, resource, original.toString)
 }
 
+case class AuditEvent(host: String,system: String,targetType: String,target: String,timestamp: Date, etype: String, user: String, userActsForUser: String)
+
+object AuditEvent {
+  def apply(responseBody:String) = {
+    val t = new XMLDecoder(new InputSource( new StringReader( responseBody ) ) ).readObject().asInstanceOf[LogEvent].getTapahtuma
+    AuditEvent(t.getHost, t.getSystem, t.getTargetType, t.getTarget, t.getTimestamp, t.getType, t.getUser, t.getUserActsForUser)
+
+  }
+}
+
+
 case class AuditUri(uri:String)
 
 class AuditLog(resource:String)(implicit val audit:AuditUri) extends Actor with Producer  {
@@ -51,15 +63,23 @@ class AuditLog(resource:String)(implicit val audit:AuditUri) extends Actor with 
   val log = Logging(context.system, this)
 
   def endpointUri: String = audit.uri
+  log.debug(s"Audit log for $resource initialized using endpoint $endpointUri")
   implicit val system = resource
-  override protected def transformOutgoingMessage(original: Any): Any = original match {
+
+  def createAuditMsg(original: Any) = original match {
     case AuthorizedQuery(q,orgs, user) => QueryEvent(q,user)
     case AuthorizedRead(id, orgs, user) => ReadEvent(id,user)
 
   }
 
-  override protected def routeResponse(msg: Any): Unit = {
-    log.debug(msg.toString)
+  override protected def transformOutgoingMessage(original: Any): Any ={
+    val msg = createAuditMsg(original)
+    log.debug(s"sending audit event: $msg for $original")
+    msg
+  }
+
+  override protected def routeResponse(msg: Any): Unit =  msg match {
+    case xml:String => log.debug(AuditEvent(xml).toString)
 
   }
 }
