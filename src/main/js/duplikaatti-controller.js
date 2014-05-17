92322a818ba19a637befd4d3d03812c57e9d36d2
tomikat <tomi.katajanheimo@gmail.com>
2014-05-17 17:18:12 +0300

    fix duplicate merge tool

--- src/main/js/duplikaatti-controller.js
+++ src/main/js/duplikaatti-controller.js
@@ -1,30 +1,16 @@
 'use strict';
 
-function DuplikaattiCtrl($scope, $rootScope, $log, Arvosanat, suoritusId) {
-    $scope.arvosanat = [];
-
-    Arvosanat.query({ suoritus: suoritusId }, function(arvosanat) {
-        $scope.arvosanat = arvosanat.filter(function(a, index, arr) {
-            return arr.some(function(b, bIndex) {
-                return (index !== bIndex && b.aine === a.aine && b.lisatieto === a.lisatieto && b.valinnainen === a.valinnainen)
-            })
-        }).sort(function(a, b) {
-            if (a.aine === b.aine) {
-                if (a.lisatieto === b.lisatieto) {
-                    return (a.valinnainen === true && b.valinnainen === false ? 1 : (a.valinnainen === false && b.valinnainen === true ? -1 : 0))
-                } else {
-                    return (a.lisatieto < b.lisatieto ? -1 : 1)
-                }
+function DuplikaattiCtrl($scope, $rootScope, $log, arvosanat) {
+    $scope.arvosanat = arvosanat.sort(function(a, b) {
+        if (a.aine === b.aine) {
+            if (a.lisatieto === b.lisatieto) {
+                return (a.valinnainen === true && b.valinnainen === false ? 1 : (a.valinnainen === false && b.valinnainen === true ? -1 : 0))
             } else {
-                return (a.aine < b.aine ? -1 : 1)
+                return (a.lisatieto < b.lisatieto ? -1 : 1)
             }
-        })
-    }, function() {
-        $rootScope.modalInstance.close({
-            type: "danger",
-            messageKey: "suoritusrekisteri.muokkaa.duplikaatti.virheladattaessaarvosanoja",
-            message: "Virhe ladattaessa arvosanoja. Yritä uudelleen."
-        })
+        } else {
+            return (a.aine < b.aine ? -1 : 1)
+        }
     });
 
     $scope.remove = function(arvosana) {
