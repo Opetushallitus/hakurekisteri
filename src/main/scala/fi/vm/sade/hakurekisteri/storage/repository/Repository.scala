e2968c9f2fd80c6b4c8d14c1a26ca8d2c9c06aa1
Verneri Åberg <verneri.aberg@gmail.com>
2014-05-26 00:41:13 +0300

    index for tiedonsiirto queries

--- src/main/scala/fi/vm/sade/hakurekisteri/storage/repository/Repository.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/storage/repository/Repository.scala
@@ -54,8 +54,11 @@ trait InMemRepository[T <: Resource] extends Repository[T] {
     val old = store.get(oid.id)
     store = store + (oid.id -> oid)
     reverseStore = old.map(o => reverseStore - o).getOrElse(reverseStore) + (oid -> oid.id)
+    index(old, oid);
     oid
   }
+
+  def index(old: Option[T with Identified], oid: T with Identified) {}
 
   def identify(o: T): T with Identified
 
