84f095c333e1cabd296b71f0c65dbda61f1c3eb6
Verneri Åberg <verneri.aberg@gmail.com>
2014-05-28 19:51:30 +0300

    fix sanity actor

--- src/main/scala/fi/vm/sade/hakurekisteri/arvosana/PerusopetusSanityActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/arvosana/PerusopetusSanityActor.scala
@@ -99,7 +99,7 @@ class PerusopetusSanityActor(val suoritusRekisteri: ActorRef, val journal:Journa
 
   override def receive: Actor.Receive = {
     case Problems => sender ! problems
-    case s:Stream => for (first <- s.headOption) goThrough(first, s.tail)
+    case s:Stream[_] => for (first <- s.headOption) goThrough(first, s.tail)
     case s::rest  => goThrough(s, rest)
     case s: Suoritus with Identified =>
       log.debug(s"received suoritus for ${s.henkiloOid} creating todistus")
