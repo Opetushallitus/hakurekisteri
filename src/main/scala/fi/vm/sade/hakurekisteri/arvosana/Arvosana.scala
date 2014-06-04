66e8efa6515f3c711da0055711f85e62b84234d0
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-06-04 09:17:49 +0300

    validate Arvio

--- src/main/scala/fi/vm/sade/hakurekisteri/arvosana/Arvosana.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/arvosana/Arvosana.scala
@@ -3,6 +3,7 @@ package fi.vm.sade.hakurekisteri.arvosana
 import fi.vm.sade.hakurekisteri.storage.Identified
 import java.util.UUID
 import fi.vm.sade.hakurekisteri.rest.support.Resource
+import scala.util.Try
 
 case class Arvosana(suoritus: UUID, arvio: Arvio, aine: String, lisatieto: Option[String], valinnainen: Boolean) extends Resource {
   override def identify(id: UUID): this.type with Identified= Arvosana.identify(this,id).asInstanceOf[this.type with Identified]
@@ -40,4 +41,9 @@ object Arvio {
 
 case class UnknownScaleException(scale:String) extends IllegalArgumentException("unknown scale: %s" format scale)
 
-case class Arvio410(override val arvosana:String) extends Arvio(arvosana)
+case class Arvio410(override val arvosana:String) extends Arvio(arvosana) {
+  require(Try(arvosana.toInt).isSuccess, "arvosana must be a number")
+  require(arvosana.toInt >= 4, "the arvosana must be greater than or equal to 4")
+  require(arvosana.toInt <= 10, "the arvosana must be less than or equal to 10")
+
+}
