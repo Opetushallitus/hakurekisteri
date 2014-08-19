a70e1c77242b58e2d3a099255bc06b84033d8b22
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-08-19 16:33:26 +0300

    refactor virta client

--- src/main/scala/fi/vm/sade/hakurekisteri/virta/VirtaActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/virta/VirtaActor.scala
@@ -1,6 +1,7 @@
 package fi.vm.sade.hakurekisteri.virta
 
 import akka.actor.Actor
+import akka.dispatch.Futures
 import akka.event.Logging
 import akka.pattern.pipe
 import fi.vm.sade.hakurekisteri.suoritus.{yksilollistaminen, Suoritus}
@@ -13,50 +14,64 @@ class VirtaActor(virtaClient: VirtaClient) extends Actor {
   val log = Logging(context.system, this)
 
   def receive: Receive = {
-    case (oppijanumero: Option[String], hetu: Option[String]) => convert(virtaClient.getOpiskelijanTiedot(oppijanumero = oppijanumero, hetu = hetu), oppijanumero) pipeTo sender
+    case (oppijanumero: Option[String], hetu: Option[String]) =>
+      virtaResult2suoritukset(virtaClient.getOpiskelijanTiedot(oppijanumero = oppijanumero, hetu = hetu))(oppijanumero) pipeTo sender
   }
 
-  def opiskeluoikeus2suoritus(o: VirtaOpiskeluoikeus): Suoritus = {
-    val valmistuminen = o.loppuPvm match {
-      case None => new LocalDate(1900 + o.alkuPvm.getYear + 7, 12, 31)
-      case Some(l) => l
-    }
-    val tila = valmistuminen match {
-      case v: LocalDate if v.isBefore(new LocalDate()) => "VALMIS"
-      case _ => "KESKEN"
-    }
-    Suoritus(
-      komo = o.koulutuskoodit.head, // FIXME
-      myontaja = o.myontaja, // FIXME
-      valmistuminen = valmistuminen,
-      tila = tila,
-      henkiloOid = "", // FIXME
-      yksilollistaminen = yksilollistaminen.Ei,
-      suoritusKieli = o.kieli
-    )
+  def opiskeluoikeus2suoritus(oppijanumero: Option[String])(o: VirtaOpiskeluoikeus): Future[Suoritus] = {
+    val valm: LocalDate = valmistuminen(o)
+    resolveOppilaitosOid(o.myontaja).flatMap((oppilaitosOid) => {
+      resolveKomoOid(o.koulutuskoodit.head, o.opintoala1995, o.koulutusala2002).map((komoOid) => {
+        Suoritus(
+          komo = komoOid,
+          myontaja = oppilaitosOid,
+          valmistuminen = valm,
+          tila = tila(valm),
+          henkiloOid = oppijanumero.get,
+          yksilollistaminen = yksilollistaminen.Ei,
+          suoritusKieli = o.kieli
+        )
+      })
+    })
   }
 
-  def tutkinto2suoritus(t: VirtaTutkinto): Suoritus = {
-    val valmistuminen = t.suoritusPvm
-    val tila = valmistuminen match {
-      case v: LocalDate if v.isBefore(new LocalDate()) => "VALMIS"
-      case _ => "KESKEN"
-    }
-    Suoritus(
-      komo = t.koulutuskoodi.get, // FIXME
-      myontaja = t.myontaja, // FIXME
-      valmistuminen = valmistuminen,
-      tila = tila,
-      henkiloOid = "", // FIXME
-      yksilollistaminen = yksilollistaminen.Ei,
-      suoritusKieli = t.kieli
-    )
+  def valmistuminen(o: VirtaOpiskeluoikeus): LocalDate = o.loppuPvm match {
+    case None => new LocalDate(1900 + o.alkuPvm.getYear + 7, 12, 31)
+    case Some(l) => l
   }
 
-  def convert(f: Future[Option[VirtaResult]], oppijanumero: Option[String]): Future[Seq[Suoritus]] = {
-    f.map {
-      case None => Seq()
-      case Some(r) => r.opiskeluoikeudet.map(opiskeluoikeus2suoritus) ++ r.tutkinnot.map(tutkinto2suoritus)
-    }
+  def tutkinto2suoritus(oppijanumero: Option[String])(t: VirtaTutkinto): Future[Suoritus] = {
+    resolveOppilaitosOid(t.myontaja).flatMap((oppilaitosOid) => {
+      resolveKomoOid(t.koulutuskoodi.get, t.opintoala1995, t.koulutusala2002).map((komoOid) => {
+        Suoritus(
+          komo = komoOid,
+          myontaja = oppilaitosOid,
+          valmistuminen = t.suoritusPvm,
+          tila = tila(t.suoritusPvm),
+          henkiloOid = oppijanumero.get,
+          yksilollistaminen = yksilollistaminen.Ei,
+          suoritusKieli = t.kieli
+        )
+      })
+    })
+  }
+
+  def tila(valmistuminen: LocalDate): String = valmistuminen match {
+    case v: LocalDate if v.isBefore(new LocalDate()) => "VALMIS"
+    case _ => "KESKEN"
+  }
+
+  def virtaResult2suoritukset(f: Future[Option[VirtaResult]])(oppijanumero: Option[String]): Future[Seq[Suoritus]] = f.flatMap {
+    case None => Future.successful(Seq())
+    case Some(r) =>
+      Future.sequence(r.opiskeluoikeudet.map(opiskeluoikeus2suoritus(oppijanumero)) ++ r.tutkinnot.map(tutkinto2suoritus(oppijanumero)))
+  }
+
+  def resolveOppilaitosOid(oppilaitosnumero: String): Future[String] = {
+    Future.successful(oppilaitosnumero) // FIXME search from organisaatio-service
+  }
+
+  def resolveKomoOid(koulutuskoodi: String, opintoala1995: Option[String], koulutusala2002: Option[String]): Future[String] = {
+    Future.successful("") // FIXME search from tarjonta-service
   }
 }
--- src/test/scala/fi/vm/sade/hakurekisteri/virta/VirtaActorSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/virta/VirtaActorSpec.scala
@@ -4,13 +4,14 @@ import java.util.concurrent.TimeUnit
 
 import akka.actor.{ActorRef, ActorSystem, Props}
 import fi.vm.sade.hakurekisteri.suoritus.Suoritus
-import org.joda.time.LocalDate
+import org.joda.time.{LocalDate}
 import org.scalatest.FlatSpec
 import org.scalatest.concurrent.AsyncAssertions
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.time.{Millis, Span}
 import org.specs.mock.Mockito
 import org.specs.specification.Examples
+import akka.pattern.ask
 
 import scala.concurrent.Future
 
@@ -22,33 +23,37 @@ class VirtaActorSpec extends FlatSpec with ShouldMatchers with AsyncAssertions w
 
   it should "convert VirtaResult into sequence of Suoritus" in {
     val virtaClient = mock[VirtaClient]
-    virtaClient.getOpiskelijanTiedot(Some("1.2.3"), Some("111111-1975")) returns Future.successful(Some(VirtaResult(Seq(
-      VirtaOpiskeluoikeus(
-        alkuPvm = new LocalDate(),
-        loppuPvm = None,
-        myontaja = "01901",
-        koulutuskoodit = Seq("782603"),
-        opintoala1995 = Some("58"),
-        koulutusala2002 = None,
-        kieli = "FI"
+    virtaClient.getOpiskelijanTiedot(Some("1.2.3"), Some("111111-1975")) returns Future.successful(
+      Some(
+        VirtaResult(
+          opiskeluoikeudet = Seq(
+            VirtaOpiskeluoikeus(
+              alkuPvm = new LocalDate().minusYears(1),
+              loppuPvm = None,
+              myontaja = "01901",
+              koulutuskoodit = Seq("782603"),
+              opintoala1995 = Some("58"),
+              koulutusala2002 = None,
+              kieli = "FI"
+            )
+          ),
+          tutkinnot = Seq(
+            VirtaTutkinto(
+              suoritusPvm = new LocalDate().minusYears(2),
+              koulutuskoodi = Some("725111"),
+              opintoala1995 = Some("79"),
+              koulutusala2002 = None,
+              myontaja = "01901",
+              kieli = "FI"
+            )
+          )
+        )
       )
-    ), Seq(
-      VirtaTutkinto(
-        suoritusPvm = new LocalDate(),
-        koulutuskoodi = Some("725111"),
-        opintoala1995 = Some("79"),
-        koulutusala2002 = None,
-        myontaja = "01901",
-        kieli = "FI"
-      )
-    ))))
+    )
 
     val virtaActor: ActorRef = system.actorOf(Props(new VirtaActor(virtaClient)))
 
-    import akka.pattern.ask
-
-    val q: (Some[String], Some[String]) = (Some("1.2.3"), Some("111111-1975"))
-    val result: Future[Seq[Suoritus]] = (virtaActor ? q)(akka.util.Timeout(3, TimeUnit.SECONDS)).mapTo[Seq[Suoritus]]
+    val result: Future[Seq[Suoritus]] = (virtaActor ? (Some("1.2.3"), Some("111111-1975")))(akka.util.Timeout(3, TimeUnit.SECONDS)).mapTo[Seq[Suoritus]]
 
     waitFuture(result) {(r: Seq[Suoritus]) => {
       r.size should be(2)
