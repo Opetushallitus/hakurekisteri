a9567ab356203923d08c754a687aa4688175bb75
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-08-29 12:23:47 +0300

    add tests for virkailija rest client

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/cas/CasClient.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/cas/CasClient.scala
@@ -2,7 +2,7 @@ package fi.vm.sade.hakurekisteri.integration.cas
 
 import java.net.{URLEncoder, URL}
 
-import com.stackmob.newman.ApacheHttpClient
+import com.stackmob.newman.{HttpClient, ApacheHttpClient}
 import com.stackmob.newman.dsl._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -11,9 +11,7 @@ import scala.concurrent.duration._
 class CasClient(serviceAccessUrl: Option[String] = None,
                 serviceUrl: String,
                 user: Option[String] = None,
-                password: Option[String] = None)(implicit val ec: ExecutionContext) {
-
-  implicit val httpClient = new ApacheHttpClient(socketTimeout = 120.seconds.toMillis.toInt)()
+                password: Option[String] = None)(implicit val httpClient: HttpClient, implicit val ec: ExecutionContext) {
 
   def getProxyTicket: Future[String] = {
     if (serviceAccessUrl.isEmpty || user.isEmpty || password.isEmpty) throw new IllegalArgumentException("serviceAccessUrl, user or password is not defined")
