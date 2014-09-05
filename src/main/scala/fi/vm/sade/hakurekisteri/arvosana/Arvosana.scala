7b6a7498e321ec94a8a76f072ee0b2f17e4fc2d0
Verneri Åberg <verneri.aberg@gmail.com>
2014-09-05 17:55:21 +0300

    new fields for YO arvosana

--- src/main/scala/fi/vm/sade/hakurekisteri/arvosana/Arvosana.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/arvosana/Arvosana.scala
@@ -4,8 +4,9 @@ import fi.vm.sade.hakurekisteri.storage.Identified
 import java.util.UUID
 import fi.vm.sade.hakurekisteri.rest.support.Resource
 import scala.util.Try
+import org.joda.time.LocalDate
 
-case class Arvosana(suoritus: UUID, arvio: Arvio, aine: String, lisatieto: Option[String], valinnainen: Boolean) extends Resource[UUID] {
+case class Arvosana(suoritus: UUID, arvio: Arvio, aine: String, lisatieto: Option[String], valinnainen: Boolean, myonnetty: Option[LocalDate] = None) extends Resource[UUID] {
   override def identify(id: UUID): this.type with Identified[UUID]= Arvosana.identify(this,id).asInstanceOf[this.type with Identified[UUID]]
 }
 
@@ -29,12 +30,15 @@ sealed abstract class Arvio(val arvosana:String)
 object Arvio {
 
   val ASTEIKKO_4_10 = "4-10"
-  val asteikot = Seq(ASTEIKKO_4_10)
+  val ASTEIKKOYO = "YO"
+  val asteikot = Seq(ASTEIKKO_4_10, ASTEIKKOYO)
 
   object NA extends Arvio("NA")
 
-  def apply(arvosana:String, asteikko:String):Arvio = asteikko match {
+  def apply(arvosana:String, asteikko:String, pisteet:Option[Int] = None):Arvio = asteikko match {
     case ASTEIKKO_4_10 => Arvio410(arvosana)
+    case ASTEIKKOYO if pisteet.isDefined => ArvioYo(arvosana, pisteet.get)
+    case ASTEIKKOYO if pisteet.isEmpty => throw new IllegalArgumentException("pisteet missing for YO arvosana")
     case _ => throw UnknownScaleException(asteikko)
   }
 }
@@ -45,5 +49,12 @@ case class Arvio410(override val arvosana:String) extends Arvio(arvosana) {
   require(Try(arvosana.toInt).isSuccess, "arvosana must be a number")
   require(arvosana.toInt >= 4, "the arvosana must be greater than or equal to 4")
   require(arvosana.toInt <= 10, "the arvosana must be less than or equal to 10")
+
+}
+
+case class ArvioYo(override val arvosana:String, pisteet: Int) extends Arvio(arvosana) {
+
+  val allowable = Set[String]("L","E","M", "C", "B", "A", "I+", "I", "I-", "I=", "K", "P" )
+  require(allowable.contains(arvosana), s"$arvosana is not in (${allowable.mkString(", ")})")
 
 }
--- src/main/scala/fi/vm/sade/hakurekisteri/arvosana/CreateArvosanaCommand.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/arvosana/CreateArvosanaCommand.scala
@@ -1,20 +1,24 @@
 package fi.vm.sade.hakurekisteri.arvosana
 
 import org.scalatra.commands._
-import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriCommand
+import fi.vm.sade.hakurekisteri.rest.support.{LocalDateSupport, HakurekisteriCommand}
 import java.util.UUID
+import org.joda.time.LocalDate
 
 
-class CreateArvosanaCommand extends HakurekisteriCommand[Arvosana] {
+class CreateArvosanaCommand extends HakurekisteriCommand[Arvosana] with LocalDateSupport {
 
   val arvio: Field[String] = asType[String]("arvio.arvosana").notBlank
   val asteikko: Field[String] = asType[String]("arvio.asteikko").required.allowableValues(Arvio.asteikot:_*)
+  val pisteet: Field[Int] = asType[Int]("arvio.asteikko").optional
   val suoritus: Field[String] = asType[String]("suoritus").required.validForFormat("([a-f\\d]{8}(-[a-f\\d]{4}){3}-[a-f\\d]{12}?)".r,"%s is not a valid UUID")
   val aine: Field[String] = asType[String]("aine").notBlank
+  val myonnetty: Field[LocalDate] = asType[LocalDate]("myonnetty").optional
+
   val lisatieto: Field[String] = asType[String]("lisatieto").optional
   val valinnainen: Field[Boolean] = asType[Boolean]("valinnainen").optional
 
-  override def toResource: Arvosana = Arvosana(suoritus.value.map(UUID.fromString(_)).get, Arvio(arvio.value.get, asteikko.value.get), aine.value.get, lisatieto.value, valinnainen.value.getOrElse(false))
+  override def toResource: Arvosana = Arvosana(suoritus.value.map(UUID.fromString).get, Arvio(arvio.value.get, asteikko.value.get, pisteet.value), aine.value.get, lisatieto.value, valinnainen.value.getOrElse(false), myonnetty.value)
 }
 
 
