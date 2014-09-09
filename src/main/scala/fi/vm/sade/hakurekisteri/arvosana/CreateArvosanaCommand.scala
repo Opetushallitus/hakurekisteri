062af3f00467643de21393cae67fe3c73952a0b9
Verneri Åberg <verneri.aberg@gmail.com>
2014-09-09 16:43:03 +0300

    fix optional fields

--- src/main/scala/fi/vm/sade/hakurekisteri/arvosana/CreateArvosanaCommand.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/arvosana/CreateArvosanaCommand.scala
@@ -4,21 +4,66 @@ import org.scalatra.commands._
 import fi.vm.sade.hakurekisteri.rest.support.{LocalDateSupport, HakurekisteriCommand}
 import java.util.UUID
 import org.joda.time.LocalDate
+import org.scalatra.util.conversion.TypeConverter
+import org.json4s._
+import fi.vm.sade.hakurekisteri.suoritus.yksilollistaminen._
+import org.scalatra.{util, DefaultValue}
+import fi.vm.sade.hakurekisteri.suoritus.yksilollistaminen
 
 
 class CreateArvosanaCommand extends HakurekisteriCommand[Arvosana] with LocalDateSupport {
 
+  implicit def OptionIntDefaultValue: DefaultValue[Option[Int]] = org.scalatra.DefaultValueMethods.default(None)
+
+  import util.RicherString._
+
+
+  implicit val stringtoOptionInt: TypeConverter[String, Option[Int]] = safe(_.blankOption.map (_.toInt))
+
+  implicit val jsontoOptionInt: TypeConverter[JValue, Option[Int]] = safe((jvalue: JValue) => jvalue match {
+    case JInt(value) => Some(value.toInt)
+    case _ => None
+  })
+
+
+  implicit val stringtoOptionString: TypeConverter[String, Option[String]] = safe(_.blankOption)
+
+  implicit val jsontoOptionString: TypeConverter[JValue, Option[String]] = safe((jvalue: JValue) => jvalue match {
+    case JString(value) => Some(value)
+    case _ => None
+  })
+
+
   val arvio: Field[String] = asType[String]("arvio.arvosana").notBlank
   val asteikko: Field[String] = asType[String]("arvio.asteikko").required.allowableValues(Arvio.asteikot:_*)
-  val pisteet: Field[Int] = asType[Int]("arvio.pisteet").optional
+  val pisteet: Field[Option[Int]] = asType[Option[Int]]("arvio.pisteet").optional
   val suoritus: Field[String] = asType[String]("suoritus").required.validForFormat("([a-f\\d]{8}(-[a-f\\d]{4}){3}-[a-f\\d]{12}?)".r,"%s is not a valid UUID")
   val aine: Field[String] = asType[String]("aine").notBlank
   val myonnetty: Field[LocalDate] = asType[LocalDate]("myonnetty").optional
 
-  val lisatieto: Field[String] = asType[String]("lisatieto").optional
+  val lisatieto: Field[Option[String]] = asType[Option[String]]("lisatieto").optional
   val valinnainen: Field[Boolean] = asType[Boolean]("valinnainen").optional
 
-  override def toResource(user: String): Arvosana = Arvosana(suoritus.value.map(UUID.fromString).get, Arvio(arvio.value.get, asteikko.value.get, pisteet.value), aine.value.get, lisatieto.value, valinnainen.value.getOrElse(false), myonnetty.value, source = user)
+
+
+  override def toResource(user: String): Arvosana = Arvosana(suoritus.value.map(UUID.fromString).get, Arvio(arvio.value.get, asteikko.value.get, pisteet.value.get), aine.value.get, lisatieto.value.get, valinnainen.value.getOrElse(false), myonnetty.value, source = user)
+}
+
+
+import org.scalatra.json.JacksonJsonValueReaderProperty
+import fi.vm.sade.hakurekisteri.rest.support._
+import org.scalatra.util.ParamsValueReaderProperties._
+import org.json4s.jackson.JsonMethods
+
+object Parser extends JacksonJsonValueReaderProperty with JsonMethods with HakurekisteriJsonSupport {
+  val body = "{\"id\":\"a57c647f-0e09-4d04-9330-bed646f6cab8\",\"suoritus\":\"69b5cdc0-2549-45ec-b11e-63387c8a6963\",\"valinnainen\":false,\"myonnetty\":\"01.12.1989\",\"arvio\":{\"arvosana\":\"L\",\"asteikko\":\"YO\",\"pisteet\":99}}"
+
+  val parsed = parse(body)
+
+  val cmd = new CreateArvosanaCommand
+
+  def test = cmd.bindTo(parsed)
+
 }
 
 
