e61175e9b7a3264401fdaa82d78f4e843dd2b533
Verneri Åberg <verneri.aberg@gmail.com>
2014-09-11 09:47:37 +0300

    Parse Ajanjakso

--- src/main/scala/fi/vm/sade/hakurekisteri/opiskeluoikeus/CreateOpiskeluoikeusCommand.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/opiskeluoikeus/CreateOpiskeluoikeusCommand.scala
@@ -15,11 +15,11 @@ class CreateOpiskeluoikeusCommand extends HakurekisteriCommand[Opiskeluoikeus] {
   implicit val stringToLocalDate: TypeConverter[String, LocalDate] = safeOption((in: String) => in.blankOption map DateTimeFormat.forPattern("dd.MM.yyyy").parseLocalDate)
   implicit val jsonToLocalDate: TypeConverter[JValue, LocalDate] = safeOption(_.extractOpt[LocalDate])
 
-  val alkuPvm: Field[LocalDate] = asType[LocalDate]("alkuPvm").required
-  val loppuPvm: Field[LocalDate] = asType[LocalDate]("loppuPvm").optional
+  val alkuPvm: Field[String] = asType[String]("aika.alku").required
+  val loppuPvm: Field[Option[String]] = asType[Option[String]]("aika.loppu").optional
   val henkiloOid: Field[String] = asType[String]("henkiloOid").notBlank
   val komo: Field[String] = asType[String]("komo").notBlank
   val myontaja: Field[String] = asType[String]("myontaja").notBlank
 
-  override def toResource(user: String): Opiskeluoikeus = Opiskeluoikeus(alkuPvm.value.get, loppuPvm.value, henkiloOid.value.get, komo.value.get, myontaja.value.get, source = user)
+  override def toResource(user: String): Opiskeluoikeus = Opiskeluoikeus(DateTime.parse(alkuPvm.value.get), loppuPvm.value.get.map(DateTime.parse), henkiloOid.value.get, komo.value.get, myontaja.value.get, source = user)
 }
