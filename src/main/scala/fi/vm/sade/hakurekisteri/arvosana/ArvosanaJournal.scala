0437de5616994019f684e4859d8dd3a7113c5b2a
Verneri Åberg <verneri.aberg@gmail.com>
2014-10-01 12:37:06 +0300

    Arvosanat to new journal

--- src/main/scala/fi/vm/sade/hakurekisteri/arvosana/ArvosanaJournal.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/arvosana/ArvosanaJournal.scala
@@ -1,90 +1,23 @@
 package fi.vm.sade.hakurekisteri.arvosana
 
 import scala.slick.driver.JdbcDriver.simple._
-import fi.vm.sade.hakurekisteri.storage.repository.{Deleted, Updated, Delta, JDBCJournal}
-import scala.slick.lifted.ColumnOrdered
-import fi.vm.sade.hakurekisteri.storage.Identified
 import scala.slick.jdbc.meta.MTable
-import scala.slick.driver.JdbcDriver
 import java.util.UUID
-import scala.compat.Platform
-import org.slf4j.LoggerFactory
-import scala.slick.lifted
 import org.joda.time.LocalDate
+import fi.vm.sade.hakurekisteri.rest.support.{JournalTable, NewJDBCJournal}
 
-class ArvosanaJournal(database: Database) extends JDBCJournal[Arvosana, ArvosanaTable, ColumnOrdered[Long], UUID] {
+class ArvosanaJournal(override val db: Database) extends NewJDBCJournal[Arvosana, UUID, ArvosanaTable](TableQuery[ArvosanaTable]) {
 
-  val logger = LoggerFactory.getLogger(getClass)
-
-  override def delta(row: ArvosanaTable#TableElementType): Delta[Arvosana, UUID] =
-    row match {
-      case (resourceId, _, _, _, _, _, _, _,_, source,  _, true) => Deleted(UUID.fromString(resourceId), source)
-      case (id,suoritus, arvosana, asteikko, aine, lisatieto, valinnainen, pisteet, myonnetty, source,  _, _) =>
-        Updated(Arvosana(UUID.fromString(suoritus), Arvio(arvosana, asteikko, pisteet), aine, lisatieto, valinnainen, myonnetty = myonnetty.map(LocalDate.parse), source).identify(UUID.fromString(id)))
-    }
-
-  override def delete(id:UUID, source: String) =  currentState(id) match
-  { case (foundid,suoritus, arvosana, asteikko, aine, lisatieto, valinnainen, pisteet, myonnetty, _ ,  _, _)  =>
-      (foundid,suoritus, arvosana, asteikko, aine, lisatieto, valinnainen, pisteet,myonnetty, source, Platform.currentTime,true)}
-
-  override def update(o:Arvosana with Identified[UUID]) = o.arvio match {
-    case Arvio410(arvosana) =>
-      logger.debug("toRow lisatieto {}", o.lisatieto)
-      (o.id.toString, o.suoritus.toString, arvosana, Arvio.ASTEIKKO_4_10 , o.aine, o.lisatieto, o.valinnainen, None, o.myonnetty.map(_.toString), o.source, Platform.currentTime, false)
-    case ArvioYo(arvosana, pisteet) =>
-      (o.id.toString, o.suoritus.toString, arvosana, Arvio.ASTEIKKOYO , o.aine, o.lisatieto, o.valinnainen, pisteet, o.myonnetty.map(_.toString), o.source, Platform.currentTime, false)
-    case a:Arvio if a == Arvio.NA => throw UnknownAssessmentResultException
-  }
-
-
-  object UnknownAssessmentResultException extends IllegalArgumentException("Trying to save unknown assessment result")
-
-  val arvosanat = TableQuery[ArvosanaTable]
-  database withSession(
+  db withSession(
     implicit session =>
       if (MTable.getTables("arvosana").list().isEmpty) {
-        arvosanat.ddl.create
+        table.ddl.create
       }
     )
+}
 
+class ArvosanaTable(tag: Tag) extends JournalTable[Arvosana, UUID, (String, String, String, String, Option[String], Boolean, Option[Int], Option[String], String)](tag, "arvosana") {
 
-
-  override def newest: (ArvosanaTable) => ColumnOrdered[Long] = _.inserted.desc
-
-  override def filterByResourceId(id: UUID): (ArvosanaTable) => Column[Boolean] = _.resourceId === id.toString
-
-
-  override val table = arvosanat
-  override val db: JdbcDriver.simple.Database = database
-  override val sortColumn = (a: ArvosanaTable) => a.inserted
-
-  override def timestamp(resource: ArvosanaTable): Column[Long] =  resource.inserted
-
-  override def timestamp(resource: ArvosanaTable#TableElementType): Long = resource._11
-
-  override val idColumn: (ArvosanaTable) => Column[String] = _.resourceId
-
-
-  override def latestResources  = {
-    val latest = for {
-      (id, resource) <- table.groupBy(idColumn)
-    } yield (id, resource.map(sortColumn).max)
-
-    val result = for {
-      delta <- table
-      (id, timestamp) <- latest
-      if idColumn(delta) === id && sortColumn(delta) === timestamp.getOrElse(0)
-
-    } yield delta
-
-    result.sortBy(sortColumn(_).asc)
-  }
-
-  }
-
-class ArvosanaTable(tag: Tag) extends Table[(String, String, String, String, String, Option[String], Boolean, Option[Int], Option[String], String, Long, Boolean)](tag, "arvosana") {
-  def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-  def resourceId = column[String]("resource_id")
   def suoritus = column[String]("suoritus")
   def arvosana = column[String]("arvosana")
   def asteikko = column[String]("asteikko")
@@ -93,9 +26,19 @@ class ArvosanaTable(tag: Tag) extends Table[(String, String, String, String, Str
   def valinnainen = column[Boolean]("valinnainen")
   def pisteet = column[Option[Int]]("pisteet")
   def myonnetty = column[Option[String]]("myonnetty")
-  def inserted = column[Long]("inserted")
-  def source = column[String]("source")
-  def deleted = column[Boolean]("deleted")
-  // Every table needs a * projection with the same type as the table's type parameter
-  def * = (resourceId, suoritus, arvosana, asteikko, aine, lisatieto, valinnainen, pisteet, myonnetty, source,  inserted, deleted)
+
+  override def resourceShape = (suoritus, arvosana, asteikko, aine, lisatieto, valinnainen, pisteet, myonnetty, source).shaped
+
+  override def row(a: Arvosana): Option[(String, String, String, String, Option[String], Boolean, Option[Int], Option[String], String)] = a.arvio match {
+    case Arvio410(arvosana) => Some(a.suoritus.toString, arvosana, Arvio.ASTEIKKO_4_10, a.aine, a.lisatieto, a.valinnainen, None, a.myonnetty.map(_.toString), a.source)
+    case ArvioYo(arvosana, pisteet) =>  Some(a.suoritus.toString, arvosana, Arvio.ASTEIKKOYO, a.aine, a.lisatieto, a.valinnainen, pisteet , a.myonnetty.map(_.toString), a.source)
+  }
+
+  override val deletedValues: (String, String, String, String, Option[String], Boolean, Option[Int], Option[String], String) = ("", "","", "", None, false, None, None, "")
+  override val resource: ((String, String, String, String, Option[String], Boolean, Option[Int], Option[String], String)) => Arvosana = (arvosanaResource _).tupled
+
+  def arvosanaResource(suoritus: String, arvosana: String, asteikko: String, aine: String, lisatieto: Option[String], valinnainen: Boolean, pisteet: Option[Int], myonnetty: Option[String], source: String) =
+    Arvosana(UUID.fromString(suoritus), Arvio(arvosana, asteikko, pisteet), aine, lisatieto, valinnainen, myonnetty = myonnetty.map(LocalDate.parse), source)
+
+  override def getId(serialized: String): UUID = UUID.fromString(serialized)
 }
