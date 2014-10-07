a764cf47962aa8e8404961815abae4ad311309c3
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-10-07 09:43:03 +0300

    valinta tulos retry

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/valintatulos/ValintaTulosActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/valintatulos/ValintaTulosActor.scala
@@ -1,6 +1,8 @@
 package fi.vm.sade.hakurekisteri.integration.valintatulos
 
+import java.io.InterruptedIOException
 import java.net.URLEncoder
+import java.util.concurrent.atomic.AtomicInteger
 
 import akka.actor.Actor
 import akka.event.Logging
@@ -28,18 +30,30 @@ class ValintaTulosActor(restClient: VirkailijaRestClient)
                        (implicit val ec: ExecutionContext) extends Actor {
 
   val log = Logging(context.system, this)
+  val maxRetries = 5
 
   override def receive: Receive = {
     case q: ValintaTulosQuery => getTulos(q) pipeTo sender
   }
 
   def getTulos(q: ValintaTulosQuery): Future[ValintaTulos] = {
+    val retryCount = new AtomicInteger(1)
+    tryTulos(q, retryCount)
+  }
+  
+  def tryTulos(q: ValintaTulosQuery, retryCount: AtomicInteger): Future[ValintaTulos] = {
     try {
       restClient.readObject[ValintaTulos](s"/haku/${URLEncoder.encode(q.hakuOid, "UTF-8")}/hakemus/${URLEncoder.encode(q.hakemusOid, "UTF-8")}", HttpResponseCode.Ok)
     } catch {
       case t: PreconditionFailedException =>
         log.warning(s"valinta tulos not found with haku ${q.hakuOid} and hakemus ${q.hakemusOid}: $t")
         Future.successful(ValintaTulos(q.hakemusOid, Seq()))
+      case t: InterruptedIOException =>
+        if (retryCount.getAndAdd(1) <= maxRetries) {
+          tryTulos(q, retryCount)
+        } else {
+          throw t
+        }
     }
   }
 
