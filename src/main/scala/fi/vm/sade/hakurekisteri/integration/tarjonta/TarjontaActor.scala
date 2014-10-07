d8fbbc96a696d63cbac545fe52a2be63976295fc
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-10-07 10:45:53 +0300

    cache valintatulos

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/tarjonta/TarjontaActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/tarjonta/TarjontaActor.scala
@@ -1,8 +1,6 @@
 package fi.vm.sade.hakurekisteri.integration.tarjonta
 
-import java.io.InterruptedIOException
 import java.net.URLEncoder
-import java.util.concurrent.atomic.AtomicInteger
 
 import akka.actor.Actor
 import com.stackmob.newman.response.HttpResponseCode
--- src/main/scala/fi/vm/sade/hakurekisteri/integration/valintatulos/ValintaTulosActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/valintatulos/ValintaTulosActor.scala
@@ -1,8 +1,6 @@
 package fi.vm.sade.hakurekisteri.integration.valintatulos
 
-import java.io.InterruptedIOException
 import java.net.URLEncoder
-import java.util.concurrent.atomic.AtomicInteger
 
 import akka.actor.Actor
 import akka.event.Logging
@@ -10,10 +8,13 @@ import akka.pattern.pipe
 import com.stackmob.newman.response.HttpResponseCode
 import fi.vm.sade.hakurekisteri.integration.{PreconditionFailedException, VirkailijaRestClient}
 
+import scala.compat.Platform
 import scala.concurrent.{Future, ExecutionContext}
+import scala.concurrent.duration._
 
 case class ValintaTulosQuery(hakuOid: String,
-                             hakemusOid: String)
+                             hakemusOid: String,
+                             cachedOk: Boolean = true)
 
 case class ValintaTulosHakutoive(hakukohdeOid: String,
                                  tarjoajaOid: String,
@@ -26,19 +27,28 @@ case class ValintaTulosHakutoive(hakukohdeOid: String,
 case class ValintaTulos(hakemusOid: String,
                         hakutoiveet: Seq[ValintaTulosHakutoive])
 
+case class CacheKey(hakuOid: String, hakemusOid: String)
+case class CachedValintaTulos(inserted: Long, tulos: Future[ValintaTulos])
+
 class ValintaTulosActor(restClient: VirkailijaRestClient)
                        (implicit val ec: ExecutionContext) extends Actor {
 
   val log = Logging(context.system, this)
   val maxRetries = 5
+  var cache: Map[CacheKey, CachedValintaTulos] = Map()
+  val expirationDurationMillis = 60.minutes.toMillis
 
   override def receive: Receive = {
     case q: ValintaTulosQuery => getTulos(q) pipeTo sender
   }
 
   def getTulos(q: ValintaTulosQuery): Future[ValintaTulos] = {
-    try {
-      restClient.readObject[ValintaTulos](s"/haku/${URLEncoder.encode(q.hakuOid, "UTF-8")}/hakemus/${URLEncoder.encode(q.hakemusOid, "UTF-8")}", maxRetries, HttpResponseCode.Ok)
+    val key = CacheKey(q.hakuOid, q.hakemusOid)
+    if (q.cachedOk && cache.contains(key) && cache(key).inserted + expirationDurationMillis > Platform.currentTime) cache(key).tulos
+    else try {
+      val f = restClient.readObject[ValintaTulos](s"/haku/${URLEncoder.encode(q.hakuOid, "UTF-8")}/hakemus/${URLEncoder.encode(q.hakemusOid, "UTF-8")}", maxRetries, HttpResponseCode.Ok)
+      cache = cache + (key -> CachedValintaTulos(Platform.currentTime, f))
+      f
     } catch {
       case t: PreconditionFailedException =>
         log.warning(s"valinta tulos not found with haku ${q.hakuOid} and hakemus ${q.hakemusOid}: $t")
