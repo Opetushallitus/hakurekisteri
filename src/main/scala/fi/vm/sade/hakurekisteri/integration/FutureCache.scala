d6b37f414b9d5b9c9e4b99b9de315ea08b8909c3
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-10-20 13:01:47 +0300

    refactor sijoittelu actor to use future cache

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/FutureCache.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/FutureCache.scala
@@ -4,7 +4,7 @@ import scala.compat.Platform
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
-case class Cacheable[T](inserted: Long = Platform.currentTime, f: Future[T])
+case class Cacheable[T](inserted: Long = Platform.currentTime, used: Long = Platform.currentTime, f: Future[T])
 
 class FutureCache[K, T](val expirationDurationMillis: Long = 60.minutes.toMillis) {
 
@@ -12,8 +12,16 @@ class FutureCache[K, T](val expirationDurationMillis: Long = 60.minutes.toMillis
 
   def contains(key: K): Boolean = cache.contains(key) && cache(key).inserted + expirationDurationMillis > Platform.currentTime
 
-  def get(key: K): Future[T] = cache(key).f
+  def get(key: K): Future[T] = {
+    val cached = cache(key)
+    cache = cache + (key -> Cacheable(inserted = cached.inserted, f = cached.f))
+    cached.f
+  }
+
+  def inUse(key: K): Boolean = contains(key) && (cache(key).used + expirationDurationMillis) > Platform.currentTime
 
   def +(key: K, f: Future[T]) = cache = cache + (key -> Cacheable(f = f))
+
+  def -(key: K) = if (cache.contains(key)) cache = cache - key
 
 }
--- src/main/scala/fi/vm/sade/hakurekisteri/integration/sijoittelu/SijoitteluActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/sijoittelu/SijoitteluActor.scala
@@ -1,59 +1,51 @@
 package fi.vm.sade.hakurekisteri.integration.sijoittelu
 
 import akka.actor.Actor
+import akka.pattern.pipe
 import com.stackmob.newman.response.HttpResponseCode
-import fi.vm.sade.hakurekisteri.integration.VirkailijaRestClient
+import fi.vm.sade.hakurekisteri.integration.{FutureCache, VirkailijaRestClient}
+import scala.concurrent.duration._
 import scala.concurrent.Future
-import scala.compat.Platform
 import akka.event.Logging
 
 class SijoitteluActor(sijoitteluClient: VirkailijaRestClient) extends Actor {
-  import akka.pattern._
-
-  import scala.concurrent.duration._
 
   implicit val ec = context.dispatcher
 
   val expiration = 4.hour
-  val touchInterval = expiration / 2
 
   val retry: FiniteDuration = 60.seconds
   val log = Logging(context.system, this)
-  var cache = Map[String, Future[SijoitteluTulos]]()
-  var cacheHistory = Map[String, Long]()
+  var cache = new FutureCache[String, SijoitteluTulos](expiration.toMillis)
   private val refetch: FiniteDuration = 2.hours
 
   override def receive: Receive = {
     case SijoitteluQuery(haku) =>
       getSijoittelu(haku) pipeTo sender
-    case Update(haku) if !inUse(haku) =>
-      cache = cache - haku
+
+    case Update(haku) if !cache.inUse(haku) =>
+      cache - haku
+
     case Update(haku) =>
       val result = sijoitteluTulos(haku)
       result.onFailure{ case t =>
         log.error(t, s"failed to fetch sijoittelu for haku $haku")
         rescheduleHaku(haku, retry)}
       result map (Sijoittelu(haku, _)) pipeTo self
+
     case Sijoittelu(haku, st) =>
-      cache = cache + (haku -> Future.successful(st))
+      cache + (haku, Future.successful(st))
       rescheduleHaku(haku)
   }
 
-  def inUse(haku: String):Boolean = cacheHistory.getOrElse(haku,0L) > (Platform.currentTime - expiration.toMillis)
-
-  def getSijoittelu(haku:String):  Future[SijoitteluTulos] = {
-    cacheHistory = cacheHistory + (haku -> Platform.currentTime)
-    cache.get(haku) match {
-      case Some(s)  =>
-        s
-      case None =>
-        updateCacheFor(haku)
-    }
+  def getSijoittelu(haku: String): Future[SijoitteluTulos] = {
+    if (cache.contains(haku)) cache.get(haku)
+    else updateCacheFor(haku)
   }
 
   def updateCacheFor(haku: String): Future[SijoitteluTulos] = {
     val result: Future[SijoitteluTulos] = sijoitteluTulos(haku)
-    cache = cache + (haku -> result)
+    cache + (haku, result)
 
     result.onFailure{ case t =>
       log.error(t, s"failed to fetch sijoittelu for haku $haku")
@@ -61,6 +53,7 @@ class SijoitteluActor(sijoitteluClient: VirkailijaRestClient) extends Actor {
     result.onSuccess{
       case _ => rescheduleHaku(haku)
     }
+
     result
   }
 
@@ -75,9 +68,6 @@ class SijoitteluActor(sijoitteluClient: VirkailijaRestClient) extends Actor {
     context.system.scheduler.scheduleOnce(time, self, Update(haku))
   }
 
-  case class Update(haku:String)
+  case class Update(haku: String)
   case class Sijoittelu(haku: String, st: SijoitteluTulos)
 }
-
-
-object Refresh
