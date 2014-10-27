5b20eae975165950655d02faac4a41c1fa51a556
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-10-27 11:32:12 +0200

    kkhakijat excel

--- src/main/scala/fi/vm/sade/hakurekisteri/kkhakija/KkExcelUtil.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/kkhakija/KkExcelUtil.scala
@@ -11,29 +11,27 @@ object KkExcelUtil extends HakijatExcelWriter[Seq[Hakija]] {
 
   override def getHeaders: Set[Row] = Set(Row(0, headers.zipWithIndex.map(h => StringCell(h._2, h._1))))
 
-  override def getRows(hakijat: Seq[Hakija]): Set[Row] = {
-    hakijat.flatMap((h) => h.hakemukset.map(ht => Set[Cell](
-      StringCell(0, h.hetu),
-      StringCell(1, h.oppijanumero),
-      StringCell(2, h.sukunimi),
-      StringCell(3, h.etunimet),
-      StringCell(4, h.kutsumanimi),
-      StringCell(5, h.lahiosoite),
-      StringCell(6, h.postinumero),
-      StringCell(7, h.postitoimipaikka),
-      StringCell(8, h.maa),
-      StringCell(9, h.kansalaisuus),
-      StringCell(10, h.matkapuhelin.getOrElse("")),
-      StringCell(12, h.puhelin.getOrElse("")),
-      StringCell(13, h.sahkoposti.getOrElse("")),
-      StringCell(14, h.kotikunta),
-      StringCell(15, h.sukupuoli),
-      StringCell(16, h.aidinkieli),
-      StringCell(17, h.asiointikieli),
-      StringCell(18, h.koulusivistyskieli),
-      StringCell(19, toBooleanX(h.koulutusmarkkinointilupa)),
-      StringCell(20, toBooleanX(h.onYlioppilas))
-    ))).toSet.zipWithIndex.map((rowWithIndex) => Row(rowWithIndex._2 + 1, rowWithIndex._1))
-  }
+  override def getRows(hakijat: Seq[Hakija]): Set[Row] = hakijat.flatMap((hakija) => hakija.hakemukset.flatMap(hakemus => hakemus.hakukohteenKoulutukset.map(koulutus => Set[Cell](
+    StringCell(0, hakija.hetu),
+    StringCell(1, hakija.oppijanumero),
+    StringCell(2, hakija.sukunimi),
+    StringCell(3, hakija.etunimet),
+    StringCell(4, hakija.kutsumanimi),
+    StringCell(5, hakija.lahiosoite),
+    StringCell(6, hakija.postinumero),
+    StringCell(7, hakija.postitoimipaikka),
+    StringCell(8, hakija.maa),
+    StringCell(9, hakija.kansalaisuus),
+    StringCell(10, hakija.matkapuhelin.getOrElse("")),
+    StringCell(12, hakija.puhelin.getOrElse("")),
+    StringCell(13, hakija.sahkoposti.getOrElse("")),
+    StringCell(14, hakija.kotikunta),
+    StringCell(15, hakija.sukupuoli),
+    StringCell(16, hakija.aidinkieli),
+    StringCell(17, hakija.asiointikieli),
+    StringCell(18, hakija.koulusivistyskieli),
+    StringCell(19, toBooleanX(hakija.koulutusmarkkinointilupa)),
+    StringCell(20, toBooleanX(hakija.onYlioppilas))
+  )))).toSet.zipWithIndex.map((rowWithIndex) => Row(rowWithIndex._2 + 1, rowWithIndex._1))
 
 }
