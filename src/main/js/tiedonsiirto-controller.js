12e423e94f29c188085afe055181c59e64d06132
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-11-04 14:16:45 +0200

    refactor file upload

--- src/main/js/tiedonsiirto-controller.js
+++ src/main/js/tiedonsiirto-controller.js
@@ -1,12 +1,9 @@
 function TiedonsiirtoCtrl($scope, $rootScope) {
     $rootScope.addToMurupolku({key: "suoritusrekisteri.tiedonsiirto.muru", text: "Tiedonsiirto"}, true);
 
-    $scope.messages = [];
-
     $scope.send = function() {
         $scope.sending = true;
         delete $scope.uploadResult;
-        $scope.messages = [];
     };
 
     $scope.uploadComplete = function(content) {
@@ -14,11 +11,11 @@ function TiedonsiirtoCtrl($scope, $rootScope) {
             $scope.uploadResult = typeof content === 'object' ? content : angular.fromJson(content);
             document.getElementById("uploadForm").reset();
         } else {
-            $scope.messages.push({
-                type: "alert",
+            $scope.uploadResult = {
+                type: "danger",
                 message: "Virhe lähettäessä tiedostoa",
                 messageKey: "suoritusrekisteri.tiedonsiirto.virhe"
-            })
+            }
         }
         delete $scope.sending;
     };
@@ -26,12 +23,6 @@ function TiedonsiirtoCtrl($scope, $rootScope) {
     $scope.reset = function() {
         document.getElementById("uploadForm").reset();
         delete $scope.uploadResult;
-        $scope.messages = [];
-    };
-
-    $scope.removeMessage = function(message) {
-        var index = $scope.messages.indexOf(message);
-        if (index !== -1) $scope.messages.splice(index, 1);
     };
 
     function isUploadResponse(content) {
