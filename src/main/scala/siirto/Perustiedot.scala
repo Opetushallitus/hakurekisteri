6d2b1747ea5a911358013b3498e38c4de18e02f4
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-11-17 09:42:05 +0200

    accept yksilollistaminen for lisaopetus

--- src/main/scala/siirto/Perustiedot.scala
+++ src/main/scala/siirto/Perustiedot.scala
@@ -1,31 +1,20 @@
 package siirto
 
-import java.io.StringReader
-import javax.xml.XMLConstants
-import javax.xml.transform.sax.SAXSource
-import javax.xml.validation.SchemaFactory
-
-import scala.xml.Source._
 import scala.xml.{XML, Elem}
 
 
 object Perustiedot extends SchemaDefinition {
-
   val schemaLocation = "perustiedot.xsd"
-
   val schema =
     <xs:schema attributeFormDefault="unqualified"
                elementFormDefault="qualified"
                xmlns:xs="http://www.w3.org/2001/XMLSchema"
                xmlns:koodisto="http://service.koodisto.sade.vm.fi/types/koodisto">
 
-
       <xs:import schemaLocation="perustiedot-koodisto.xsd"
-                 namespace="http://service.koodisto.sade.vm.fi/types/koodisto">
-      </xs:import>
+                 namespace="http://service.koodisto.sade.vm.fi/types/koodisto"/>
 
       <xs:element name="perustiedot" type="perustiedotType"/>
-
 
       <xs:simpleType name="hetuType">
         <xs:restriction base="xs:string">
@@ -40,32 +29,25 @@ object Perustiedot extends SchemaDefinition {
         </xs:restriction>
       </xs:simpleType>
 
-
       <xs:complexType name="henkiloType">
-
         <xs:sequence>
           <xs:choice>
-            <xs:element type="hetuType" name="hetu"></xs:element>
+            <xs:element type="hetuType" name="hetu"/>
             <xs:element type="oppijaNumeroType"
-                        name="oppijanumero">
-            </xs:element>
+                        name="oppijanumero"/>
             <xs:sequence>
               <xs:element type="xs:string"
-                          name="henkiloTunniste">
-              </xs:element>
+                          name="henkiloTunniste"/>
               <xs:element type="xs:date"
-                          name="syntymaAika">
-              </xs:element>
+                          name="syntymaAika"/>
               <xs:element type="koodisto:sukupuoli"
-                          name="sukupuoli">
-              </xs:element>
+                          name="sukupuoli"/>
             </xs:sequence>
           </xs:choice>
           <xs:element type="koodisto:oppilaitosnumero"
                       name="lahtokoulu" maxOccurs="1" minOccurs="1"/>
           <xs:element type="xs:string" name="luokka" maxOccurs="1"
                       minOccurs="1"/>
-
           <xs:element type="xs:string" name="sukunimi" maxOccurs="1"
                       minOccurs="1"/>
           <xs:element type="xs:string" name="etunimet" maxOccurs="1"
@@ -89,16 +71,13 @@ object Perustiedot extends SchemaDefinition {
           <xs:element type="xs:string" name="muuPuhelin" maxOccurs="1"
                       minOccurs="0"/>
           <xs:choice>
-            <xs:group ref="PerusopetuksenKaynyt"></xs:group>
-            <xs:group ref="UlkomainenKorvaava"></xs:group>
-            <xs:element name="lukio" type="SuoritusType" minOccurs="1" maxOccurs="1"></xs:element>
-            <xs:element name="ammatillinen" type="SuoritusType" minOccurs="1" maxOccurs="1"></xs:element>
-
+            <xs:group ref="PerusopetuksenKaynyt"/>
+            <xs:group ref="UlkomainenKorvaava"/>
+            <xs:element name="lukio" type="SuoritusType" minOccurs="1" maxOccurs="1"/>
+            <xs:element name="ammatillinen" type="SuoritusType" minOccurs="1" maxOccurs="1"/>
           </xs:choice>
         </xs:sequence>
       </xs:complexType>
-
-
 
       <xs:complexType name="perustiedotType">
         <xs:all>
@@ -118,7 +97,6 @@ object Perustiedot extends SchemaDefinition {
               <xs:selector xpath="henkilo"/>
               <xs:field xpath="henkiloTunniste"/>
             </xs:unique>
-
           </xs:element>
         </xs:all>
       </xs:complexType>
@@ -131,10 +109,10 @@ object Perustiedot extends SchemaDefinition {
 
       <xs:complexType name="SuoritusType">
         <xs:sequence>
-          <xs:element name="valmistuminen" type="xs:date" maxOccurs="1" minOccurs="1"></xs:element>
-          <xs:element name="myontaja" type="koodisto:oppilaitosnumero" maxOccurs="1" minOccurs="1"></xs:element>
-          <xs:element name="suorituskieli" type="koodisto:kieli" maxOccurs="1" minOccurs="1"></xs:element>
-          <xs:element name="tila" type="koodisto:suorituksentila" maxOccurs="1" minOccurs="1"></xs:element>
+          <xs:element name="valmistuminen" type="xs:date" maxOccurs="1" minOccurs="1"/>
+          <xs:element name="myontaja" type="koodisto:oppilaitosnumero" maxOccurs="1" minOccurs="1"/>
+          <xs:element name="suorituskieli" type="koodisto:kieli" maxOccurs="1" minOccurs="1"/>
+          <xs:element name="tila" type="koodisto:suorituksentila" maxOccurs="1" minOccurs="1"/>
         </xs:sequence>
       </xs:complexType>
 
@@ -142,42 +120,36 @@ object Perustiedot extends SchemaDefinition {
         <xs:complexContent>
           <xs:extension base="SuoritusType">
             <xs:sequence>
-              <xs:element name="yksilollistaminen" type="koodisto:yksilollistaminen" minOccurs="1" maxOccurs="1"></xs:element>
+              <xs:element name="yksilollistaminen" type="koodisto:yksilollistaminen" minOccurs="1" maxOccurs="1"/>
             </xs:sequence>
-
-
           </xs:extension>
         </xs:complexContent>
       </xs:complexType>
 
-
       <xs:group name="PerusopetuksenKaynyt">
         <xs:sequence>
           <xs:element name="perusopetus" type="PerusopetusType"
-                      maxOccurs="1" minOccurs="1">
-          </xs:element>
+                      maxOccurs="1" minOccurs="1"/>
           <xs:element name="perusopetuksenlisaopetus"
-                      type="SuoritusType" maxOccurs="1" minOccurs="0">
-          </xs:element>
-          <xs:group ref="LisapisteKoulutukset"></xs:group>
+                      type="PerusopetusType" maxOccurs="1" minOccurs="0"/>
+          <xs:group ref="LisapisteKoulutukset"/>
         </xs:sequence>
       </xs:group>
 
       <xs:group name="LisapisteKoulutukset">
         <xs:sequence>
           <xs:element name="ammattistartti"
-                      type="SuoritusType" maxOccurs="1" minOccurs="0">
-          </xs:element>
-          <xs:element name="valmentava" type="SuoritusType" maxOccurs="1" minOccurs="0"></xs:element>
-          <xs:element name="maahanmuuttajienlukioonvalmistava" type="SuoritusType" maxOccurs="1" minOccurs="0"></xs:element>
-          <xs:element name="maahanmuuttajienammvalmistava" type="SuoritusType" maxOccurs="1" minOccurs="0"></xs:element>
+                      type="SuoritusType" maxOccurs="1" minOccurs="0"/>
+          <xs:element name="valmentava" type="SuoritusType" maxOccurs="1" minOccurs="0"/>
+          <xs:element name="maahanmuuttajienlukioonvalmistava" type="SuoritusType" maxOccurs="1" minOccurs="0"/>
+          <xs:element name="maahanmuuttajienammvalmistava" type="SuoritusType" maxOccurs="1" minOccurs="0"/>
         </xs:sequence>
       </xs:group>
 
       <xs:group name="UlkomainenKorvaava">
         <xs:sequence>
-          <xs:element name="ulkomainen" type="SuoritusType" maxOccurs="1" minOccurs="1"></xs:element>
-          <xs:group ref="LisapisteKoulutukset"></xs:group>
+          <xs:element name="ulkomainen" type="SuoritusType" maxOccurs="1" minOccurs="1"/>
+          <xs:group ref="LisapisteKoulutukset"/>
         </xs:sequence>
       </xs:group>
 
@@ -187,12 +159,9 @@ object Perustiedot extends SchemaDefinition {
 
 object PerustiedotKoodisto extends IncludeSchema("http://service.koodisto.sade.vm.fi/types/koodisto", "https://virkailija.opintopolku.fi/koodisto-service/rest/kunta.xsd", "https://virkailija.opintopolku.fi/koodisto-service/rest/oppilaitosnumero.xsd", "https://virkailija.opintopolku.fi/koodisto-service/rest/kieli.xsd", "https://virkailija.opintopolku.fi/koodisto-service/rest/maatjavaltiot2.xsd", "https://virkailija.opintopolku.fi/koodisto-service/rest/posti.xsd", "https://virkailija.opintopolku.fi/koodisto-service/rest/sukupuoli.xsd", "https://virkailija.opintopolku.fi/koodisto-service/rest/yksilollistaminen.xsd", "https://virkailija.opintopolku.fi/koodisto-service/rest/suorituksentila.xsd") {
   override val schemaLocation: String = "perustiedot-koodisto.xsd"
-
 }
 
 abstract class IncludeSchema(namespace: String, remoteschemas: String*) extends SchemaWithRemotes {
-
-
   lazy val schema =
     <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
                                targetNamespace={namespace}
@@ -201,30 +170,22 @@ abstract class IncludeSchema(namespace: String, remoteschemas: String*) extends 
       {includes}
     </xs:schema>
 
-
   def includes: Seq[Elem] = for (
     remote <- remoteschemas
   ) yield <xs:include schemaLocation={remote}/>
 }
 
 trait RemoteSchema extends SchemaDefinition {
-
   lazy val schema: Elem  = XML.load(schemaLocation)
-
-
 }
 
 trait SchemaWithRemotes extends SchemaDefinition  {
-
   lazy val remotes: Seq[SchemaDefinition] = (schema \\ "include" \\ "@schemaLocation").map((sl) => new RemoteSchema {
     override val schemaLocation: String = sl.text
   })
-
 }
 
 trait SchemaDefinition {
   val schemaLocation:String
   val schema: Elem
-
-
 }
--- src/test/scala/siirto/ValidXmlSpec.scala
+++ src/test/scala/siirto/ValidXmlSpec.scala
@@ -12,7 +12,7 @@ import scala.language.implicitConversions
 class ValidXmlSpec extends FlatSpec with Matchers {
 
   implicit def elemToInputSource(elem:Elem): InputSource = {
-    fromReader(new StringReader(elem.toString))
+    fromReader(new StringReader(elem.toString()))
   }
 
   behavior of "Xml Validation"
@@ -20,7 +20,6 @@ class ValidXmlSpec extends FlatSpec with Matchers {
   val testiKoodisto =  Seq(Kunnat, Oppilaitokset, Kielet, MaatJaValtiot, Posti, Sukupuoli, YksilollistysKoodisto, SuorituksenTila)
 
   val validator = new ValidXml(Perustiedot,  (PerustiedotKoodisto +: testiKoodisto):_*)
-
 
   it should "find invalid xml" in {
     validator.load(<perustiedot></perustiedot>).isFailure should be (true)
@@ -33,9 +32,7 @@ class ValidXmlSpec extends FlatSpec with Matchers {
   it should "disregard conflicting schema in root element" in {
     val path = getClass.getClassLoader.getResource("simple.xml")
     validator.load(path).isSuccess should be (false)
-
   }
-
 
   val valid =
     <perustiedot>
@@ -80,23 +77,18 @@ class ValidXmlSpec extends FlatSpec with Matchers {
           <matkapuhelin>040 1234 567</matkapuhelin>
           <muuPuhelin>09 1234 567</muuPuhelin>
           <ulkomainen>
-
             <valmistuminen>2014-06-04</valmistuminen>
             <myontaja>05127</myontaja>
             <suorituskieli>FI</suorituskieli>
             <tila>KESKEN</tila>
-
           </ulkomainen>
           <maahanmuuttajienammvalmistava>
             <valmistuminen>2015-06-04</valmistuminen>
             <myontaja>05127</myontaja>
             <suorituskieli>FI</suorituskieli>
             <tila>VALMIS</tila>
-
           </maahanmuuttajienammvalmistava>
-
         </henkilo>
-
         <henkilo>
           <oppijanumero>1.2.246.562.24.12345678901</oppijanumero>
           <lahtokoulu>05127</lahtokoulu>
@@ -123,227 +115,183 @@ class ValidXmlSpec extends FlatSpec with Matchers {
             <myontaja>05127</myontaja>
             <suorituskieli>FI</suorituskieli>
             <tila>KESKEN</tila>
-
+            <yksilollistaminen>EI</yksilollistaminen>
           </perusopetuksenlisaopetus>
         </henkilo>
       </henkilot>
     </perustiedot>
 
+  object Kunnat extends SchemaDefinition {
+    override val schema: Elem =
+      <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                 targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 elementFormDefault="qualified">
+        <xs:simpleType name="kunta">
+          <xs:restriction base="xs:string">
+            <xs:enumeration value="020">
+              <xs:annotation>
+                <xs:documentation xml:lang="sv">Akaa</xs:documentation>
+                <xs:documentation xml:lang="fi">Akaa</xs:documentation>
+              </xs:annotation>
+            </xs:enumeration>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:schema>
 
+    override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/kunta.xsd"
+  }
 
-object Kunnat extends SchemaDefinition {
-  override val schema: Elem =
-    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
-               targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
-               xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
-               elementFormDefault="qualified">
+  object Oppilaitokset extends SchemaDefinition {
+    override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/oppilaitosnumero.xsd"
+    override val schema: Elem =
+      <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                                           targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
+                                           xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
+                                           elementFormDefault="qualified">
+        <xs:simpleType name="oppilaitosnumero">
+          <xs:restriction base="xs:string">
+            <xs:enumeration value="05127">
+              <xs:annotation>
+                <xs:documentation xml:lang="fi">Pikkolan koulu</xs:documentation>
+                <xs:documentation xml:lang="sv">Pikkolan koulu</xs:documentation>
+              </xs:annotation>
+            </xs:enumeration>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:schema>
+  }
 
-    <xs:simpleType name="kunta">
-      <xs:restriction base="xs:string">
-        <xs:enumeration value="020">
-          <xs:annotation>
-            <xs:documentation xml:lang="sv">Akaa</xs:documentation>
-            <xs:documentation xml:lang="fi">Akaa</xs:documentation>
-          </xs:annotation>
-        </xs:enumeration>
-      </xs:restriction>
-    </xs:simpleType>
-  </xs:schema>
+  object Kielet extends SchemaDefinition {
+    override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/kieli.xsd"
 
-  override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/kunta.xsd"
-}
+    override val schema: Elem =
+      <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                 targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 elementFormDefault="qualified">
+        <xs:simpleType name="kieli">
+          <xs:restriction base="xs:string">
+            <xs:enumeration value="FI">
+              <xs:annotation>
+                <xs:documentation xml:lang="en">Finnish</xs:documentation>
+                <xs:documentation xml:lang="fi">suomi</xs:documentation>
+                <xs:documentation xml:lang="sv">finska</xs:documentation>
+              </xs:annotation>
+            </xs:enumeration>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:schema>
+  }
 
-object Oppilaitokset extends SchemaDefinition {
-  override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/oppilaitosnumero.xsd"
-  override val schema: Elem =
-    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
-                                         targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
-                                         xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
-                                         elementFormDefault="qualified">
+  object MaatJaValtiot extends SchemaDefinition {
+    override val schemaLocation: String ="https://virkailija.opintopolku.fi/koodisto-service/rest/maatjavaltiot2.xsd"
 
-    <xs:simpleType name="oppilaitosnumero">
-      <xs:restriction base="xs:string">
-        <xs:enumeration value="05127">
-          <xs:annotation>
-            <xs:documentation xml:lang="fi">Pikkolan koulu</xs:documentation>
-            <xs:documentation xml:lang="sv">Pikkolan koulu</xs:documentation>
-          </xs:annotation>
-        </xs:enumeration>
-      </xs:restriction>
-    </xs:simpleType>
+    override val schema: Elem =
+      <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                 targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 elementFormDefault="qualified">
+        <xs:simpleType name="maatjavaltiot2">
+          <xs:restriction base="xs:string">
+            <xs:enumeration value="246">
+              <xs:annotation>
+                <xs:documentation xml:lang="fi">Suomi</xs:documentation>
+                <xs:documentation xml:lang="en">Finland</xs:documentation>
+                <xs:documentation xml:lang="sv">Finland</xs:documentation>
+              </xs:annotation>
+            </xs:enumeration>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:schema>
+  }
 
-  </xs:schema>
+  object Posti extends SchemaDefinition {
+    override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/posti.xsd"
 
-        }
+    override val schema: Elem =
+      <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                 targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 elementFormDefault="qualified">
+        <xs:simpleType name="posti">
+          <xs:restriction base="xs:string">
+            <xs:enumeration value="00100">
+              <xs:annotation>
+                <xs:documentation xml:lang="fi">HELSINKI</xs:documentation>
+                <xs:documentation xml:lang="sv">HELSINGFORS</xs:documentation>
+              </xs:annotation>
+            </xs:enumeration>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:schema>
+  }
 
-object Kielet extends SchemaDefinition {
-  override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/kieli.xsd"
+  object Sukupuoli extends SchemaDefinition {
+    override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/sukupuoli.xsd"
 
-  override val schema: Elem =
-    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
-               targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
-               xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
-               elementFormDefault="qualified">
+    override val schema: Elem =
+      <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                 targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 elementFormDefault="qualified">
+        <xs:simpleType name="sukupuoli">
+          <xs:restriction base="xs:string">
+            <xs:enumeration value="2">
+              <xs:annotation>
+                <xs:documentation xml:lang="fi">nainen</xs:documentation>
+                <xs:documentation xml:lang="sv">kvinna</xs:documentation>
+                <xs:documentation xml:lang="en">woman</xs:documentation>
+              </xs:annotation>
+            </xs:enumeration>
+            <xs:enumeration value="1">
+              <xs:annotation>
+                <xs:documentation xml:lang="en">man</xs:documentation>
+                <xs:documentation xml:lang="sv">man</xs:documentation>
+                <xs:documentation xml:lang="fi">mies</xs:documentation>
+              </xs:annotation>
+            </xs:enumeration>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:schema>
+  }
 
-      <xs:simpleType name="kieli">
-        <xs:restriction base="xs:string">
-          <xs:enumeration value="FI">
-            <xs:annotation>
-              <xs:documentation xml:lang="en">Finnish</xs:documentation>
-              <xs:documentation xml:lang="fi">suomi</xs:documentation>
-              <xs:documentation xml:lang="sv">finska</xs:documentation>
-            </xs:annotation>
-          </xs:enumeration>
+  object YksilollistysKoodisto extends SchemaDefinition {
+    override val schemaLocation: String ="https://virkailija.opintopolku.fi/koodisto-service/rest/yksilollistaminen.xsd"
 
-        </xs:restriction>
-      </xs:simpleType>
+    override val schema: Elem =
+      <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                 targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 elementFormDefault="qualified">
+        <xs:simpleType name="yksilollistaminen">
+          <xs:restriction base="xs:string">
+            <xs:enumeration value="EI"/>
+            <xs:enumeration value="OSITTAIN"/>
+            <xs:enumeration value="ALUEITTAIN"/>
+            <xs:enumeration value="KOKONAAN"/>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:schema>
+  }
 
-    </xs:schema>
+  object SuorituksenTila extends  SchemaDefinition {
+    override val schemaLocation: String ="https://virkailija.opintopolku.fi/koodisto-service/rest/suorituksentila.xsd"
 
-}
-
-object MaatJaValtiot extends SchemaDefinition {
-  override val schemaLocation: String ="https://virkailija.opintopolku.fi/koodisto-service/rest/maatjavaltiot2.xsd"
-
-  override val schema: Elem =
-    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
-               targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
-               xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
-               elementFormDefault="qualified">
-
-      <xs:simpleType name="maatjavaltiot2">
-        <xs:restriction base="xs:string">
-          <xs:enumeration value="246">
-            <xs:annotation>
-              <xs:documentation xml:lang="fi">Suomi</xs:documentation>
-              <xs:documentation xml:lang="en">Finland</xs:documentation>
-              <xs:documentation xml:lang="sv">Finland</xs:documentation>
-            </xs:annotation>
-          </xs:enumeration>
-
-        </xs:restriction>
-      </xs:simpleType>
-
-    </xs:schema>
-
-}
-
-object Posti extends SchemaDefinition {
-  override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/posti.xsd"
-
-  override val schema: Elem =
-    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
-               targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
-               xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
-               elementFormDefault="qualified">
-
-      <xs:simpleType name="posti">
-        <xs:restriction base="xs:string">
-          <xs:enumeration value="00100">
-            <xs:annotation>
-              <xs:documentation xml:lang="fi">HELSINKI</xs:documentation>
-              <xs:documentation xml:lang="sv">HELSINGFORS</xs:documentation>
-            </xs:annotation>
-          </xs:enumeration>
-
-        </xs:restriction>
-      </xs:simpleType>
-
-    </xs:schema>
-}
-
-object Sukupuoli extends SchemaDefinition {
-  override val schemaLocation: String = "https://virkailija.opintopolku.fi/koodisto-service/rest/sukupuoli.xsd"
-
-  override val schema: Elem =
-    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
-               targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
-               xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
-               elementFormDefault="qualified">
-
-      <xs:simpleType name="sukupuoli">
-        <xs:restriction base="xs:string">
-          <xs:enumeration value="2">
-            <xs:annotation>
-              <xs:documentation xml:lang="fi">nainen</xs:documentation>
-              <xs:documentation xml:lang="sv">kvinna</xs:documentation>
-              <xs:documentation xml:lang="en">woman</xs:documentation>
-            </xs:annotation>
-          </xs:enumeration>
-
-          <xs:enumeration value="1">
-            <xs:annotation>
-              <xs:documentation xml:lang="en">man</xs:documentation>
-              <xs:documentation xml:lang="sv">man</xs:documentation>
-              <xs:documentation xml:lang="fi">mies</xs:documentation>
-            </xs:annotation>
-          </xs:enumeration>
-
-        </xs:restriction>
-      </xs:simpleType>
-
-
-    </xs:schema>
-
-
-
-}
-
-object YksilollistysKoodisto extends SchemaDefinition {
-  override val schemaLocation: String ="https://virkailija.opintopolku.fi/koodisto-service/rest/yksilollistaminen.xsd"
-
-  override val schema: Elem =
-    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
-               targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
-               xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
-               elementFormDefault="qualified">
-
-      <xs:simpleType name="yksilollistaminen">
-        <xs:restriction base="xs:string">
-          <xs:enumeration value="EI">
-
-          </xs:enumeration>
-
-          <xs:enumeration value="OSITTAIN">
-          </xs:enumeration>
-
-          <xs:enumeration value="ALUEITTAIN">
-          </xs:enumeration>
-
-          <xs:enumeration value="KOKONAAN">
-          </xs:enumeration>
-
-        </xs:restriction>
-      </xs:simpleType>
-
-    </xs:schema>
-}
-
-object SuorituksenTila extends  SchemaDefinition {
-  override val schemaLocation: String ="https://virkailija.opintopolku.fi/koodisto-service/rest/suorituksentila.xsd"
-
-  override val schema: Elem =
-    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
-               targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
-               xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
-               elementFormDefault="qualified">
-
-      <xs:simpleType name="suorituksentila">
-        <xs:restriction base="xs:string">
-          <xs:enumeration value="KESKEN">
-          </xs:enumeration>
-
-          <xs:enumeration value="KESKEYTYNYT">
-          </xs:enumeration>
-
-          <xs:enumeration value="VALMIS">
-          </xs:enumeration>
-
-        </xs:restriction>
-      </xs:simpleType>
-
-    </xs:schema>
-}
+    override val schema: Elem =
+      <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                 targetNamespace="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 xmlns="http://service.koodisto.sade.vm.fi/types/koodisto"
+                 elementFormDefault="qualified">
+        <xs:simpleType name="suorituksentila">
+          <xs:restriction base="xs:string">
+            <xs:enumeration value="KESKEN"/>
+            <xs:enumeration value="KESKEYTYNYT"/>
+            <xs:enumeration value="VALMIS"/>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:schema>
+  }
 }
 
 
