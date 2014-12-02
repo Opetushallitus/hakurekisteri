08f8d3fd67989b0c605253f5e032a70954ef0f55
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-12-02 16:51:02 +0200

    adjust import batch test

--- src/test/scala/fi/vm/sade/hakurekisteri/batchimport/ImportBatchProcessingActorSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/batchimport/ImportBatchProcessingActorSpec.scala
@@ -6,10 +6,10 @@ import akka.actor.{Props, ActorSystem}
 import com.ning.http.client.AsyncHttpClient
 import fi.vm.sade.hakurekisteri.integration.organisaatio.OrganisaatioActor
 import fi.vm.sade.hakurekisteri.integration._
-import fi.vm.sade.hakurekisteri.integration.henkilo.HenkiloActor
+import fi.vm.sade.hakurekisteri.integration.henkilo.{Henkilo, HenkiloActor}
 import fi.vm.sade.hakurekisteri.integration.virta.MockedResourceActor
 import fi.vm.sade.hakurekisteri.opiskelija.Opiskelija
-import fi.vm.sade.hakurekisteri.rest.support.Query
+import fi.vm.sade.hakurekisteri.rest.support.{HakurekisteriJsonSupport, Query}
 import fi.vm.sade.hakurekisteri.storage.Identified
 import fi.vm.sade.hakurekisteri.suoritus.{VirallinenSuoritus, Suoritus}
 import org.mockito.Mockito._
@@ -20,16 +20,50 @@ import org.scalatest.mock.MockitoSugar
 import scala.concurrent.ExecutionContext
 
 
-class ImportBatchProcessingActorSpec extends FlatSpec with Matchers with MockitoSugar with DispatchSupport with AsyncAssertions {
+class ImportBatchProcessingActorSpec extends FlatSpec with Matchers with MockitoSugar with DispatchSupport with AsyncAssertions with HakurekisteriJsonSupport {
   implicit val system = ActorSystem("test-import-batch-processing")
   implicit val ec: ExecutionContext = system.dispatcher
 
   behavior of "ImportBatchProcessingActor"
 
+  val batch: ImportBatch with Identified[UUID] = ImportBatch(<perustiedot>
+    <eranTunniste>eranTunniste</eranTunniste>
+    <henkilot>
+      <henkilo>
+        <hetu>111111-1975</hetu>
+        <lahtokoulu>05127</lahtokoulu>
+        <luokka>9A</luokka>
+        <sukunimi>Testinen</sukunimi>
+        <etunimet>Juha Jaakko</etunimet>
+        <kutsumanimi>Jaakko</kutsumanimi>
+        <kotikunta>020</kotikunta>
+        <aidinkieli>FI</aidinkieli>
+        <kansalaisuus>246</kansalaisuus>
+        <lahiosoite>Katu 1 A 1</lahiosoite>
+        <postinumero>00100</postinumero>
+        <matkapuhelin>040 1234 567</matkapuhelin>
+        <muuPuhelin>09 1234 567</muuPuhelin>
+        <perusopetus>
+          <valmistuminen>2015-06-04</valmistuminen>
+          <myontaja>05127</myontaja>
+          <suorituskieli>FI</suorituskieli>
+          <tila>KESKEN</tila>
+          <yksilollistaminen>EI</yksilollistaminen>
+        </perusopetus>
+      </henkilo>
+    </henkilot>
+  </perustiedot>, Some("foo"), "perustiedot", "testitiedonsiirto").identify(UUID.randomUUID())
+
   def createEndpoint = {
     val result = mock[Endpoint]
 
-    when(result.request(forUrl("http://localhost/authentication-service/resources/s2s/tiedonsiirrot"))).thenReturn((200, List(), "{\"oidHenkilo\":\"1.2.246.562.24.123\"}"))
+    val henkiloBody = {
+      val henkilo: Henkilo = (batch.data \ "henkilot" \ "henkilo").map(ImportHenkilo(_)("")).head.toHenkilo
+      import org.json4s.jackson.Serialization.write
+      write[Henkilo](henkilo)
+    }
+
+    when(result.request(forUrl("http://localhost/authentication-service/resources/s2s/tiedonsiirrot", henkiloBody))).thenReturn((200, List(), "{\"oidHenkilo\":\"1.2.246.562.24.123\"}"))
 
     when(result.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio"))).thenReturn((200, List(), "[]"))
     when(result.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/05127"))).thenReturn((200, List(), "{\"oid\":\"1.2.246.562.5.05127\",\"nimi\":{}}"))
@@ -41,7 +75,6 @@ class ImportBatchProcessingActorSpec extends FlatSpec with Matchers with Mockito
 
 
   it should "import data into henkilopalvelu and suoritusrekisteri" in {
-    val batch: ImportBatch with Identified[UUID] = ImportBatch(data, Some("foo"), "perustiedot", "testitiedonsiirto").identify(UUID.randomUUID())
     val importBatchActor = system.actorOf(Props(new MockedResourceActor[ImportBatch](save = {r =>}, query = { (q) => Seq(batch) })))
     val henkiloClient = new VirkailijaRestClient(ServiceConfig(serviceUrl = "http://localhost/authentication-service"), Some(new AsyncHttpClient(asyncProvider)))
     val henkiloActor = system.actorOf(Props(new HenkiloActor(henkiloClient)))
@@ -70,31 +103,4 @@ class ImportBatchProcessingActorSpec extends FlatSpec with Matchers with Mockito
     oWaiter.await(timeout(10.seconds), dismissals(1))
   }
 
-  val data = <perustiedot>
-    <eranTunniste>eranTunniste</eranTunniste>
-    <henkilot>
-      <henkilo>
-        <hetu>111111-1975</hetu>
-        <lahtokoulu>05127</lahtokoulu>
-        <luokka>9A</luokka>
-        <sukunimi>Testinen</sukunimi>
-        <etunimet>Juha Jaakko</etunimet>
-        <kutsumanimi>Jaakko</kutsumanimi>
-        <kotikunta>020</kotikunta>
-        <aidinkieli>FI</aidinkieli>
-        <kansalaisuus>246</kansalaisuus>
-        <lahiosoite>Katu 1 A 1</lahiosoite>
-        <postinumero>00100</postinumero>
-        <matkapuhelin>040 1234 567</matkapuhelin>
-        <muuPuhelin>09 1234 567</muuPuhelin>
-        <perusopetus>
-          <valmistuminen>2015-06-04</valmistuminen>
-          <myontaja>05127</myontaja>
-          <suorituskieli>FI</suorituskieli>
-          <tila>KESKEN</tila>
-          <yksilollistaminen>EI</yksilollistaminen>
-        </perusopetus>
-      </henkilo>
-    </henkilot>
-  </perustiedot>
 }
