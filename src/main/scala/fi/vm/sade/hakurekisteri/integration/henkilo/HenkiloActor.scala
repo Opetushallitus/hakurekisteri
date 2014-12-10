a22d1de7b233ca9e1172fe2e48552de1e001a11a
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-12-10 11:14:52 +0200

    perustiedot tiedonsiirto, henkilon luonti (devel)

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/henkilo/HenkiloActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/henkilo/HenkiloActor.scala
@@ -1,18 +1,31 @@
 package fi.vm.sade.hakurekisteri.integration.henkilo
 
 import java.net.URLEncoder
+import java.util.concurrent.ExecutionException
 
 import akka.actor.{ActorLogging, Actor}
 import akka.pattern.pipe
 import fi.vm.sade.hakurekisteri.Config
-import fi.vm.sade.hakurekisteri.integration.VirkailijaRestClient
+import fi.vm.sade.hakurekisteri.integration.{PreconditionFailedException, VirkailijaRestClient}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Future, ExecutionContext}
 
 case class Kieli(kieliKoodi: String, kieliTyyppi: Option[String] = None)
 
-case class Henkilo(id: Option[Int] = None,
-                   oidHenkilo: Option[String],
+case class OrganisaatioHenkilo(organisaatioOid: String)
+
+case class CreateHenkilo(etunimet: String,
+                         kutsumanimi: String,
+                         sukunimi: String,
+                         hetu: Option[String],
+                         syntymaaika: Option[String],
+                         sukupuoli: Option[String],
+                         asiointiKieli: Kieli,
+                         henkiloTyyppi: String,
+                         kasittelijaOid: String,
+                         organisaatioHenkilo: Seq[OrganisaatioHenkilo])
+
+case class Henkilo(oidHenkilo: String,
                    hetu: Option[String],
                    henkiloTyyppi: String,
                    etunimet: Option[String],
@@ -21,19 +34,22 @@ case class Henkilo(id: Option[Int] = None,
                    kotikunta: Option[String],
                    aidinkieli: Option[Kieli])
 
-case class SaveHenkilo(henkilo: Henkilo, tunniste: String)
+case class SaveHenkilo(henkilo: CreateHenkilo, tunniste: String)
 case class SavedHenkilo(henkiloOid: String, tunniste: String)
+case class CheckHenkilo(henkiloOid: String)
 
 case class HenkiloSearchResponse(totalCount: Int, results: Seq[Henkilo])
 
 case class FoundHenkilos(henkilot: Seq[Henkilo], tunniste: String)
+
+case class HenkiloNotFoundException(oid: String) extends Exception(s"henkilo not found with oid $oid")
 
 class HenkiloActor(henkiloClient: VirkailijaRestClient) extends Actor with ActorLogging {
   val maxRetries = Config.httpClientMaxRetries
 
   implicit val ec: ExecutionContext = context.dispatcher
 
-  val Hetu = "([0-9]{6}[-A][0-9]{3}[0123456789ABCDEFHJKLMNPRSTUVWXY])".r
+  import HetuUtil.Hetu
 
   override def receive: Receive = {
     case henkiloOid: String =>
@@ -53,9 +69,32 @@ class HenkiloActor(henkiloClient: VirkailijaRestClient) extends Actor with Actor
           map(r => FoundHenkilos(r.results, q.tunniste)) pipeTo sender
       }
 
-    case SaveHenkilo(henkilo, tunniste) =>
-      henkiloClient.postObject[Henkilo, String](s"/resources/s2s/tiedonsiirrot", 200, henkilo).
+    case SaveHenkilo(henkilo: CreateHenkilo, tunniste) =>
+      henkiloClient.postObject[CreateHenkilo, String](s"/resources/s2s/tiedonsiirrot", 200, henkilo).
         map(saved => SavedHenkilo(saved, tunniste)) pipeTo sender
+
+    case CheckHenkilo(henkiloOid) =>
+      def notFound(t: Throwable) = t match {
+        case PreconditionFailedException(_, 500) => true
+        case _ => false
+      }
+      henkiloClient.readObject[Henkilo](s"/resources/s2s/${URLEncoder.encode(henkiloOid, "UTF-8")}", 200, maxRetries).map(h => SavedHenkilo(h.oidHenkilo, henkiloOid)).recoverWith {
+        case t: ExecutionException if t.getCause != null && notFound(t.getCause) => Future.failed(HenkiloNotFoundException(henkiloOid))
+      } pipeTo sender
+  }
+}
+
+object HetuUtil {
+  val Hetu = "([0-9]{6}[-A][0-9]{3}[0123456789ABCDEFHJKLMNPRSTUVWXY])".r
+
+  def toSyntymaAika(hetu: String): Option[String] = hetu match {
+    case Hetu(h) =>
+      val vuosisata = h.charAt(6) match {
+        case '-' => "19"
+        case 'A' => "20"
+      }
+      Some(s"$vuosisata${h.substring(4, 6)}-${h.substring(2, 4)}-${h.substring(0, 2)}")
+   case _ => None
   }
 }
 
