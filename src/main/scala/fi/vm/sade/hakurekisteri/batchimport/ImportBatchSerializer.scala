d396c2ba13af8b0b4b8b2b2489df1c7fd5a6cf2d
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-01-09 12:18:14 +0200

    decompose instead of manual deserialization

--- src/main/scala/fi/vm/sade/hakurekisteri/batchimport/ImportBatchSerializer.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/batchimport/ImportBatchSerializer.scala
@@ -5,7 +5,7 @@ import java.util.UUID
 import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriJsonSupport
 import fi.vm.sade.hakurekisteri.storage.Identified
 import org.joda.time.DateTime
-import org.json4s.CustomSerializer
+import org.json4s.{Extraction, CustomSerializer}
 import org.json4s.JsonAST.{JField, JValue, JString, JObject}
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
@@ -36,31 +36,18 @@ class ImportBatchSerializer extends CustomSerializer[ImportBatch] (format => (
   },
   {
     case ib: ImportBatch with Identified[UUID] =>
-      val status: JObject =
-        ("sentTime" -> ib.status.sentTime.toString) ~
-        ("messages" -> ib.status.messages)
-      val statusWithProcessedTime: JObject = ib.status.processedTime.map((t: DateTime) => status ~ ("processedTime" -> t.toString)).getOrElse(status)
-      val statusWithSuccessRows: JObject = ib.status.successRows.map((r: Int) => statusWithProcessedTime ~ ("successRows" -> r)).getOrElse(statusWithProcessedTime)
-      val statusWithFailureRows: JObject = ib.status.failureRows.map((r: Int) => statusWithSuccessRows ~ ("failureRows" -> r)).getOrElse(statusWithSuccessRows)
-      val statusWithTotalRows: JObject = ib.status.totalRows.map((r: Int) => statusWithFailureRows ~ ("totalRows" -> r)).getOrElse(statusWithFailureRows)
-      val statusWithSavedReferences: JObject = ib.status.savedReferences.map((refs) => {
-        val references = for (
-          henkilo <- refs.toList
-        ) yield {
-          val saved = for (
-            ref <- henkilo._2.toList
-          ) yield (ref._1, JString(ref._2))
-          JField(henkilo._1, JObject(saved))
-        }
-        statusWithTotalRows ~ ("savedReferences" -> JObject(references))
-      }).getOrElse(statusWithTotalRows)
+      implicit val formats = HakurekisteriJsonSupport.format
+      val s: JObject = Extraction.decompose(ib.status) match {
+        case o: JObject => o
+        case _ => JObject(List())
+      }
 
       val result =  ("id" -> ib.id.toString) ~
                     ("data" -> toJson(ib.data)) ~
                     ("batchType" -> ib.batchType) ~
                     ("source" -> ib.source) ~
                     ("state" -> ib.state.toString) ~
-                    ("status" -> statusWithSavedReferences)
+                    ("status" -> s)
 
       ib.externalId.map(id => result ~ ("externalId" -> id)).getOrElse(result)
   }
