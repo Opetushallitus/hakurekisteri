368f698359744f9572f6da21ffc3473c9e1c3944
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-01-16 09:42:14 +0200

    handle datetime zone correctly to json

--- src/main/scala/fi/vm/sade/hakurekisteri/rest/support/HakurekisteriJsonSupport.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/rest/support/HakurekisteriJsonSupport.scala
@@ -1,17 +1,19 @@
 package fi.vm.sade.hakurekisteri.rest.support
 
 import fi.vm.sade.hakurekisteri.batchimport.ImportBatchSerializer
-import org.json4s.{FieldSerializer, DefaultFormats, Formats}
+import org.joda.time.DateTime
+import org.json4s.JsonAST.JString
+import org.json4s._
 import fi.vm.sade.hakurekisteri.suoritus.yksilollistaminen
 import fi.vm.sade.hakurekisteri.storage.Identified
 import fi.vm.sade.hakurekisteri.integration.valintatulos.{Ilmoittautumistila, Valintatila, Vastaanottotila}
 import fi.vm.sade.hakurekisteri.batchimport.BatchState
-import org.json4s.ext.DateTimeSerializer
-import java.util.UUID
+import org.json4s.ext.DateParser
+import java.util.{TimeZone, UUID}
 
 trait HakurekisteriJsonSupport {
 
-  protected implicit def jsonFormats: Formats = DefaultFormats.lossless.withBigDecimal +
+  protected implicit def jsonFormats: Formats = HakurekisteriDefaultFormats.lossless.withBigDecimal +
     new org.json4s.ext.EnumNameSerializer(yksilollistaminen) +
     new org.json4s.ext.EnumNameSerializer(Ilmoittautumistila) +
     new org.json4s.ext.EnumNameSerializer(Valintatila) +
@@ -20,7 +22,7 @@ trait HakurekisteriJsonSupport {
     FieldSerializer[Identified[UUID]]() +
     new UUIDSerializer +
     new IdentitySerializer +
-    DateTimeSerializer +
+    HakurekisteriDateTimeSerializer +
     new LocalDateSerializer() +
     new ArvioSerializer +
     new AjanjaksoSerializer +
@@ -36,3 +38,31 @@ object HakurekisteriJsonSupport extends HakurekisteriJsonSupport  {
   val format = jsonFormats
 
 }
+
+
+object HakurekisteriDefaultFormats extends DefaultFormats {
+  private class ThreadLocal[A](init: => A) extends java.lang.ThreadLocal[A] with (() => A) {
+    override def initialValue = init
+    def apply = get
+  }
+  override def lossless: Formats = new DefaultFormats {
+    val losslessDate = new ThreadLocal(new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
+    override def dateFormatter = {
+      val formatter = losslessDate()
+      formatter.setTimeZone(TimeZone.getTimeZone("UTC"))
+      formatter
+    }
+  }
+  val UTC = TimeZone.getTimeZone("UTC")
+}
+
+case object HakurekisteriDateTimeSerializer extends CustomSerializer[DateTime](format => (
+  {
+    case JString(s) => new DateTime(DateParser.parse(s, format))
+    case JNull => null
+  },
+  {
+    case d: DateTime =>
+      JString(format.dateFormat.format(d.toDate))
+  }
+  ))
