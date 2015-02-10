c74d64bafe154d48d84391cbb1fc3508911e2d8b
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-02-10 10:57:24 +0200

    match valmistuminen

--- core/src/main/scala/fi/vm/sade/hakurekisteri/batchimport/ArvosanatProcessing.scala
+++ core/src/main/scala/fi/vm/sade/hakurekisteri/batchimport/ArvosanatProcessing.scala
@@ -83,8 +83,8 @@ class ArvosanatProcessing(organisaatioActor: ActorRef, henkiloActor: ActorRef, s
   }
 
   private def matchSuoritus(todistus: ImportTodistus)(suoritus: Suoritus): Boolean = (todistus, suoritus) match {
-    case (ImportTodistus(Config.perusopetusKomoOid, _, _, _), s: VirallinenSuoritus) if s.komo == Config.perusopetusKomoOid => true
-    case (ImportTodistus(Config.lisaopetusKomoOid, _, _, _), s: VirallinenSuoritus) if s.komo == Config.lisaopetusKomoOid => true
+    case (ImportTodistus(Config.perusopetusKomoOid, _, _, v), s: VirallinenSuoritus) if s.komo == Config.perusopetusKomoOid && s.valmistuminen == v => true
+    case (ImportTodistus(Config.lisaopetusKomoOid, _, _, v), s: VirallinenSuoritus) if s.komo == Config.lisaopetusKomoOid && s.valmistuminen == v => true
     case _ => false
   }
 
