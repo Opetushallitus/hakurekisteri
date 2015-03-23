e5b7a057861af1dc9eef401d0b77f1b2cd7a6cc1
Mikko Apo <mikko.apo@reaktor.fi>
2015-03-23 14:38:34 +0200

    removed old style tests

--- web/src/main/webapp/demo/spec/opiskelijatiedotPageSpec.js
+++ web/src/main/webapp/demo/spec/opiskelijatiedotPageSpec.js
@@ -19,29 +19,6 @@
         })
 
         describe("Haku", function () {
-            it('Voi hakea oppilaitoksen perusteella', function (done) {
-                exists(page.organizationSearch)()
-                    .then(wait.forAngular)
-                    .then(function () {
-                        page.organizationSearch().val("Pik").change()
-                    })
-                    .then(wait.forAngular)
-                    .then(function () {
-                        page.dropDownMenu().children().first().click()
-                    })
-                    .then(function () {
-                        page.searchButton().click()
-                    })
-                    .then(wait.forAngular)
-                    .then(function () {
-                        expect(page.resultsTable().length).to.equal(5)
-                    }).then(function () {
-                        done()
-                    }, function (err) {
-                        done(err)
-                    })
-            })
-/*
             it('Voi hakea oppilaitoksen perusteella - test-dsl', seqDone(
                 wait.forAngular,
                 autocomplete(opiskelijatiedot.organizationSearch, "Pik", opiskelijatiedot.organizationDropDownMenuChild(1)),
@@ -52,33 +29,9 @@
                     expect(opiskelijatiedot.resultsTable().length).to.equal(5)
                 }
             ))
-            */
         })
 
         describe('Henkilohaku', function () {
-            it('Voi hakea oidin perusteella', function (done) {
-                exists(page.henkiloSearch)()
-                    .then(wait.forAngular)
-                    .then(function () {
-                        page.henkiloSearch().val('1.2.246.562.24.71944845619').change()
-                    })
-                    .then(wait.forAngular)
-                    .then(function () {
-                        page.searchButton().click()
-                    })
-                    .then(wait.forAngular)
-                    .then(function () {
-                        expect(page.resultsTable().length).to.equal(1)
-                        expect(page.henkiloTiedot().is(':visible')).to.equal(true)
-                        expect(page.suoritusTiedot().is(':visible')).to.equal(true)
-                        expect(page.luokkaTiedot().is(':visible')).to.equal(true)
-                    }).then(function () {
-                        done()
-                    }, function (err) {
-                        done(err)
-                    })
-            })
-/*
             it('Voi hakea oidin perusteella - test-dsl', seqDone(
                 wait.forAngular,
                 input(opiskelijatiedot.henkiloSearch, '1.2.246.562.24.71944845619'),
@@ -91,30 +44,7 @@
                     expect(opiskelijatiedot.luokkaTiedot().is(':visible')).to.equal(true)
                 }
             ))
-*/
-            it('Voi hakea hetun perusteella', function (done) {
-                exists(page.henkiloSearch)()
-                    .then(wait.forAngular)
-                    .then(function () {
-                        page.henkiloSearch().val('123456-789').change()
-                    })
-                    .then(wait.forAngular)
-                    .then(function () {
-                        page.searchButton().click()
-                    })
-                    .then(wait.forAngular)
-                    .then(function () {
-                        expect(page.resultsTable().length).to.equal(1)
-                        expect(page.henkiloTiedot().is(':visible')).to.equal(true)
-                        expect(page.suoritusTiedot().is(':visible')).to.equal(true)
-                        expect(page.luokkaTiedot().is(':visible')).to.equal(true)
-                    }).then(function () {
-                        done()
-                    }, function (err) {
-                        done(err)
-                    })
-            })
-/*
+
             it('Voi hakea hetun perusteella - test.dsl', seqDone(
                 wait.forAngular,
                 input(opiskelijatiedot.henkiloSearch, '123456-789'),
@@ -127,30 +57,7 @@
                     expect(opiskelijatiedot.luokkaTiedot().is(':visible')).to.equal(true)
                 }
             ))
-*/
-            it('Puuttuva hetu perusteella', function (done) {
-                exists(page.henkiloSearch)()
-                    .then(wait.forAngular)
-                    .then(function () {
-                        page.henkiloSearch().val('foobar').change()
-                    })
-                    .then(wait.forAngular)
-                    .then(function () {
-                        page.searchButton().click()
-                    })
-                    .then(wait.forAngular)
-                    .then(function () {
-                        expect(page.resultsTable().length).to.equal(0)
-                        expect(page.henkiloTiedot().is(':visible')).to.equal(false)
-                        expect(page.suoritusTiedot().is(':visible')).to.equal(false)
-                        expect(page.luokkaTiedot().is(':visible')).to.equal(false)
-                    }).then(function () {
-                        done()
-                    }, function (err) {
-                        done(err)
-                    })
-            })
-/*
+
             it('Puuttuva hetu perusteella - test.dsl', seqDone(
                 wait.forAngular,
                 input(opiskelijatiedot.henkiloSearch, 'foobar'),
@@ -163,7 +70,6 @@
                     expect(opiskelijatiedot.luokkaTiedot().is(':visible')).to.equal(false)
                 }
             ))
- */
         })
 
         describe('Muuta tietoja', function (done) {
