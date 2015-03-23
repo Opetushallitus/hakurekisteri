526aabf454b796bbc0a2013ea0cf00a4c0c314e7
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2015-03-23 15:32:41 +0200

    Oppilaiden etsiminen organisaatiosta elements visibility check added

--- web/src/main/webapp/test/spec/opiskelijatiedotPageSpec.js
+++ web/src/main/webapp/test/spec/opiskelijatiedotPageSpec.js
@@ -37,7 +37,7 @@
                 input(opiskelijatiedot.henkiloSearch, '1.2.246.562.24.71944845619'),
                 click(opiskelijatiedot.searchButton),
                 wait.forAngular,
-                function() {
+                function () {
                     expect(opiskelijatiedot.resultsTable().length).to.equal(1)
                     expect(opiskelijatiedot.henkiloTiedot().is(':visible')).to.equal(true)
                     expect(opiskelijatiedot.suoritusTiedot().is(':visible')).to.equal(true)
@@ -50,7 +50,7 @@
                 input(opiskelijatiedot.henkiloSearch, '123456-789'),
                 click(opiskelijatiedot.searchButton),
                 wait.forAngular,
-                function() {
+                function () {
                     expect(opiskelijatiedot.resultsTable().length).to.equal(1)
                     expect(opiskelijatiedot.henkiloTiedot().is(':visible')).to.equal(true)
                     expect(opiskelijatiedot.suoritusTiedot().is(':visible')).to.equal(true)
@@ -63,7 +63,7 @@
                 input(opiskelijatiedot.henkiloSearch, 'foobar'),
                 click(opiskelijatiedot.searchButton),
                 wait.forAngular,
-                function() {
+                function () {
                     expect(opiskelijatiedot.resultsTable().length).to.equal(0)
                     expect(opiskelijatiedot.henkiloTiedot().is(':visible')).to.equal(false)
                     expect(opiskelijatiedot.suoritusTiedot().is(':visible')).to.equal(false)
@@ -72,7 +72,14 @@
             ))
         })
 
-        describe('Muuta tietoja', function (done) {
+        describe('Oppilaiden valitseminen/etsiminen organisaatiossa', function () {
+
+            function areElementsVisible() {
+                return opiskelijatiedot.henkiloTiedot().is(':visible') &&
+                    opiskelijatiedot.suoritusTiedot().is(':visible') &&
+                    opiskelijatiedot.luokkaTiedot().is(':visible')
+            }
+
             it('Etsi organisaatiosta henkiloita - test.dsl', seqDone(
                 wait.forAngular,
                 autocomplete(opiskelijatiedot.organizationSearch, "Pik", opiskelijatiedot.organizationDropDownMenuChild(1)),
@@ -80,14 +87,29 @@
                 click(opiskelijatiedot.searchButton),
                 wait.forAngular,
                 click(opiskelijatiedot.resultsTableChild(1)),
+                function () {
+                    expect(areElementsVisible()).to.equal(true)
+                },
                 wait.forAngular,
                 click(opiskelijatiedot.resultsTableChild(2)),
+                function () {
+                    expect(areElementsVisible()).to.equal(true)
+                },
                 wait.forAngular,
                 click(opiskelijatiedot.resultsTableChild(3)),
+                function () {
+                    expect(areElementsVisible()).to.equal(true)
+                },
                 wait.forAngular,
                 click(opiskelijatiedot.resultsTableChild(4)),
+                function () {
+                    expect(areElementsVisible()).to.equal(true)
+                },
                 wait.forAngular,
-                click(opiskelijatiedot.resultsTableChild(5))
+                click(opiskelijatiedot.resultsTableChild(5)),
+                function () {
+                    expect(areElementsVisible()).to.equal(true)
+                }
             ))
         })
     })
