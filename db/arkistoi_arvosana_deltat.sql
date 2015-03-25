aa7f87881117b1aecc2c9a6063b8ec30478cfb72
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-03-25 10:20:43 +0200

    archive arvosana deltas

---
+++ db/arkistoi_arvosana_deltat.sql
@@ -0,0 +1,39 @@
+-- Function: arkistoi_arvosana_deltat()
+
+-- DROP FUNCTION arkistoi_arvosana_deltat();
+
+
+
+CREATE TABLE IF NOT EXISTS a_arvosana (LIKE arvosana INCLUDING ALL);
+
+ALTER TABLE a_arvosana OWNER TO oph;
+
+
+
+CREATE OR REPLACE FUNCTION arkistoi_arvosana_deltat()
+  RETURNS integer AS
+$BODY$
+DECLARE
+  _resource_id varchar(200);
+  _inserted bigint;
+  _count int := 0;
+  delta record;
+BEGIN
+  FOR delta IN
+    SELECT resource_id, inserted FROM arvosana
+    EXCEPT
+    SELECT resource_id, inserted FROM v_arvosana
+  LOOP
+    INSERT INTO a_arvosana SELECT * FROM arvosana WHERE resource_id = delta.resource_id AND inserted = delta.inserted;
+    DELETE FROM arvosana WHERE resource_id = delta.resource_id AND inserted = delta.inserted;
+    _count := _count + 1;
+    RAISE NOTICE '%: archived delta: %, %', _count, delta.resource_id, delta.inserted;
+  END LOOP;
+
+  RETURN _count;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+ALTER FUNCTION arkistoi_arvosana_deltat() OWNER TO oph;
