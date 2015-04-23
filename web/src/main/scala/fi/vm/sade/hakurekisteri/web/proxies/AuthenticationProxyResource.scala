0a2af2cc840b36649e1ce72f8f728b1c843f12c6
Juha Paananen <juha.paananen@gmail.com>
2015-04-23 16:43:58 +0300

    implement henkiloByOid

--- web/src/main/scala/fi/vm/sade/hakurekisteri/web/proxies/AuthenticationProxyResource.scala
+++ web/src/main/scala/fi/vm/sade/hakurekisteri/web/proxies/AuthenticationProxyResource.scala
@@ -20,8 +20,13 @@ class AuthenticationProxyResource(config: Config, system: ActorSystem) extends O
     "artifactId=authentication-service\nmocked"
   }
 
-  post("/resources/henkilo/henkilotByHenkiloOidList") {
+  get("/resources/henkilo/:oid") {
+    new AsyncResult() {
+      val is = proxy.henkiloByOid(params("oid"))
+    }
+  }
 
+  post("/resources/henkilo/henkilotByHenkiloOidList") {
     new AsyncResult() {
       val parsedBody = parse(request.body)
       val is = proxy.henkilotByOidList(parsedBody.extract[List[String]])
@@ -31,15 +36,20 @@ class AuthenticationProxyResource(config: Config, system: ActorSystem) extends O
 
 trait AuthenticationProxy {
   def henkilotByOidList(oidList: List[String]): Future[String]
+  def henkiloByOid(oid: String): Future[String]
 }
 
 class MockAuthenticationProxy extends AuthenticationProxy {
   lazy val data = Source.fromInputStream(getClass.getResourceAsStream("/proxy-mockdata/henkilot-by-oid-list.json")).mkString
   def henkilotByOidList(oidList: List[String]) = Future.successful(data)
+  def henkiloByOid(oid: String) = Future.successful("""{"id":109341,"etunimet":"aa","syntymaaika":"1958-10-12","passinnumero":null,"hetu":"123456-789","kutsumanimi":"aa","oidHenkilo":"1.2.246.562.24.71944845619","oppijanumero":null,"sukunimi":"AA","sukupuoli":"1","turvakielto":null,"henkiloTyyppi":"OPPIJA","eiSuomalaistaHetua":false,"passivoitu":false,"yksiloity":false,"yksiloityVTJ":true,"yksilointiYritetty":false,"duplicate":false,"created":null,"modified":null,"kasittelijaOid":null,"asiointiKieli":null,"aidinkieli":null,"huoltaja":null,"kayttajatiedot":null,"kielisyys":[],"kansalaisuus":[],"yhteystiedotRyhma":[]}""")
 }
 
 class HttpAuthenticationProxy(config: Config, system: ActorSystem) extends OphProxy(config, system, config.integrations.henkiloConfig, "authentication-proxy") with AuthenticationProxy {
   override def henkilotByOidList(oidList: List[String]) = {
     client.postObject[List[String], String]("/resources/henkilo/henkilotByHenkiloOidList", 200, oidList)
+  }
+  def henkiloByOid(oid: String) = {
+    client.readObject[String]("/resources/henkilo/" + oid, 200, 1)
   }
 }
