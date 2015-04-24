999a1e0749a73b139b6d9cb1ed7e3fe475dce9ad
Juha Paananen <juha.paananen@gmail.com>
2015-04-24 13:59:40 +0300

    replace for-comprehension with map and partial function

--- web/src/main/scala/fi/vm/sade/hakurekisteri/web/rest/support/SecuritySupport.scala
+++ web/src/main/scala/fi/vm/sade/hakurekisteri/web/rest/support/SecuritySupport.scala
@@ -9,19 +9,15 @@ import org.springframework.security.core.{Authentication, GrantedAuthority}
 
 trait SecuritySupport {
   def currentUser(implicit request: HttpServletRequest): Option[User]
-
 }
 
 trait SpringSecuritySupport extends SecuritySupport {
   import scala.collection.JavaConverters._
 
-  def currentUser(implicit request: HttpServletRequest): Option[User] =  for(
-    user: Principal <- userPrincipal
-  ) yield user match {
-      case a: Authentication => OPHUser(username(a), authorities(a).toSet)
-      case u: Principal => OPHUser(username(u), Set())
-    }
-
+  def currentUser(implicit request: HttpServletRequest): Option[User] = userPrincipal.map {
+    case a: Authentication => OPHUser(username(a), authorities(a).toSet)
+    case u: Principal => OPHUser(username(u), Set())
+  }
 
   def username(u: Principal): String = {
     Option(u.getName).getOrElse("anonymous")
