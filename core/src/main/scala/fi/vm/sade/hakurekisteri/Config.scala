190929ab7e0e47238c8ea2d0f4181afd24022bce
Juha Paananen <juha.paananen@gmail.com>
2015-04-24 15:35:56 +0300

    resolve NullPointerIssue related to vals

--- core/src/main/scala/fi/vm/sade/hakurekisteri/Config.scala
+++ core/src/main/scala/fi/vm/sade/hakurekisteri/Config.scala
@@ -42,20 +42,20 @@ class Oids(properties: Map[String, String] = Map.empty) {
 }
 
 class DefaultConfig extends Config {
-  val mockMode = false
-  private val homeDir = sys.props.getOrElse("user.home", "")
+  def mockMode = false
+  private lazy val homeDir = sys.props.getOrElse("user.home", "")
   lazy val ophConfDir: Path = Paths.get(homeDir, "/oph-configuration/")
 }
 
 class MockConfig extends Config {
-  val mockMode = true
+  def mockMode = true
   override val importBatchProcessingInitialDelay = 1.seconds
   override val h2 = true
   lazy val ophConfDir = Paths.get(ProjectRootFinder.findProjectRoot().getAbsolutePath, "web/src/test/resources/oph-configuration")
 }
 
 abstract class Config {
-  val mockMode: Boolean
+  def mockMode: Boolean
   val h2 = false
 
   val log = LoggerFactory.getLogger(getClass)
