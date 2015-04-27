c426c7032ba3c790e125563edec2a6ed804975a1
Juha Paananen <juha.paananen@gmail.com>
2015-04-27 13:50:14 +0300

    introduce SharedJetty

--- web/src/test/scala/fi/vm/sade/hakurekisteri/HakuRekisteriJetty.scala
+++ web/src/test/scala/fi/vm/sade/hakurekisteri/HakuRekisteriJetty.scala
@@ -1,15 +1,24 @@
 package fi.vm.sade.hakurekisteri
 
+import java.io.IOException
+import java.net.Socket
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.util.resource.ResourceCollection
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
+import scala.util.Random
 
 object HakuRekisteriJetty extends App {
   new HakuRekisteriJetty(8080).start
 }
 
-class HakuRekisteriJetty(port: Int, config: Config = Config.globalConfig) {
+object SharedJetty {
+  private lazy val jetty = new HakuRekisteriJetty(config = Config.mockConfig)
+  def start = jetty.start
+  def port = jetty.port
+}
+
+class HakuRekisteriJetty(val port: Int = PortFinder.findFreeLocalPort, config: Config = Config.globalConfig) {
   val root = ProjectRootFinder.findProjectRoot()
   val contextPath = "/"
   val server = new Server(port)
@@ -35,6 +44,29 @@ class HakuRekisteriJetty(port: Int, config: Config = Config.globalConfig) {
       block
     } finally {
       server.stop
+    }
+  }
+
+}
+
+object PortFinder {
+  def findFreeLocalPort: Int = {
+    val range = 1024 to 60000
+    val port = ((range(new Random().nextInt(range length))))
+    if (isFreeLocalPort(port)) {
+      port
+    } else {
+      findFreeLocalPort
+    }
+  }
+
+  def isFreeLocalPort(port: Int): Boolean = {
+    try {
+      val socket = new Socket("127.0.0.1", port)
+      socket.close()
+      false
+    } catch {
+      case e: IOException => true
     }
   }
 }
--- web/src/test/scala/fi/vm/sade/hakurekisteri/mocha/HakureRekisteriMochaTest.scala
+++ web/src/test/scala/fi/vm/sade/hakurekisteri/mocha/HakureRekisteriMochaTest.scala
@@ -1,43 +1,16 @@
 package fi.vm.sade.hakurekisteri.mocha
 
-import java.io.IOException
-import java.net.Socket
-
-import fi.vm.sade.hakurekisteri.{Config, HakuRekisteriJetty}
+import fi.vm.sade.hakurekisteri.SharedJetty
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.language.postfixOps
 import scala.sys.process._
-import scala.util.Random
 
 class HakureRekisteriMochaTest extends FlatSpec with Matchers {
-  def isFreeLocalPort(port: Int): Boolean = {
-    try {
-      val socket = new Socket("127.0.0.1", port)
-      socket.close()
-      false
-    } catch {
-      case e: IOException => true
-    }
-  }
-
-  def findFreeLocalPort: Int = {
-    val range = 1024 to 60000
-    val port = ((range(new Random().nextInt(range length))))
-    if (isFreeLocalPort(port)) {
-      port
-    } else {
-      findFreeLocalPort
-    }
-  }
-
   "Mocha tests" should "pass" in {
-    val port: Int = findFreeLocalPort
-    new HakuRekisteriJetty(port, Config.mockConfig).withJetty {
-      val pb = Seq("node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:" + port + "/test/runner.html")
-      val res = pb.!
-      res should be(0)
-    }
+    SharedJetty.start
+    val pb = Seq("node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:" + SharedJetty.port + "/test/runner.html")
+    val res = pb.!
+    res should be(0)
   }
-
 }
