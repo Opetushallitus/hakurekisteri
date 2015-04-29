010e526ba462e0dcdce7581fe1e43920def3cc03
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2015-04-29 12:43:12 +0300

    Mock files organized to directories

--- core/src/main/scala/fi/vm/sade/hakurekisteri/integration/mocks/HenkiloMock.scala
+++ core/src/main/scala/fi/vm/sade/hakurekisteri/integration/mocks/HenkiloMock.scala
@@ -10,22 +10,22 @@ object HenkiloMock {
   }
 
   def henkilotByHenkiloOidList(oids: List[String]): String = {
-    if (oids.contains("1.2.246.562.24.71944845619") && oids.size == 1) getResourceJson("/mock-data/henkilo-aarne-oidList.json")
-    else if (oids.contains("1.2.246.562.24.49719248091") && oids.size == 5) getResourceJson("/mock-data/henkilo-all-oidList.json")
+    if (oids.contains("1.2.246.562.24.71944845619") && oids.size == 1) getResourceJson("/mock-data/henkilo/henkilo-aarne-oidList.json")
+    else if (oids.contains("1.2.246.562.24.49719248091") && oids.size == 5) getResourceJson("/mock-data/henkilo/henkilo-all-oidList.json")
     else "[]"
   }
 
   def getHenkiloByQParam(q: String): String = {
-    if (q.equals("123456-789")) getResourceJson("/mock-data/henkilo-aarne-hetuhaku.json")
-    else if (q.equals("1.2.246.562.24.71944845619")) getResourceJson("/mock-data/henkilo-aarne-hetuhaku.json")
-    else if (q.equals("1.2.246.562.24.98743797763")) getResourceJson("/mock-data/henkilo-tyyne.json")
-    else getResourceJson("/mock-data/henkilo-empty.json")
+    if (q.equals("123456-789")) getResourceJson("/mock-data/henkilo/henkilo-aarne-hetuhaku.json")
+    else if (q.equals("1.2.246.562.24.71944845619")) getResourceJson("/mock-data/henkilo/henkilo-aarne-hetuhaku.json")
+    else if (q.equals("1.2.246.562.24.98743797763")) getResourceJson("/mock-data/henkilo/henkilo-tyyne.json")
+    else getResourceJson("/mock-data/henkilo/henkilo-empty.json")
   }
 
   def getHenkiloByOid(oid: String): String = {
-    if (oid.equals("1.2.246.562.24.71944845619")) getResourceJson("/mock-data/henkilo-aarne.json")
-    else if (oid.equals("1.2.246.562.24.98743797763")) getResourceJson("/mock-data/henkilo-tyyne.json")
-    else getResourceJson("/mock-data/henkilo-empty.json")
+    if (oid.equals("1.2.246.562.24.71944845619")) getResourceJson("/mock-data/henkilo/henkilo-aarne.json")
+    else if (oid.equals("1.2.246.562.24.98743797763")) getResourceJson("/mock-data/henkilo/henkilo-tyyne.json")
+    else getResourceJson("/mock-data/henkilo/henkilo-empty.json")
   }
 
 }
--- core/src/main/scala/fi/vm/sade/hakurekisteri/integration/mocks/OrganisaatioMock.scala
+++ core/src/main/scala/fi/vm/sade/hakurekisteri/integration/mocks/OrganisaatioMock.scala
@@ -5,16 +5,16 @@ import fi.vm.sade.hakurekisteri.integration.mocks.HenkiloMock._
 object OrganisaatioMock {
 
   def findAll(): String = {
-    getResourceJson("/mock-data/organisaatio-all.json")
+    getResourceJson("/mock-data/organisaatio/organisaatio-all.json")
   }
 
   def findByOid(tunniste: String): String = {
     if (tunniste.equals("06345") || tunniste.equals("1.2.246.562.10.39644336305")) {
-      getResourceJson("/mock-data/organisaatio-pikkarala.json")
+      getResourceJson("/mock-data/organisaatio/organisaatio-pikkarala.json")
     } else if (tunniste.equals("05127") || tunniste.equals("1.2.246.562.10.16546622305")) {
-      getResourceJson("/mock-data/organisaatio-pikkola.json")
+      getResourceJson("/mock-data/organisaatio/organisaatio-pikkola.json")
     } else {
-      getResourceJson("/mock-data/organisaatio-foobar.json")
+      getResourceJson("/mock-data/organisaatio/organisaatio-foobar.json")
     }
   }
 }
