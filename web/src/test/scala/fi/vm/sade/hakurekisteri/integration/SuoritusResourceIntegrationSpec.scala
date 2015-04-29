360c147094adde7e7a1ec2658fc2d35a98586c91
Jani Rahkola <jani.rahkola@reaktor.fi>
2015-04-29 10:30:16 +0300

    Use the SharedJetty helper trait in specs

--- web/src/test/scala/fi/vm/sade/hakurekisteri/integration/SuoritusResourceIntegrationSpec.scala
+++ web/src/test/scala/fi/vm/sade/hakurekisteri/integration/SuoritusResourceIntegrationSpec.scala
@@ -1,15 +1,10 @@
 package fi.vm.sade.hakurekisteri.integration
 
-import fi.vm.sade.hakurekisteri.{SharedJetty, Config, HakuRekisteriJetty}
+import fi.vm.sade.hakurekisteri._
 import org.scalatest.{Matchers, FlatSpec}
-import org.scalatra.test.HttpComponentsClient
 
-class SuoritusResourceIntegrationSpec extends FlatSpec with Matchers with HttpComponentsClient {
-  val port = SharedJetty.port
-  val baseUrl = s"http://localhost:${port}"
-
+class SuoritusResourceIntegrationSpec extends FlatSpec with CleanSharedJettyBeforeEach with Matchers {
   it should "asdf" in {
-    SharedJetty.start
     get("/") {
       response.status should be(200)
     }
--- web/src/test/scala/fi/vm/sade/hakurekisteri/mocha/HakureRekisteriMochaTest.scala
+++ web/src/test/scala/fi/vm/sade/hakurekisteri/mocha/HakureRekisteriMochaTest.scala
@@ -1,14 +1,13 @@
 package fi.vm.sade.hakurekisteri.mocha
 
-import fi.vm.sade.hakurekisteri.SharedJetty
+import fi.vm.sade.hakurekisteri.{CleanSharedJetty, SharedJetty}
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.language.postfixOps
 import scala.sys.process._
 
-class HakureRekisteriMochaTest extends FlatSpec with Matchers {
+class HakureRekisteriMochaTest extends FlatSpec with CleanSharedJetty with Matchers {
   "Mocha tests" should "pass" in {
-    SharedJetty.start
     val pb = Seq("node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:" + SharedJetty.port + "/test/runner.html")
     val res = pb.!
     res should be(0)
