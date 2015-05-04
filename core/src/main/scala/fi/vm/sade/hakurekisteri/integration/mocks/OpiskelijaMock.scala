d0e0a6d2c099d466436e6ef85df8c29f96765d2f
Jani Rahkola <jani.rahkola@reaktor.fi>
2015-05-04 14:32:36 +0300

    OpiskelijaMock

---
+++ core/src/main/scala/fi/vm/sade/hakurekisteri/integration/mocks/OpiskelijaMock.scala
@@ -0,0 +1,16 @@
+package fi.vm.sade.hakurekisteri.integration.mocks
+
+
+object OpiskelijaMock {
+  def getResourceJson(filename: String): String = {
+    scala.io.Source.fromInputStream(getClass.getResourceAsStream(filename))
+      .getLines()
+      .mkString
+  }
+
+  def getOpiskelijaByOid(henkiloOid: String): String = {
+    Map(
+      "1.2.246.562.24.71944845619" -> getResourceJson("/mock-data/opiskelija/opiskelija-aarne.json"),
+      "1.2.246.562.24.98743797763" -> getResourceJson("/mock-data/opiskelija/opiskelija-tyyne.json"))(henkiloOid)
+  }
+}
