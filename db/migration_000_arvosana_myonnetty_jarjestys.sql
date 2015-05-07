cddab2afc8b6451fa1158eef3c25048d054fcf5b
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-05-07 08:34:50 +0300

    db migration for arvosana required fields

--- db/migration_000_arvosana_myonnetty_jarjestys.sql
+++ db/migration_000_arvosana_myonnetty_jarjestys.sql
@@ -1,6 +1,19 @@
+BEGIN TRANSACTION;
+
 INSERT INTO arvosana (resource_id, inserted, deleted, suoritus, arvosana, asteikko, aine, lisatieto, valinnainen, pisteet, myonnetty, source, jarjestys)
   SELECT a.resource_id, floor(extract(epoch from now()) * 1000), false, a.suoritus, a.arvosana, a.asteikko, a.aine, a.lisatieto, a.valinnainen, a.pisteet, s.valmistuminen, a.source, a.jarjestys
     FROM v_arvosana a
       JOIN v_suoritus s ON a.suoritus = s.resource_id
     WHERE a.asteikko IN ('YO', 'OSAKOE')
       AND a.myonnetty IS NULL;
+
+END TRANSACTION;
+
+BEGIN TRANSACTION;
+
+UPDATE arvosana SET jarjestys = 0
+WHERE asteikko = '4-10'
+  AND valinnainen = true
+  AND jarjestys IS NULL;
+
+END TRANSACTION;
