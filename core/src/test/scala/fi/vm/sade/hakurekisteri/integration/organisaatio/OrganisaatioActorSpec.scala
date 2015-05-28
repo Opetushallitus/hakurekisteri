1cba1d68db0b84fe323ac56d89b168e0a59a2abf
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-05-28 13:24:29 +0300

    refactor organisaatio actor tests

--- core/src/test/scala/fi/vm/sade/hakurekisteri/integration/organisaatio/OrganisaatioActorSpec.scala
+++ core/src/test/scala/fi/vm/sade/hakurekisteri/integration/organisaatio/OrganisaatioActorSpec.scala
@@ -1,6 +1,6 @@
 package fi.vm.sade.hakurekisteri.integration.organisaatio
 
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{ActorRef, Props, ActorSystem}
 import akka.pattern.ask
 import akka.util.Timeout
 import com.ning.http.client.AsyncHttpClient
@@ -12,94 +12,132 @@ import org.scalatest.concurrent.AsyncAssertions
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.time.{Millis, Span}
 import org.scalatra.test.scalatest.ScalatraFunSuite
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 
 class OrganisaatioActorSpec extends ScalatraFunSuite with Matchers with AsyncAssertions with MockitoSugar with DispatchSupport {
 
-  implicit val system = ActorSystem("organisaatioactor-test-system")
-  implicit val ec = system.dispatcher
   implicit val timeout: Timeout = 60.seconds
-
-  val endPoint = mock[Endpoint]
-
-  when(endPoint.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))).thenReturn((200, List(), OrganisaatioResults.all))
-  when(endPoint.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/00004"))).thenReturn((200, List(), OrganisaatioResults.alppila))
-  when(endPoint.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/05127"))).thenReturn((200, List(), OrganisaatioResults.pikkola))
-  when(endPoint.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/1.2.246.562.10.16546622305"))).thenReturn((200, List(), OrganisaatioResults.pikkola))
-
   val organisaatioConfig = ServiceConfig(serviceUrl = "http://localhost/organisaatio-service")
 
-  val organisaatioActor = system.actorOf(Props(new HttpOrganisaatioActor(new VirkailijaRestClient(config = organisaatioConfig, aClient = Some(new AsyncHttpClient(new CapturingProvider(endPoint)))), Config.mockConfig)))
+  private def withSystem(f: ActorSystem => Unit) = {
+    val system = ActorSystem("test-organisaatioactor-system")
+
+    f(system)
+
+    system.shutdown()
+    system.awaitTermination(15.seconds)
+  }
+
+  def createEndPoint = {
+    val e = mock[Endpoint]
+
+    when(e.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))).thenReturn((200, List(), OrganisaatioResults.hae))
+    when(e.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/99999"))).thenReturn((200, List(), OrganisaatioResults.ysiysiysiysiysi))
+    when(e.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/05127"))).thenReturn((200, List(), OrganisaatioResults.pikkola))
+    when(e.request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/1.2.246.562.10.16546622305"))).thenReturn((200, List(), OrganisaatioResults.pikkola))
+
+    e
+  }
+
+
 
   test("OrganisaatioActor should contain all organisaatios after startup") {
-    Thread.sleep(1000)
+    withSystem(
+      implicit system => {
+        implicit val ec = system.dispatcher
+        val (endPoint, organisaatioActor) = initOrganisaatioActor()
 
-    waitFuture((organisaatioActor ? Oppilaitos("05127")).mapTo[OppilaitosResponse])(o => {
-      o.oppilaitos.oppilaitosKoodi.get should be ("05127")
-    })
+        waitFuture((organisaatioActor ? Oppilaitos("05127")).mapTo[OppilaitosResponse])(o => {
+          o.oppilaitos.oppilaitosKoodi.get should be ("05127")
+        })
 
-    verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
-    verify(endPoint, times(0)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/05127"))
+        verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
+        verify(endPoint, times(0)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/05127"))
+      }
+    )
   }
 
   test("OrganisaatioActor should return organisaatio from cache using organisaatio oid") {
-    Thread.sleep(1000)
+    withSystem(
+      implicit system => {
+        implicit val ec = system.dispatcher
+        val (endPoint, organisaatioActor) = initOrganisaatioActor()
 
-    waitFuture((organisaatioActor ? "1.2.246.562.10.16546622305").mapTo[Option[Organisaatio]])(o => {
-      o.get.oppilaitosKoodi.get should be ("05127")
-    })
+        waitFuture((organisaatioActor ? "1.2.246.562.10.16546622305").mapTo[Option[Organisaatio]])(o => {
+          o.get.oppilaitosKoodi.get should be ("05127")
+        })
 
-    verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
-    verify(endPoint, times(0)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/1.2.246.562.10.16546622305"))
+        verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
+        verify(endPoint, times(0)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/1.2.246.562.10.16546622305"))
+      }
+    )
   }
 
   test("OrganisaatioActor should return organisaatio from cache using oppilaitoskoodi") {
-    Thread.sleep(1000)
+    withSystem(
+      implicit system => {
+        implicit val ec = system.dispatcher
+        val (endPoint, organisaatioActor) = initOrganisaatioActor()
 
-    waitFuture((organisaatioActor ? Oppilaitos("05127")).mapTo[OppilaitosResponse])(o => {
-      o.oppilaitos.oppilaitosKoodi.get should be ("05127")
-    })
+        waitFuture((organisaatioActor ? Oppilaitos("05127")).mapTo[OppilaitosResponse])(o => {
+          o.oppilaitos.oppilaitosKoodi.get should be ("05127")
+        })
 
-    verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
-    verify(endPoint, times(0)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/05127"))
+        verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
+        verify(endPoint, times(0)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/05127"))
+      }
+    )
   }
 
   test("OrganisaatioActor should find organisaatio from organisaatio-service if not found in cache") {
-    Thread.sleep(1000)
+    withSystem(
+      implicit system => {
+        implicit val ec = system.dispatcher
+        val (endPoint, organisaatioActor) = initOrganisaatioActor()
 
-    waitFuture((organisaatioActor ? Oppilaitos("00004")).mapTo[OppilaitosResponse])(o => {
-      o.oppilaitos.oppilaitosKoodi.get should be ("00004")
-    })
+        waitFuture((organisaatioActor ? Oppilaitos("99999")).mapTo[OppilaitosResponse])(o => {
+          o.oppilaitos.oppilaitosKoodi.get should be ("99999")
+        })
 
-    verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
-    verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/00004"))
+        verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
+        verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/99999"))
+      }
+    )
   }
 
   test("OrganisaatioActor should cache a single result") {
-    Thread.sleep(1000)
+    withSystem(
+      implicit system => {
+        implicit val ec = system.dispatcher
+        val (endPoint, organisaatioActor) = initOrganisaatioActor()
 
-    waitFuture((organisaatioActor ? Oppilaitos("00004")).mapTo[OppilaitosResponse])(o => {
-      o.oppilaitos.oppilaitosKoodi.get should be ("00004")
-    })
+        waitFuture((organisaatioActor ? Oppilaitos("99999")).mapTo[OppilaitosResponse])(o => {
+          o.oppilaitos.oppilaitosKoodi.get should be ("99999")
+        })
 
-    Thread.sleep(1000)
+        Thread.sleep(200)
 
-    waitFuture((organisaatioActor ? Oppilaitos("00004")).mapTo[OppilaitosResponse])(o => {
-      o.oppilaitos.oppilaitosKoodi.get should be ("00004")
-    })
+        waitFuture((organisaatioActor ? Oppilaitos("99999")).mapTo[OppilaitosResponse])(o => {
+          o.oppilaitos.oppilaitosKoodi.get should be ("99999")
+        })
 
-    verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
-    verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/00004"))
+        verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/v2/hierarkia/hae?aktiiviset=true&lakkautetut=false&suunnitellut=true"))
+        verify(endPoint, times(1)).request(forUrl("http://localhost/organisaatio-service/rest/organisaatio/99999"))
+      }
+    )
   }
 
-  override def stop(): Unit = {
-    system.shutdown()
-    system.awaitTermination(15.seconds)
-    super.stop()
+  def initOrganisaatioActor()(implicit system: ActorSystem, ec: ExecutionContext): (Endpoint, ActorRef) = {
+    val endPoint = createEndPoint
+    val organisaatioActor = system.actorOf(Props(new HttpOrganisaatioActor(new VirkailijaRestClient(config = organisaatioConfig, aClient = Some(new AsyncHttpClient(new CapturingProvider(endPoint)))), Config.mockConfig)))
+
+    Thread.sleep(2000)
+
+    (endPoint, organisaatioActor)
   }
 
-  def waitFuture[A](f: Future[A])(assertion: A => Unit) = {
+  def waitFuture[A](f: Future[A])(assertion: A => Unit)(implicit ec: ExecutionContext) = {
     val w = new Waiter
 
     f.onComplete(r => {
@@ -113,7 +151,7 @@ class OrganisaatioActorSpec extends ScalatraFunSuite with Matchers with AsyncAss
 }
 
 object OrganisaatioResults {
-  val all = scala.io.Source.fromURL(getClass.getResource("/mock-data/organisaatio/organisaatio-all.json")).mkString
+  val hae = scala.io.Source.fromURL(getClass.getResource("/mock-data/organisaatio/organisaatio-hae.json")).mkString
   val pikkola = scala.io.Source.fromURL(getClass.getResource("/mock-data/organisaatio/organisaatio-pikkola.json")).mkString
-  val alppila = scala.io.Source.fromURL(getClass.getResource("/mock-data/organisaatio/organisaatio-alppilanlukio.json")).mkString
+  val ysiysiysiysiysi = scala.io.Source.fromURL(getClass.getResource("/mock-data/organisaatio/organisaatio-99999.json")).mkString
 }
