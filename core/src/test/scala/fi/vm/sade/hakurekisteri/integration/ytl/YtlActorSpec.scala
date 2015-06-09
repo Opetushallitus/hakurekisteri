a95f960f558069297652e969036deaaf44ade71a
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-06-09 13:51:06 +0300

    adjust test timing

--- core/src/test/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlActorSpec.scala
+++ core/src/test/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlActorSpec.scala
@@ -35,7 +35,7 @@ class YtlActorSpec extends ScalatraFunSuite {
   val arvosanaJournal = new JDBCJournal[Arvosana, UUID, ArvosanaTable](TableQuery[ArvosanaTable])
   val arvosanaActor = system.actorOf(Props(new ArvosanaActor(journal = arvosanaJournal)), "arvosanat")
   val hakemusActor = system.actorOf(Props(new DummyActor()))
-  val actor = system.actorOf(Props(new YtlActor(henkiloActor, suoritusActor, arvosanaActor, hakemusActor, Some(YTLConfig("","","","","",List(),"")))), "ytl")
+  val actor = system.actorOf(Props(new YtlActor(henkiloActor, suoritusActor, arvosanaActor, hakemusActor, Some(YTLConfig("", "", "", "", "", List(), "")))), "ytl")
 
   private def waitForSuoritus(henkilo: String): Future[Suoritus with Identified[UUID]] = {
     Future {
@@ -43,7 +43,7 @@ class YtlActorSpec extends ScalatraFunSuite {
       var results: Seq[Suoritus with Identified[UUID]] = List()
       while(results.isEmpty) {
         Thread.sleep(100)
-        results = Await.result((suoritusActor ? suoritusQ).mapTo[Seq[Suoritus with Identified[UUID]]], 15.seconds)
+        results = Await.result((suoritusActor ? suoritusQ).mapTo[Seq[Suoritus with Identified[UUID]]], 60.seconds)
       }
       results.head
     }
@@ -51,12 +51,12 @@ class YtlActorSpec extends ScalatraFunSuite {
 
   private def waitForArvosanat(len: Int = 27): Future[Seq[Arvosana with Identified[UUID]]] = {
     Future {
-      val suoritus = Await.result(waitForSuoritus("1.2.246.562.24.71944845619"), 15.seconds)
+      val suoritus = Await.result(waitForSuoritus("1.2.246.562.24.71944845619"), 60.seconds)
       val arvosanatQ = ArvosanaQuery(Some(suoritus.id))
       var results: Seq[Arvosana with Identified[UUID]] = List()
       while(results.length < len) {
         Thread.sleep(100)
-        results = Await.result((arvosanaActor ? arvosanatQ).mapTo[Seq[Arvosana with Identified[UUID]]], 15.seconds)
+        results = Await.result((arvosanaActor ? arvosanatQ).mapTo[Seq[Arvosana with Identified[UUID]]], 60.seconds)
       }
       results
     }
@@ -65,7 +65,7 @@ class YtlActorSpec extends ScalatraFunSuite {
   test("YtlActor should insert arvosanat to database with koetunnus and aineyhdistelmarooli fields") {
     actor ! YtlResult(UUID.randomUUID(), getClass.getResource("/ytl-osakoe-test.xml").getFile)
 
-    val arvosanat = Await.result(waitForArvosanat(), 15.seconds)
+    val arvosanat = Await.result(waitForArvosanat(), 60.seconds)
     arvosanat.length should equal (27)
     val arvosanaSA = arvosanat.filter(arvosana => {
       arvosana.aine.equals("A") && arvosana.lahdeArvot.get("koetunnus").contains("SA")
@@ -73,27 +73,27 @@ class YtlActorSpec extends ScalatraFunSuite {
     arvosanaSA.length should equal (1)
     arvosanaSA.head.lahdeArvot.get("aineyhdistelmarooli") should equal (Some("61"))
 
-    Await.result(suoritusActor ? DeleteResource(arvosanaSA.head.suoritus, "test"), 15.seconds)
+    Await.result(suoritusActor ? DeleteResource(arvosanaSA.head.suoritus, "test"), 60.seconds)
   }
 
   test("YtlActor should deduplicate kokeet properly") {
     actor ! YtlResult(UUID.randomUUID(), getClass.getResource("/ytl-koe-test.xml").getFile)
 
-    val arvosanat = Await.result(waitForArvosanat(len = 17), 15.seconds)
+    val arvosanat = Await.result(waitForArvosanat(len = 17), 60.seconds)
     arvosanat.length should be (17)
 
     actor ! YtlResult(UUID.randomUUID(), getClass.getResource("/ytl-koe-test2.xml").getFile)
 
-    val arvosanat2 = Await.result(waitForArvosanat(len = 19), 15.seconds)
+    val arvosanat2 = Await.result(waitForArvosanat(len = 19), 60.seconds)
     arvosanat2.length should be (19)
 
-    Await.result(suoritusActor ? DeleteResource(arvosanat.head.suoritus, "test"), 15.seconds)
+    Await.result(suoritusActor ? DeleteResource(arvosanat.head.suoritus, "test"), 60.seconds)
   }
 
   override def stop(): Unit = {
     RunScript.execute("jdbc:h2:file:data/ytl-integration-test", "", "", "classpath:clear-h2.sql", Charset.forName("UTF-8"), false)
     system.shutdown()
-    system.awaitTermination(60.seconds)
+    system.awaitTermination(15.seconds)
     super.stop()
   }
 
