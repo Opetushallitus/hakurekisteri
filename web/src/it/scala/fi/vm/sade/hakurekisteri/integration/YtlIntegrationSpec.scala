1ce44685c2abf1e26612387304a0dc058fb7165a
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-06-09 14:37:49 +0300

    fix integration test

--- web/src/it/scala/fi/vm/sade/hakurekisteri/integration/YtlIntegrationSpec.scala
+++ web/src/it/scala/fi/vm/sade/hakurekisteri/integration/YtlIntegrationSpec.scala
@@ -17,7 +17,7 @@ import scala.concurrent.{Await, Future}
 
 class YtlIntegrationSpec extends FlatSpec with Matchers with CleanSharedJetty {
   val expectedSuoritus = VirallinenSuoritus(
-    henkilo = "123456-789",
+    henkilo = "1.2.246.562.24.71944845619",
     lahde = "1.2.246.562.10.43628088406",
     vahv = true,
     komo = "1.2.246.562.5.2013061010184237348007",
@@ -54,7 +54,7 @@ class YtlIntegrationSpec extends FlatSpec with Matchers with CleanSharedJetty {
       var result: Option[Seq[Todistus]] = None
       while (result.isEmpty) {
         Thread.sleep(50)
-        val check = get("/rest/v1/oppijat/123456-789") { response }
+        val check = get("/rest/v1/oppijat/1.2.246.562.24.71944845619") { response }
         if (check.status == 200) {
           val json = JsonMethods.parse(check.body)
           if((json \\ "arvosanat").children.nonEmpty) {
