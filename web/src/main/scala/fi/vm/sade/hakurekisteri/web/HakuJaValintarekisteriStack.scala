1551ca0c36b80449fe6b9c1e114333314ed6cddb
Mikko Apo <mikko.apo@reaktor.fi>
2016-03-03 16:31:43 +0200

    removed CorsSupport altogether. It does not filter simple cors requests at all.

--- web/src/main/scala/fi/vm/sade/hakurekisteri/web/HakuJaValintarekisteriStack.scala
+++ web/src/main/scala/fi/vm/sade/hakurekisteri/web/HakuJaValintarekisteriStack.scala
@@ -2,18 +2,11 @@ package fi.vm.sade.hakurekisteri.web
 
 import akka.event.LoggingAdapter
 import fi.vm.sade.auditlog.{ApplicationType, Audit}
-import fi.vm.sade.hakurekisteri.integration.OphUrlProperties
 import fi.vm.sade.hakurekisteri.web.rest.support.IncidentReporting
 import org.scalatra._
 
-trait HakuJaValintarekisteriStack extends ScalatraServlet with IncidentReporting with CorsSupport {
+trait HakuJaValintarekisteriStack extends ScalatraServlet with IncidentReporting {
 
   val logger: LoggingAdapter
   val audit = new Audit("hakurekisteri", ApplicationType.VIRKAILIJA)
-
-  if("DEVELOPMENT" == OphUrlProperties.ophProperties.getProperty("common.corsfilter.mode")) {
-    options("/*") {
-      response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"))
-    }
-  }
 }
