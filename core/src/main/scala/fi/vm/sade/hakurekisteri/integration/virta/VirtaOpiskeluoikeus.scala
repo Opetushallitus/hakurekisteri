42f1984bb779dce41aaa1da530f92293c6a4fc95
tomikat <tomi.katajanheimo@gmail.com>
2016-04-01 17:10:01 +0300

    Add support for virta integration api version 1.06

--- core/src/main/scala/fi/vm/sade/hakurekisteri/integration/virta/VirtaOpiskeluoikeus.scala
+++ core/src/main/scala/fi/vm/sade/hakurekisteri/integration/virta/VirtaOpiskeluoikeus.scala
@@ -6,6 +6,4 @@ case class VirtaOpiskeluoikeus(alkuPvm: LocalDate,
                                loppuPvm: Option[LocalDate],
                                myontaja: String,
                                koulutuskoodit: Seq[String],
-                               opintoala1995: Option[String],
-                               koulutusala2002: Option[String],
                                kieli: String)
--- core/src/main/scala/fi/vm/sade/hakurekisteri/integration/virta/VirtaTutkinto.scala
+++ core/src/main/scala/fi/vm/sade/hakurekisteri/integration/virta/VirtaTutkinto.scala
@@ -4,7 +4,5 @@ import org.joda.time.LocalDate
 
 case class VirtaTutkinto(suoritusPvm: LocalDate,
                          koulutuskoodi: Option[String],
-                         opintoala1995: Option[String],
-                         koulutusala2002: Option[String],
                          myontaja: String,
                          kieli: String)
