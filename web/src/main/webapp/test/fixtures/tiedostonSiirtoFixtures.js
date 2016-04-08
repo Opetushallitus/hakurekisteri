7e1b25782f181e540025cb156a82e0e1c680c67b
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2016-04-08 10:17:53 +0300

    Remove console.log

--- web/src/main/webapp/test/fixtures/tiedostonSiirtoFixtures.js
+++ web/src/main/webapp/test/fixtures/tiedostonSiirtoFixtures.js
@@ -7,14 +7,12 @@ function tiedostonSiirtoFixtures() {
     return {
         perustiedotOpen: function (isOpen) {
             return function () {
-                console.log('perustiedotOpen='+JSON.stringify({"open": isOpen}))
                 mockHttpBackEnd().when('GET', /.*rest\/v2\/siirto\/perustiedot\/isopen$/).respond({"open": isOpen})
             }
         },
 
         arvosanatOpen: function (isOpen) {
             return function () {
-                console.log('arvosanatOpen='+JSON.stringify({"open": isOpen}))
                 mockHttpBackEnd().when('GET', /.*rest\/v1\/siirto\/arvosanat\/isopen$/).respond({"open": isOpen})
             }
         }
