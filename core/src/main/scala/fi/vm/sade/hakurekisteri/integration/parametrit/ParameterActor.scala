9f846a366b4524f792de4371410ed4eeb320aeaa
Harri Hämäläinen <hhamalai@iki.fi>
2016-05-04 16:21:49 +0300

    add possibility to query restriction parameters from ohjausparametrit

--- core/src/main/scala/fi/vm/sade/hakurekisteri/integration/parametrit/ParameterActor.scala
+++ core/src/main/scala/fi/vm/sade/hakurekisteri/integration/parametrit/ParameterActor.scala
@@ -13,9 +13,15 @@ import scala.concurrent.duration._
 case class ParamsFailedException(haku: String, from: ActorRef, t: Throwable) extends Exception(s"call to parameter service failed for haku $haku", t)
 case class NoParamFoundException(haku: String) extends Exception(s"no parameter found for haku $haku")
 
+object ParameterActor {
+  val opoUpdateGraduation = "opoUpdateGraduation"
+}
+
 abstract class ParameterActor extends Actor with ActorLogging {
   implicit val ec = context.dispatcher
   private val tiedonsiirtoSendingPeriodCache = new FutureCache[String, Boolean](2.minute.toMillis)
+  protected val HTTP_OK = 200
+
   object ProcessNext
 
   override def receive: Actor.Receive = {
@@ -24,6 +30,10 @@ abstract class ParameterActor extends Actor with ActorLogging {
 
     case IsSendingEnabled(key) =>
       isSendingEnabled(key) pipeTo sender
+
+      case IsRestrictionActive(restriction) =>
+      isRestrictionActive(restriction) pipeTo sender
+
   }
 
   private def isSendingEnabled(key: String): Future[Boolean] = {
@@ -41,9 +51,15 @@ abstract class ParameterActor extends Actor with ActorLogging {
     period.dateStart <= now && period.dateEnd > now
   }
 
+  protected def isAnyPeriodEffective(periods: List[SendingPeriod]): Boolean = {
+    periods.exists(period => isPeriodEffective(period))
+  }
+
   protected def getParams(hakuOid: String): Future[DateTime]
 
   protected def isEnabledFromRest(key: String): Future[Boolean]
+
+  protected def isRestrictionActive(restriction: String): Future[Boolean]
 }
 
 class HttpParameterActor(restClient: VirkailijaRestClient) extends ParameterActor {
@@ -68,9 +84,22 @@ class HttpParameterActor(restClient: VirkailijaRestClient) extends ParameterActo
     })
   }
 
-  override def isEnabledFromRest(key: String): Future[Boolean] = restClient.readObject[TiedonsiirtoSendingPeriods]("ohjausparametrit-service.parametri", "tiedonsiirtosendingperiods")(200).map(p => key match {
+  override def isEnabledFromRest(key: String): Future[Boolean] =
+    restClient.readObject[TiedonsiirtoSendingPeriods]("ohjausparametrit-service.parametri", "tiedonsiirtosendingperiods")(HTTP_OK).map(p => key match {
     case k if k == ImportBatch.batchTypePerustiedot => isPeriodEffective(p.perustiedot)
     case k if k == ImportBatch.batchTypeArvosanat => isPeriodEffective(p.arvosanat)
+    case _ => false
+  }).recoverWith {
+    case t: Throwable =>
+      log.error(t, "error retrieving parameter")
+      Future.successful(false)
+  }
+
+  override def isRestrictionActive(restriction: String): Future[Boolean] =
+    restClient.readObject[RestrictionPeriods]("ohjausparametrit-service.parametri", "restrictedperiods")(HTTP_OK).map(p => restriction match {
+    case ParameterActor.opoUpdateGraduation =>  {
+      isAnyPeriodEffective(p.opoUpdateGraduation)
+    }
     case _ => false
   }).recoverWith {
     case t: Throwable =>
@@ -83,6 +112,11 @@ class MockParameterActor extends ParameterActor {
   override protected def getParams(hakuOid: String) = Future { new DateTime().plusMonths(1) }
 
   override protected def isEnabledFromRest(key: String) = Future { true }
+
+  override protected def isRestrictionActive(restriction: String) = Future {
+    if (restriction.equals(ParameterActor.opoUpdateGraduation)) true
+    else false
+  }
 }
 
 case class KierrosRequest(haku: String)
@@ -91,5 +125,7 @@ case class KierrosParams(PH_HKP: Option[KierrosEndParams])
 case class HakuParams(end: DateTime)
 
 case class IsSendingEnabled(key: String)
+case class IsRestrictionActive(restriction: String)
 case class SendingPeriod(dateStart: Long, dateEnd: Long)
 case class TiedonsiirtoSendingPeriods(arvosanat: SendingPeriod, perustiedot: SendingPeriod)
+case class RestrictionPeriods(opoUpdateGraduation: List[SendingPeriod])
