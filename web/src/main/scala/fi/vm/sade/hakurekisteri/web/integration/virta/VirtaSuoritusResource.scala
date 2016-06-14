1c5cd1e9de9441ddf6c17edf3b9af560ae227938
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2016-06-14 12:12:08 +0300

    Auditlog virta query

--- web/src/main/scala/fi/vm/sade/hakurekisteri/web/integration/virta/VirtaSuoritusResource.scala
+++ web/src/main/scala/fi/vm/sade/hakurekisteri/web/integration/virta/VirtaSuoritusResource.scala
@@ -4,12 +4,13 @@ import akka.actor.{ActorRef, ActorSystem}
 import akka.event.{Logging, LoggingAdapter}
 import akka.pattern.{AskTimeoutException, ask}
 import akka.util.Timeout
+import fi.vm.sade.auditlog.hakurekisteri.{HakuRekisteriOperation, LogMessage}
 import fi.vm.sade.hakurekisteri.integration.hakemus.HasPermission
 import fi.vm.sade.hakurekisteri.integration.henkilo.{Henkilo, HetuQuery}
 import fi.vm.sade.hakurekisteri.integration.virta.{VirtaQuery, VirtaResult}
 import fi.vm.sade.hakurekisteri.rest.support.{HakurekisteriJsonSupport, User}
 import fi.vm.sade.hakurekisteri.web.HakuJaValintarekisteriStack
-import fi.vm.sade.hakurekisteri.web.rest.support.{IncidentReport, Security, SecuritySupport}
+import fi.vm.sade.hakurekisteri.web.rest.support.{IncidentReport, Security, SecuritySupport, UserNotAuthorized}
 import org.scalatra.json.JacksonJsonSupport
 import org.scalatra.swagger.{Swagger, SwaggerEngine}
 import org.scalatra.{AsyncResult, FutureSupport, InternalServerError}
@@ -26,34 +27,39 @@ class VirtaSuoritusResource(virtaActor: ActorRef, hakuAppPermissionChecker: Acto
   override protected implicit def swagger: SwaggerEngine[_] = sw
   override protected implicit def executor: ExecutionContext = system.dispatcher
   override protected def applicationDescription: String = "Henkilön suoritusten haun rajapinta Virta-palvelusta"
-  val hetuPattern: Regex = "[0-9]{6}[A+-][0-9]{3}[0-9A-FHJ-NPR-Y]$".r
   implicit val defaultTimeout: Timeout = 30.seconds
 
   // XXX: Replace with proper permission checks
-  def hasAccess(personOid: String, user: Option[User]): Future[Boolean] =
-    if (user.exists(_.isAdmin)) {
+  def hasAccess(personOid: String, user: User): Future[Boolean] =
+    if (user.isAdmin) {
       Future.successful(true)
     } else {
-      (hakuAppPermissionChecker ? HasPermission(user.get, personOid)).mapTo[Boolean]
+      (hakuAppPermissionChecker ? HasPermission(user, personOid)).mapTo[Boolean]
     }
 
   before() {
     contentType = formats("json")
   }
 
-  def parsePIN(id: String): Option[String] = {
-    hetuPattern.findFirstIn(id)
+  private def auditlogQuery(username: String, henkilo: String): Unit = {
+    audit.log(LogMessage.builder()
+      .id(username)
+      .setOperaatio(HakuRekisteriOperation.READ_VIRTA_TIEDOT)
+      .setResourceId(henkilo)
+      .build()
+    )
   }
 
   get("/:hetu", operation(query)) {
     val hetu = params("hetu")
-    val user = currentUser
+    val user = currentUser.getOrElse(throw new UserNotAuthorized("not authorized"))
     new AsyncResult() {
       override implicit def timeout: Duration = 30.seconds
       override val is =
         (henkiloActor ? HetuQuery(hetu)).mapTo[Henkilo].flatMap(henkilo => {
           hasAccess(henkilo.oidHenkilo, user).flatMap(access => {
             if (access) {
+              auditlogQuery(user.username, henkilo.oidHenkilo)
               virtaActor ? VirtaQuery(oppijanumero = henkilo.oidHenkilo, hetu = Some(hetu))
             } else {
               Future.successful(VirtaResult(hetu))
