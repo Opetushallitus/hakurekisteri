ba705c3eab8e170be3892309a589bae0551e0e97
Mikko Apo <mikko.apo@reaktor.fi>
2016-07-25 12:19:58 +0300

    fixed build_deploy.sh, maven shade plugin creates a jar with different suffix

--- sbin/build_deploy_zip.sh
+++ sbin/build_deploy_zip.sh
@@ -14,7 +14,7 @@ ZIP=${PWD}/target/${SERVICE}_${BRANCH}_$(date +%Y%m%d%H%M%S).zip
 echo "Building $ZIP"
 
 cd ./target
-JAR=( ./*jar-with-dependencies.jar )
+JAR=( ./*-allinone.jar )
 [ ${#JAR[@]} -lt 1 ] && { printf "No runnable jar found\n"; exit 1; }
 [ ${#JAR[@]} -gt 1 ] && { printf "Multiple runnable jars found: %s\n" "${JAR[*]}"; exit 1; }
 cp "${JAR[0]}" "${SERVICE}.jar"
