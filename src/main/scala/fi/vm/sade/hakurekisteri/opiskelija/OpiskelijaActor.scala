661edf0140449dd42c684189822cb16031d89cc5
Jani Rahkola <jani.rahkola@reaktor.com>
2016-08-10 16:03:38 +0300

    Take in use Opiskelija and Arvosana JDBC actors

--- src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/OpiskelijaActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/OpiskelijaActor.scala
@@ -1,18 +1,22 @@
 package fi.vm.sade.hakurekisteri.opiskelija
 
+import java.util.UUID
+import java.util.concurrent.Executors
+
+import akka.dispatch.ExecutionContexts
 import akka.event.Logging
 import com.github.nscala_time.time.Imports._
+import fi.vm.sade.hakurekisteri.rest.support
+import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriDriver.api._
 import fi.vm.sade.hakurekisteri.rest.support.Kausi._
 import fi.vm.sade.hakurekisteri.rest.support.{JDBCJournal, JDBCRepository, JDBCService}
 import fi.vm.sade.hakurekisteri.storage._
 import fi.vm.sade.hakurekisteri.storage.repository._
-import java.util.UUID
-
-import fi.vm.sade.hakurekisteri.rest.support
+import slick.ast.Node
+import slick.driver.JdbcStatementBuilderComponent
+import slick.lifted
 
 import scala.concurrent.{ExecutionContext, Future}
-import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriDriver.api._
-import slick.lifted
 
 
 trait OpiskelijaRepository extends JournaledRepository[Opiskelija, UUID] {
@@ -174,11 +178,15 @@ class OpiskelijaJDBCActor(val journal: JDBCJournal[Opiskelija, UUID, OpiskelijaT
   override def deduplicationQuery(i: Opiskelija)(t: OpiskelijaTable): Rep[Boolean] =
     t.oppilaitosOid === i.oppilaitosOid && t.luokkataso === i.luokkataso && t.henkiloOid === i.henkiloOid
 
-  override val dbExecutor: ExecutionContext = context.dispatcher
+  override val dbExecutor: ExecutionContext = ExecutionContexts.fromExecutor(Executors.newFixedThreadPool(poolSize))
 
   override val dbQuery: PartialFunction[support.Query[Opiskelija], lifted.Query[OpiskelijaTable, Delta[Opiskelija, UUID], Seq]] = {
     case OpiskelijaQuery(henkilo, kausi, vuosi, paiva, oppilaitosOid, luokka) =>
-      all.filter(t => matchHenkilo(henkilo)(t) && matchOppilaitosOid(oppilaitosOid)(t) && matchLuokka(luokka)(t))
+      all.filter(t => matchHenkilo(henkilo)(t) &&
+        matchOppilaitosOid(oppilaitosOid)(t) &&
+        matchPaiva(paiva)(t) &&
+        matchVuosiAndKausi(vuosi, kausi)(t) &&
+        matchLuokka(luokka)(t))
   }
 
   private def matchHenkilo(henkilo: Option[String])(t: OpiskelijaTable): Rep[Boolean] = henkilo match {
@@ -196,6 +204,46 @@ class OpiskelijaJDBCActor(val journal: JDBCJournal[Opiskelija, UUID, OpiskelijaT
     case None => true
   }
 
-  // TODO implement the remaining matchers
+  private def matchPaiva(paiva: Option[DateTime])(t: OpiskelijaTable): Rep[Boolean] = paiva match {
+    case Some(date) => t.alkuPaiva < date && (t.loppuPaiva.isEmpty || (t.loppuPaiva > date).asColumnOf[Boolean])
+    case None => true
+  }
 
+  private def matchVuosiAndKausi(vuosi: Option[String], kausi: Option[Kausi])(t: OpiskelijaTable): Rep[Boolean] = {
+    val start = t.alkuPaiva
+    val end = t.loppuPaiva
+    (vuosi, kausi) match {
+      case (Some(v), Some(Kevät)) => (end.isEmpty || (startOfYear(v) <= end).asColumnOf[Boolean]) && start < startOfAutumn(v)
+      case (Some(v), Some(Syksy)) => (end.isEmpty || (startOfAutumn(v) <= end).asColumnOf[Boolean]) && start < startOfYear((v.toInt + 1).toString)
+      case (Some(v), None) => (end.isEmpty || (startOfYear(v) <= end).asColumnOf[Boolean]) && start < startOfYear((v.toInt + 1).toString)
+      case (None, Some(Kevät)) => end.isEmpty || (start < startOfAutumn(yearOfNullable(end))).asColumnOf[Boolean]
+      case (None, Some(Syksy)) => end.isEmpty || (startOfAutumn(yearOf(start)) <= end).asColumnOf[Boolean]
+      case (None, None) => true
+      case (_, Some(k)) => throw new IllegalArgumentException(s"Not a kausi $k")
+    }
+  }
+
+  private val startOfYear = SimpleExpression.unary[String, DateTime] {
+    case (year, qb) =>
+      qb.sqlBuilder += "extract(epoch from to_timestamp("
+      qb.expr(year)
+      qb.sqlBuilder += " || '-01-01', 'YYYY-MM-DD')) * 1000"
+  }
+
+  private val startOfAutumn = SimpleExpression.unary[String, DateTime] {
+    case (year, qb) =>
+      qb.sqlBuilder += "extract(epoch from to_timestamp("
+      qb.expr(year)
+      qb.sqlBuilder += " || '-08-01', 'YYYY-MM-DD')) * 1000"
+  }
+
+  private def yearOfExpr(millis: Node, qb: JdbcStatementBuilderComponent#QueryBuilder): Unit = {
+    qb.sqlBuilder += "extract(year from to_timestamp("
+    qb.expr(millis)
+    qb.sqlBuilder += " / 1000))"
+  }
+
+  private val yearOf = SimpleExpression.unary[DateTime, String](yearOfExpr)
+
+  private val yearOfNullable = SimpleExpression.unary[Option[DateTime], String](yearOfExpr)
 }
