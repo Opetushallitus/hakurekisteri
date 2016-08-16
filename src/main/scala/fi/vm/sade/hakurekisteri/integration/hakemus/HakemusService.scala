4fc4736d9368999babe524c0d4ba113a007e9f53
Alexis Troberg <alexis.troberg@gofore.com>
2016-08-16 17:05:48 +0300

    Modified hakemukset trigger re-implemented
    TODO:
    - error handling
    - current solution initially (on startup) fetches applications that have been modified
      in the past 48 hours. This could be fixed by storing the last
      fetch time in DB and then fetch applications that have been modified
      since last check.

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/hakemus/HakemusService.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/hakemus/HakemusService.scala
@@ -1,11 +1,17 @@
 package fi.vm.sade.hakurekisteri.integration.hakemus
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import akka.actor.Scheduler
 import fi.vm.sade.hakurekisteri.hakija.HakijaQuery
 import fi.vm.sade.hakurekisteri.integration.{ServiceConfig, VirkailijaRestClient}
 import fi.vm.sade.hakurekisteri.rest.support.Query
 
+import scala.compat.Platform
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.{Future, Promise}
 
 case class HakemusConfig(serviceConf: ServiceConfig, maxApplications: Int)
 
@@ -32,7 +38,9 @@ object Trigger {
   })
 }
 
-class HakemusService(restClient: VirkailijaRestClient) {
+class HakemusService(restClient: VirkailijaRestClient, pageSize: Int = 2000) {
+
+  var triggers: Seq[Trigger] = Seq()
 
   def hakemuksetForPerson(personOid: String): Future[Seq[FullHakemus]] = {
     for (
@@ -54,6 +62,52 @@ class HakemusService(restClient: VirkailijaRestClient) {
 
   def personOidsForHakukohde(hakukohdeOid: String, organisaatio: Option[String]): Future[Set[String]] = {
     restClient.postObject[Set[String], Set[String]]("haku-app.personoidsbyapplicationoption", organisaatio.orNull)(200, Set(hakukohdeOid))
+  }
+
+  def addTrigger(trigger: Trigger) = triggers = triggers :+ trigger
+
+  private def triggerHakemukset(hakemukset: Seq[FullHakemus]) =
+    hakemukset.foreach(hakemus =>
+      triggers.foreach(trigger => trigger.f(hakemus))
+    )
+
+  private val twoDaysAgo = 1000 * 60 * 60 * 24 * 2
+
+  def processModifiedHakemukset(modifiedAfter: Date = new Date(Platform.currentTime - twoDaysAgo),
+                                refreshFrequency: FiniteDuration = 1.minute)(implicit scheduler: Scheduler): Unit = {
+    scheduler.scheduleOnce(refreshFrequency)({
+      fetchHakemukset(modifiedAfter = new SimpleDateFormat("yyyyMMddHHmm").format(modifiedAfter)).future.onSuccess {
+        case hakemukset: Seq[FullHakemus] =>
+          triggerHakemukset(hakemukset)
+          processModifiedHakemukset(modifiedAfter = new Date(Platform.currentTime - (5 * 60 * 1000)), refreshFrequency)
+      }
+    })
+  }
+
+  private def getQueryParams(page: Int, modifiedAfter: String) = {
+    Map(
+      "updatedAfter" -> modifiedAfter,
+      "start" -> page * pageSize,
+      "rows" -> pageSize
+    )
+  }
+
+  private def fetchHakemukset(page: Int = 0, modifiedAfter: String): Promise[Seq[FullHakemus]] = {
+    val futureHakemukset: Future[List[FullHakemus]] = restClient.readObject[List[FullHakemus]]("haku-app.listfull", getQueryParams(page, modifiedAfter))(acceptedResponseCode = 200, maxRetries = 2)
+
+    val promise = Promise[Seq[FullHakemus]]()
+
+    for (hakemukset <- futureHakemukset) {
+      if (hakemukset.length < pageSize) {
+        promise.success(hakemukset)
+      } else {
+        fetchHakemukset(page + 1, modifiedAfter).future.onSuccess {
+          case nextPageHakemukset: Seq[FullHakemus] => promise.success(hakemukset ++ nextPageHakemukset)
+        }
+      }
+    }
+
+    promise
   }
 
 }
--- src/test/scala/fi/vm/sade/hakurekisteri/integration/hakemus/HakemusServiceSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/hakemus/HakemusServiceSpec.scala
@@ -3,12 +3,12 @@ package fi.vm.sade.hakurekisteri.integration.hakemus
 import akka.actor.ActorSystem
 import com.ning.http.client.AsyncHttpClient
 import fi.vm.sade.hakurekisteri.integration._
-import scala.concurrent.duration._
 import org.mockito.Mockito._
 import org.scalatest._
 import org.scalatest.mock.MockitoSugar
 
 import scala.compat.Platform
+import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext}
 
 class HakemusServiceSpec extends FlatSpec with Matchers with MockitoSugar with DispatchSupport with LocalhostProperties {
@@ -18,7 +18,7 @@ class HakemusServiceSpec extends FlatSpec with Matchers with MockitoSugar with D
   val endPoint = mock[Endpoint]
   val asyncProvider = new CapturingProvider(endPoint)
   val client = new VirkailijaRestClient(ServiceConfig(serviceUrl = "http://localhost/haku-app"), aClient = Some(new AsyncHttpClient(asyncProvider)))
-  val hakemusService = new HakemusService(client)
+  val hakemusService = new HakemusService(restClient = client, pageSize = 10)
 
   it should "return applications by person oid" in {
     when(endPoint.request(forPattern(".*applications/byPersonOid.*")))
@@ -32,6 +32,30 @@ class HakemusServiceSpec extends FlatSpec with Matchers with MockitoSugar with D
       .thenReturn((200, List(), getJson("byApplicationOption")))
 
     Await.result(hakemusService.hakemuksetForHakukohde("1.2.246.562.20.649956391810", None), 10.seconds).size should be (6)
+  }
+
+  it should "execute trigger function for modified applications" in {
+    val system = ActorSystem("hakurekisteri")
+    implicit val scheduler = system.scheduler
+
+    when(endPoint.request(forPattern(".*listfull.*start=0.*")))
+      .thenReturn((200, List(), getJson("listfull-0")))
+      .thenReturn((200, List(), "[]"))
+    when(endPoint.request(forPattern(".*listfull.*start=1.*")))
+      .thenReturn((200, List(), getJson("listfull-1")))
+    when(endPoint.request(forPattern(".*listfull.*start=2.*")))
+      .thenReturn((200, List(), "[]"))
+
+
+    var triggerCounter = 0
+    hakemusService.addTrigger(Trigger(f = (hakemus: FullHakemus) => {
+      triggerCounter += 1
+    }))
+    hakemusService.processModifiedHakemukset(refreshFrequency = 1.millisecond)
+
+    Thread.sleep(100)
+
+    triggerCounter should be (20)
   }
 
 }
