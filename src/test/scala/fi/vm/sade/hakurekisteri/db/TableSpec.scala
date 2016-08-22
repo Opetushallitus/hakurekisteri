9c30138fa4b37d630de808a6393bea5f8f70346e
Harri Hämäläinen <hhamalai@iki.fi>
2016-08-22 13:16:52 +0300

    Replace H2 database with embedded Postgres
    Most of this has already been done during JDBC actors rollout, removing now this
    completely from the project dependencies.
     Goodbye H2

--- src/test/scala/fi/vm/sade/hakurekisteri/db/TableSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/db/TableSpec.scala
@@ -7,67 +7,78 @@ import fi.vm.sade.hakurekisteri.batchimport.{BatchState, ImportBatch, ImportBatc
 import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriDriver.api._
 import fi.vm.sade.hakurekisteri.storage.Identified
 import fi.vm.sade.hakurekisteri.storage.repository.{Delta, Updated}
-import org.h2.engine.SysProperties
+import fi.vm.sade.hakurekisteri.tools.ItPostgres
 import org.joda.time.DateTime
-import org.scalatest.{FlatSpec, Matchers}
+import org.postgresql.util.PSQLException
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 import slick.jdbc.meta.MTable
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext}
 import scala.language.existentials
 
-class TableSpec extends FlatSpec with Matchers {
+class TableSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   implicit val system = ActorSystem("test-jdbc")
   implicit val ec: ExecutionContext = system.dispatcher
-  SysProperties.serializeJavaObject = false
   behavior of "ImportBatchTable"
 
   val table = TableQuery[ImportBatchTable]
-
+  implicit var db: Database = _
   println(table.baseTableRow.tableName)
 
-  def getDb: Database = {
-    Database.forURL("jdbc:h2:mem:test;MODE=PostgreSQL", driver = "org.h2.Driver")
+  override def beforeAll(): Unit = {
+    db = Database.forURL(ItPostgres.getEndpointURL())
+    try {
+      Await.result(db.run(
+        table.schema.create
+      ), 10.seconds)
+    } catch {
+      case e: PSQLException => println("Database already exists")
+    }
+  }
+
+  override def afterAll(): Unit = {
+    try super.afterAll()
+    finally {
+      Await.result(system.terminate(), 15.seconds)
+      db.close()
+    }
   }
 
   it should "be able create itself" in {
-    val db = getDb
+    ItPostgres.reset()
     val tables: Vector[MTable] = Await.result(db.run(
-      table.schema.create andThen MTable.getTables(table.baseTableRow.tableName)
+      MTable.getTables(table.baseTableRow.tableName)
     ), 10.seconds)
-
-    db.close()
 
     tables.size should be(1)
   }
 
   it should "be able to store updates" in {
-    val db = getDb
+    ItPostgres.reset()
     val xml = <batchdata>data</batchdata>
     val batch = ImportBatch(xml, Some("externalId"), "test", "test", BatchState.READY, ImportStatus()).identify(UUID.randomUUID())
 
     val result = Await.result(db.run(
-      table.schema.create andThen (table += Updated(batch))
+      table += Updated(batch)
     ), 10.seconds)
 
-    db.close()
 
     result should be(1)
   }
 
 
   it should "be able to retrieve updates" in {
-    val db = getDb
+    ItPostgres.reset()
     val xml = <batchdata>data</batchdata>
     val batch: ImportBatch with Identified[UUID] = ImportBatch(xml, Some("externalIdToo"), "test", "test", BatchState.READY, ImportStatus(new DateTime(), Some(new DateTime()), Map("foo" -> Set("foo exception")), Some(1), Some(0), Some(1))).identify(UUID.randomUUID())
     val q = table.filter(_.resourceId === batch.id)
 
-    val action =  table.schema.create andThen (table += Updated(batch)) andThen q.result
+    val action = (table += Updated(batch)) andThen q.result
     val results: Seq[Delta[ImportBatch, UUID]] = Await.result(db.run(action), 60.seconds)
     results.size should be(1)
     val Updated(current) = results.head
     current should be(batch)
-    db.close()
   }
 
 
