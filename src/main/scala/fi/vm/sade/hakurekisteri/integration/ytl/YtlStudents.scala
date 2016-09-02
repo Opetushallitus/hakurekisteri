2e6b5d174eb6ced1057e48a16f136c3f2705e04b
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-09-02 12:49:00 +0300

    YTL json spec

---
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlStudents.scala
@@ -0,0 +1,32 @@
+package fi.vm.sade.hakurekisteri.integration.ytl
+
+import java.util.Date
+
+
+case class YtlStudents(students: List[Either[Throwable, Student]])
+
+case class Student(ssn: String, lastname: String, firstnames: String, graduationPeriod: Kausi,
+                   graduationDate: Date,
+                   graduationSchoolOphOid: String,
+                   graduationSchoolYtlNumber: String,
+                   language: String,
+                   exams: Seq[Exam])
+
+case class Exam(examId: String,examRole: Int, period: Kausi, grade: String, points: Int, sections: Seq[Section])
+
+case class Section(sectionId: String, sectionPoints: Int)
+
+trait Kausi
+case class Kevat(vuosi:Int) extends Kausi
+case class Syksy(vuosi:Int) extends Kausi
+
+object Kausi {
+  private val kausi = """(\d\d\d\d)(K|S)""".r
+
+  def apply(kausiSpec: String): Kausi = kausiSpec match {
+    case kausi(year, "K") => Kevat(Integer.parseInt(year))
+    case kausi(year, "S") => Syksy(Integer.parseInt(year))
+    case kausi(year, c) => throw new IllegalArgumentException(s"Illegal last character '$c'. Valid values are 'K' and 'S'")
+    case s => throw new IllegalArgumentException(s"Illegal kausi specification '$s'. Expected format YYYY(K|S), e.g. 2015K")
+  }
+}
