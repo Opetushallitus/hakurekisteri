e968868e8cb7fd0ead803dafb6ebacf57bae0f54
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-09-02 12:49:00 +0300

    YTL json spec

---
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlJsonSpec.scala
@@ -0,0 +1,27 @@
+package fi.vm.sade.hakurekisteri.integration.ytl
+
+import java.text.SimpleDateFormat
+
+import fi.vm.sade.hakurekisteri.rest.support.StudentDeserializer
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import org.json4s.jackson.Serialization
+import org.scalatra.test.scalatest.ScalatraFunSuite
+
+
+class YtlJsonSpec extends ScalatraFunSuite {
+  def dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+  implicit val formats = Serialization.formats(NoTypeHints) + new StudentDeserializer
+
+  test("Parse YTL json") {
+    val json = scala.io.Source.fromFile(getClass.getResource("/ytl-students.json").getFile).getLines.mkString
+    val ytlstudents = parse(json).extract[YtlStudents]
+    ytlstudents.students.size should equal (3)
+    val validStudents = ytlstudents.students.filter(_.isRight).map(s => s.right.get)
+
+    validStudents.size should equal (1)
+    validStudents.head.graduationDate should equal(dateFormat.parse("2016-06-04"))
+    validStudents.head.graduationPeriod should equal (Kevat(2015))
+  }
+
+}
