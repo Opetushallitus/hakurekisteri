798a2c042df048f02c1f89046d934d11f530835d
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-09-05 16:07:19 +0300

    reading zip entry + mock

--- src/test/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlHttpFetchSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlHttpFetchSpec.scala
@@ -5,18 +5,33 @@ import org.scalatra.test.scalatest.ScalatraFunSuite
 
 
 class YtlHttpFetchSpec extends ScalatraFunSuite with YtlMockFixture {
+  val config = new OphProperties()
+    .addDefault("ytl.http.host.bulk", bulkUrl)
+    .addDefault("ytl.http.host.download", downloadUrl)
+    .addDefault("ytl.http.host.fetchone", fetchOneUrl)
+    .addDefault("ytl.http.host.status", statusUrl)
+    .addDefault("ytl.http.username", username)
+    .addDefault("ytl.http.password", password)
+  val fileSystem = new YtlFileSystem(config)
+  val ytlHttpFetch = new YtlHttpFetch(config,fileSystem)
+
+  test("zip to students") {
+
+    ytlHttpFetch.zipToStudents(getClass.getResource("/student-results.zip").openStream())
+    "ok" should equal ("ok")
+  }
 
 
+  test("Fetch one with basic auth") {
 
-  test("Fetch with basic auth") {
-    val v = new OphProperties()
-    v.addDefault("ytl.http.host", url)
-    v.addDefault("ytl.http.username", username)
-    v.addDefault("ytl.http.password", password)
-    val ytlHttpFetch = new YtlHttpFetch(v)
     val student = ytlHttpFetch.fetchOne("050996-9574")
     student.lastname should equal ("Vasala")
     student.firstnames should equal ("Sampsa")
+  }
+  test("Fetch many as zip") {
+    val students = ytlHttpFetch.fetch(List("050996-9574"))
+
+    students should equal (Right(List()))
   }
 
 }
