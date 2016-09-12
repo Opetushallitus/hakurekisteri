ee6c8aae695e7f4b08b886915e80c804aa302c04
Alexis Troberg <alexis.troberg@gofore.com>
2016-09-12 15:16:49 +0300

    WiP: write old vs. new integration results as JSON
    Write old and new YTL integration results as JSON, in
    order to verify that they produce the same results.

---
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlDiffSpec.scala
@@ -0,0 +1,55 @@
+package fi.vm.sade.hakurekisteri.integration.ytl
+
+import java.io.File
+
+import fi.vm.sade.hakurekisteri.suoritus.{VirallinenSuoritus, yksilollistaminen}
+import org.joda.time.LocalDate
+import org.scalatra.test.scalatest.ScalatraFunSuite
+
+class YtlDiffSpec extends ScalatraFunSuite {
+
+  private def testKokelas(oid: String) = Kokelas(oid, VirallinenSuoritus("yo1", "myontaja", "VALMIS", LocalDate.now(), "henkilo", yksilollistaminen.Ei, "fi", None, vahv = true, "YTL"), None, Seq(), Seq())
+
+  test("YtlActor should write JSON for diff purposes") {
+    val filename = "ytl-diff-kokelaat.json"
+    val kokelaat = Seq(Some(testKokelas("oid1")), Some(testKokelas("oid2")))
+    YtlDiff.writeKokelaatAsJson(kokelaat, filename)
+
+    val expected =
+      """[ {
+        |  "oid" : "oid1",
+        |  "yo" : {
+        |    "komo" : "yo1",
+        |    "myontaja" : "myontaja",
+        |    "tila" : "VALMIS",
+        |    "valmistuminen" : { },
+        |    "yksilollistaminen" : {
+        |      "name" : "Ei"
+        |    },
+        |    "suoritusKieli" : "fi"
+        |  },
+        |  "yoTodistus" : [ ],
+        |  "osakokeet" : [ ]
+        |}, {
+        |  "oid" : "oid2",
+        |  "yo" : {
+        |    "komo" : "yo1",
+        |    "myontaja" : "myontaja",
+        |    "tila" : "VALMIS",
+        |    "valmistuminen" : { },
+        |    "yksilollistaminen" : {
+        |      "name" : "Ei"
+        |    },
+        |    "suoritusKieli" : "fi"
+        |  },
+        |  "yoTodistus" : [ ],
+        |  "osakokeet" : [ ]
+        |} ]""".stripMargin
+
+    val actual = scala.io.Source.fromFile(filename).mkString
+    new File(filename).delete()
+
+    actual should be(expected)
+  }
+
+}
