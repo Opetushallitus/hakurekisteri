9fb311c19e43b1d7e0a0767973badc1a8e07e719
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-09-12 13:43:50 +0300

    Remove unneeded line
    Running it took 35 minutes of the total of the 45 minute migration run
    on my machine using production data

--- db/migration_008_add_current_field_to_resources.sql
+++ db/migration_008_add_current_field_to_resources.sql
@@ -12,7 +12,6 @@ create index arvosana_new_suoritus_idx on arvosana_new (suoritus, deleted);
 alter table arvosana rename to arvosana_old;
 alter table arvosana_new rename to arvosana;
 
-update arvosana set current = TRUE from (select resource_id, max(inserted) as max_inserted from arvosana group by resource_id) x2 where arvosana.resource_id = x2.resource_id and inserted = x2.max_inserted;
 update import_batch set current = TRUE from (select resource_id, max(inserted) as max_inserted from import_batch group by resource_id) x2 where import_batch.resource_id = x2.resource_id and inserted = x2.max_inserted;
 update opiskelija set current = TRUE from (select resource_id, max(inserted) as max_inserted from opiskelija group by resource_id) x2 where opiskelija.resource_id = x2.resource_id and inserted = x2.max_inserted;
 update opiskeluoikeus set current = TRUE from (select resource_id, max(inserted) as max_inserted from opiskeluoikeus group by resource_id) x2 where opiskeluoikeus.resource_id = x2.resource_id and inserted = x2.max_inserted;
