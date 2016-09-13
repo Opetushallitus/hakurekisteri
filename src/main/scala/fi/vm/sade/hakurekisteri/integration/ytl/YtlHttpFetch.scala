ec76be235712b0bfedab5c497b0788c4b8975dc6
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-09-13 12:32:30 +0300

    Iterate through 'kokelaat'

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlHttpFetch.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlHttpFetch.scala
@@ -6,6 +6,7 @@ import java.util.zip.{ZipInputStream}
 import java.io
 import com.google.common.io.ByteStreams
 import fi.vm.sade.hakurekisteri.integration.ytl.Student.StudentAsyncParser
+import fi.vm.sade.hakurekisteri.tools.Zip
 import fi.vm.sade.javautils.httpclient._
 import fi.vm.sade.properties.OphProperties
 import jawn.{ast, Parser, AsyncParser}
@@ -28,11 +29,11 @@ import scala.util.{Failure, Success, Try}
 
 class YtlHttpFetch(config: OphProperties, fileSystem: YtlFileSystem) {
   val logger = LoggerFactory.getLogger(this.getClass)
-
   import scala.language.implicitConversions
   implicit val formats = Student.formatsStudent
   val username = config.getProperty("ytl.http.username")
   val password = config.getProperty("ytl.http.password")
+  val bufferSize = config.getOrElse("ytl.http.buffersize", "4096").toInt // 4K
   val credentials = new UsernamePasswordCredentials(username, password)
   val preemptiveBasicAuthentication = new BasicScheme().authenticate(credentials, new HttpPost(), null).getValue
 
@@ -49,34 +50,48 @@ class YtlHttpFetch(config: OphProperties, fileSystem: YtlFileSystem) {
 
   val client = buildClient()
 
-  def zipToStudents(stream: InputStream): Iterator[Student] = {
-    val zip = new ZipInputStream(stream)
+  def zipToStudents(z: ZipInputStream): Iterator[Student] = {
+    zipToStudents(Zip.toInputStreams(z))
+  }
 
+  def zipToStudents(streams: Iterator[InputStream]): Iterator[Student] = streams.flatMap(
+    input => {
+      val parser = StudentAsyncParser()
+      val data = new Array[Byte](bufferSize)
 
-    Stream.continually(Try(zip.getNextEntry()).getOrElse(null))
-      .takeWhile(_ != null)
-      .toIterator
-      .flatMap(e => {
-        val parser = StudentAsyncParser()
-        val oneBigByteString = ByteStreams.toByteArray(zip)
-        parser.feedChunk(oneBigByteString).flatMap {
-          case Success(student) => Some(student)
-          case Failure(e) =>
-            logger.error(s"Unable to parse student from YTL data! ${e.getMessage}")
-            None
+      Iterator.continually(ByteStreams.read(input, data, 0, data.length))
+        .takeWhile(_ != 0)
+        .flatMap {
+          case x if x == data.length => safeParseStudentsFromBytes(parser, data)
+          case x if x < data.length =>
+            val newarr = new Array[Byte](x)
+            Array.copy(data, 0, newarr, 0, x)
+            safeParseStudentsFromBytes(parser, newarr)
         }
-      })
+    }
+  )
+
+  private def safeParseStudentsFromBytes(parser: StudentAsyncParser, data: Array[Byte]): Seq[Student] = {
+    parser.feedChunk(data).flatMap {
+      case Success(student) => Some(student)
+      case Failure(e) =>
+        logger.error(s"Unable to parse student from YTL data! ${e.getMessage}")
+        None
+    }
   }
 
   def fetchOne(hetu: String): Student =
     client.get("ytl.http.host.fetchone", hetu).expectStatus(200).execute((r:OphHttpResponse) => parse(r.asText()).extract[Student])
 
-  def fetch(hetus: List[String]): Either[Throwable, Iterator[Student]] = {
+  def fetch(hetus: List[String]): Either[Throwable, (ZipInputStream, Iterator[Student])] = {
     for {
       operation <- fetchOperation(hetus).right
       ok <- fetchStatus(operation.operationUuid).right
       zip <- downloadZip(operation.operationUuid).right
-    } yield zipToStudents(zip)
+    } yield {
+      val z = new ZipInputStream(zip)
+      (z, zipToStudents(z))
+    }
   }
 
   def fetchStatus(uuid: String): Either[Throwable,Status] = {
