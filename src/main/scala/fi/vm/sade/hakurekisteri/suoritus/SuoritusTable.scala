7d05fccfb4a6e6bc7fec456ed5a5a889e7fc0053
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-09-13 15:34:21 +0300

    Start adding ammatillisen kielikoe suoritus
    It's not possible to enter it via the API yet.

--- src/main/scala/fi/vm/sade/hakurekisteri/suoritus/SuoritusTable.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/suoritus/SuoritusTable.scala
@@ -29,10 +29,12 @@ class SuoritusTable(tag: Tag) extends JournalTable[Suoritus, UUID, SuoritusRow](
   def yksilollistaminen = column[Option[Yksilollistetty]]("yksilollistaminen")
   def suoritusKieli = column[Option[String]]("suoritus_kieli")
 
-  //vapaamuotoinen
+  //vapaamuotoinen tai ammatillisen koulutuksen kielikoe
   def kuvaus = column[Option[String]]("kuvaus")
   def vuosi = column[Option[Int]]("vuosi")
   def tyyppi = column[Option[String]]("tyyppi")
+
+  //vapaamuotoinen
   def index = column[Option[Int]]("index")
 
   override def resourceShape = (myontaja, henkiloOid, vahvistettu, komo, tila, valmistuminen, yksilollistaminen, suoritusKieli, kuvaus, vuosi, tyyppi, index, source).shaped
@@ -42,6 +44,8 @@ class SuoritusTable(tag: Tag) extends JournalTable[Suoritus, UUID, SuoritusRow](
       Some( o.myontaja, o.henkiloOid, Some(o.vahvistettu), Some(o.komo),Some(o.tila), Some(o.valmistuminen), Some(o.yksilollistaminen), Some(o.suoritusKieli), None, None, None, None, o.source)
     case s: VapaamuotoinenSuoritus =>
       Some(s.myontaja, s.henkiloOid, Some(s.vahvistettu), None, None, None,  None, None, Some(s.kuvaus), Some(s.vuosi), Some(s.tyyppi), Some(s.index), s.source)
+    case s: AmmatillisenKielikoeSuoritus =>
+      Some(s.myontaja, s.henkiloOid, Some(s.vahvistettu), None, None, None,  None, None, Some(s.kuvaus), Some(s.vuosi), Some(AmmatillisenKielikoeSuoritus.tyyppi), None, s.source)
 
   }
 
@@ -52,6 +56,8 @@ class SuoritusTable(tag: Tag) extends JournalTable[Suoritus, UUID, SuoritusRow](
       VirallinenSuoritus(komo, myontaja, tila, valmistuminen, henkiloOid, yks, suoritusKieli, vahv = vahvistettu.getOrElse(true), lahde = source)
     case (myontaja, henkiloOid, vahvistettu, _, _, _, _, _, Some(kuvaus), Some(vuosi), Some(tyyppi), Some(index), source)  =>
       VapaamuotoinenSuoritus(henkiloOid,kuvaus, myontaja, vuosi, tyyppi, index, source)
+    case (myontaja, henkiloOid, vahvistettu, _, _, _, _, _, Some(kuvaus), Some(vuosi), Some(AmmatillisenKielikoeSuoritus.tyyppi), None, source)  =>
+      AmmatillisenKielikoeSuoritus(henkiloOid,kuvaus, myontaja, vuosi, source)
     case row => throw new InvalidSuoritusDataException(row)
   }
 
--- src/test/scala/fi/vm/sade/hakurekisteri/rest/support/SuoritusDeserializer.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/rest/support/SuoritusDeserializer.scala
@@ -3,7 +3,7 @@ package fi.vm.sade.hakurekisteri.rest.support
 import java.util.UUID
 
 import fi.vm.sade.hakurekisteri.suoritus.yksilollistaminen.Yksilollistetty
-import fi.vm.sade.hakurekisteri.suoritus.{Suoritus, VapaamuotoinenSuoritus, VirallinenSuoritus}
+import fi.vm.sade.hakurekisteri.suoritus.{AmmatillisenKielikoeSuoritus, Suoritus, VapaamuotoinenSuoritus, VirallinenSuoritus}
 import org.joda.time.LocalDate
 import org.json4s.JsonAST.{JInt, JString, _}
 import org.json4s.{CustomSerializer, Formats}
@@ -20,8 +20,6 @@ class SuoritusDeserializer extends CustomSerializer[Suoritus]((format: Formats) 
         case JNothing =>  rawSuoritus
         case jv: JValue =>  rawSuoritus.identify(jv.extract[UUID])
       }
-
-
   }, {
     implicit val form = format
     new SuoritusSerializer().serialize
@@ -36,10 +34,7 @@ object SuoritusDeserializer {
     val JString(lahde) = suoritus \ "source"
     suoritus \ "kuvaus" match {
       case JString(kuvaus) =>
-        val JInt(vuosi) = suoritus \ "vuosi"
-        val JString(tyyppi) = suoritus \ "tyyppi"
-        val JInt(index) = suoritus \ "index"
-        VapaamuotoinenSuoritus(henkilo: String, kuvaus: String, myontaja: String, vuosi.toInt: Int, tyyppi: String, index.toInt: Int, lahde: String)
+        extractSuoritusWithKuvaus(suoritus, henkilo, myontaja, lahde, kuvaus)
       case JNothing =>
         val JString(komo) = suoritus \ "komo"
         val JString(tila) = suoritus \ "tila"
@@ -65,6 +60,17 @@ object SuoritusDeserializer {
           vahv: Boolean,
           lahde: String)
       case _ => throw new IllegalArgumentException("unable to extract Suoritus kuvaus is of wrong type")
+    }
+  }
+
+  private def extractSuoritusWithKuvaus(suoritus: JValue, henkilo: String, myontaja: String, lahde: String, kuvaus: String): Suoritus = {
+    val JInt(vuosi) = suoritus \ "vuosi"
+    val JString(tyyppi) = suoritus \ "tyyppi"
+    tyyppi match {
+      case AmmatillisenKielikoeSuoritus.tyyppi => AmmatillisenKielikoeSuoritus(henkilo, kuvaus, myontaja, vuosi.toInt, lahde)
+      case x =>
+        val JInt(index) = suoritus \ "index"
+        VapaamuotoinenSuoritus(henkilo: String, kuvaus: String, myontaja: String, vuosi.toInt: Int, tyyppi: String, index.toInt: Int, lahde: String)
     }
   }
 }
