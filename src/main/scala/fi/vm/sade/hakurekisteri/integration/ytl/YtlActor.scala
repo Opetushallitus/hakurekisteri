6280c1c9acb53dd0a9bd64dd8655bca0b3377f54
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-09-13 12:32:30 +0300

    Iterate through 'kokelaat'

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlActor.scala
@@ -301,7 +301,7 @@ class YtlActor(henkiloActor: ActorRef, suoritusRekisteri: ActorRef, arvosanaReki
         val missing = batch.items.map(_.oid).toSet -- found
         for (problem <- missing) log.warning(s"Missing result from YTL for oid $problem in batch ${batch.id}")
         log.info(s"process returned ${found.size} results of ${batch.items.size} requested, started saving")
-        YtlDiff.writeKokelaatAsJson(parsed, "ytl-v1-kokelaat.json")
+        YtlDiff.writeKokelaatAsJson(parsed.flatten.toIterator, "ytl-v1-kokelaat.json")
 
       case Failure(t) if requested.isDefined => log.error(t, s"failure in fetching results for ${requested.get.id}")
 
--- src/main/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlIntegration.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/ytl/YtlIntegration.scala
@@ -4,6 +4,8 @@ import java.util.concurrent.Executors
 
 import akka.actor.ActorRef
 import fi.vm.sade.hakurekisteri.integration.hakemus.{FullHakemus, HakemusService}
+import org.apache.commons.io.IOUtils
+import org.slf4j.LoggerFactory
 
 import scala.collection.Set
 import scala.concurrent._
@@ -12,6 +14,7 @@ import scala.util.{Failure, Success}
 class YtlIntegration(ytlHttpClient: YtlHttpFetch,
                      hakemusService: HakemusService,
                      suoritusRekisteri: ActorRef) {
+  private val logger = LoggerFactory.getLogger(getClass)
 
   var activeHakuOids = Set[String]()
   implicit val ec = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(5))
@@ -28,16 +31,18 @@ class YtlIntegration(ytlHttpClient: YtlHttpFetch,
 
         ytlHttpClient.fetch(hetuToPersonOid.keys.toList) match {
           case Left(e: Throwable) =>
-            // TODO
-          case Right(students) =>
-            val kokelaat: Iterator[Some[Kokelas]] = students.map { student =>
-              Some(StudentToKokelas.convert(hetuToPersonOid.get(student.ssn).get, student))
-            }
-            YtlDiff.writeKokelaatAsJson(kokelaat.toSeq, "ytl-v2-kokelaat.json")
+            logger.error(s"failed to fetch YTL data: ${e.getMessage}")
+            throw e
+          case Right((zip, students)) =>
+            YtlDiff.writeKokelaatAsJson(students.map { student =>
+              StudentToKokelas.convert(hetuToPersonOid.get(student.ssn).get, student)
+            }, "ytl-v2-kokelaat.json")
+            IOUtils.closeQuietly(zip)
         }
 
       case Failure(e: Throwable) =>
-        // TODO
+        logger.error(s"failed to fetch 'henkilotunnukset' from hakemus service: ${e.getMessage}")
+        throw e
     }
 
   }
