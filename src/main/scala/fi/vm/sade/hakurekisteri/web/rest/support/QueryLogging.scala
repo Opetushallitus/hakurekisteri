4495ae2a178d4e55c04eac92c54d1815393d379d
Jani Rahkola <jani.rahkola@reaktor.com>
2016-09-14 15:40:05 +0300

    Truncate long slow query messages

--- src/main/scala/fi/vm/sade/hakurekisteri/web/rest/support/QueryLogging.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/web/rest/support/QueryLogging.scala
@@ -15,7 +15,13 @@ trait QueryLogging { this: HakuJaValintarekisteriStack =>
   def logQuery(q: Any, t0: Long, f: Future[_])(implicit ec: ExecutionContext): Unit = {
     f.onComplete(t => {
       val requestLength = Platform.currentTime - t0
-      val message = s"query $q took $requestLength ms, result ${result(t)}"
+      val queryStr = if (q.toString.length > 500) {
+        s"${q.toString.take(500)}...(truncated from ${q.toString.length} chars)"
+      } else {
+        q.toString
+      }
+
+      val message = s"Query $queryStr took $requestLength ms, result ${result(t)}"
       requestLength match {
         case reallySlow if requestLength > 1000  => logger.warning(message)
         case slow if requestLength > 100 => logger.info(message)
