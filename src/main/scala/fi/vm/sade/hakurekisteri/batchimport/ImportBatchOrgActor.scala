2bf35ba94c1b4f2cdd111eb6b06066f92f6dd89a
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-12-09 10:32:15 +0200

    create schema for new table and create schema code refactored

--- src/main/scala/fi/vm/sade/hakurekisteri/batchimport/ImportBatchOrgActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/batchimport/ImportBatchOrgActor.scala
@@ -4,9 +4,11 @@ import java.util.UUID
 
 import akka.actor.Actor.Receive
 import akka.actor.{ActorRef, ActorLogging, Actor}
+import fi.vm.sade.hakurekisteri.rest.support.JDBCUtil
 import fi.vm.sade.hakurekisteri.storage.ResourceService
 import fi.vm.sade.hakurekisteri.storage.repository.Repository
 import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriDriver.api._
+import slick.jdbc.meta.MTable
 import scala.compat.Platform
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.Duration
@@ -15,7 +17,7 @@ import scala.concurrent.{Await, ExecutionContext, Future}
 import slick.dbio
 import slick.driver.JdbcActionComponent
 import slick.driver.JdbcTypesComponent._
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 import akka.pattern.{ask, pipe}
 
 case class QueryImportBatchReferences(organisations: Set[String])
@@ -24,7 +26,8 @@ case class ReferenceResult(references: Seq[UUID])
 class ImportBatchOrgActor(db: Database) extends Actor with ActorLogging {
   implicit val executionContext: ExecutionContext = context.dispatcher
   val table = TableQuery[ImportBatchOrgTable]
-
+  JDBCUtil.createSchemaForTable(table, db)
+  
   override def receive: Receive = {
     case i: ImportBatchOrg =>
       log.info(s"Saving import batch organisation ${i.oid}!")
@@ -34,10 +37,16 @@ class ImportBatchOrgActor(db: Database) extends Actor with ActorLogging {
       //val query = sql"select resource_id,oid from import_batch_org where resource_id in (select resource_id from import_batch_org where oid in ($o))".as[(String,String)]
       val subQuery = table.filter(_.oid.inSet(orgs)).map(_.resourceId)
       val query = table.filter(_.resourceId.in(subQuery)).result
-      db.run(query).map(result => {
+      Try(Await.result(db.run(query).map(result => {
         val byUUID: Map[UUID, Set[String]] = result.groupBy(_._1).mapValues(_.map(_._2).toSet)
         byUUID.filter(_._2.subsetOf(orgs)).keys.toSeq
-      }).map(ReferenceResult(_)).pipeTo(sender)
+      }).map(ReferenceResult(_)), 10.seconds)) match {
+        case Success(result) =>
+          sender ! result
+        case Failure(exception) =>
+          log.error(exception, s"Could not fetch import_batch_org's for ${orgs}!")
+          sender ! exception
+      }
     case _ =>
   }
 
