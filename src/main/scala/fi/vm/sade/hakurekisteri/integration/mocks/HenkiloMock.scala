d47e90752b7dd2f1103457f9e41c601154623fcf
Tommi Helander <tommi.helander@gofore.com>
2017-01-16 15:05:10 +0200

    Add mock data for linked person

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/mocks/HenkiloMock.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/mocks/HenkiloMock.scala
@@ -12,6 +12,7 @@ object HenkiloMock {
   def henkilotByHenkiloOidList(oids: List[String]): String = {
     if (oids.contains("1.2.246.562.24.71944845619") && oids.size == 1) getResourceJson("/mock-data/henkilo/henkilo-aarne-oidList.json")
     else if (oids.contains("1.2.246.562.24.49719248091") && oids.size == 5) getResourceJson("/mock-data/henkilo/henkilo-all-oidList.json")
+    else if (oids.contains("1.2.246.562.24.58099330694") && oids.size == 1) getResourceJson("/mock-data/henkilo/henkilo-matti-oidList.json")
     else "[]"
   }
 
@@ -25,6 +26,7 @@ object HenkiloMock {
   def getHenkiloByOid(oid: String): String = {
     if (oid.equals("1.2.246.562.24.71944845619")) getResourceJson("/mock-data/henkilo/henkilo-aarne.json")
     else if (oid.equals("1.2.246.562.24.98743797763")) getResourceJson("/mock-data/henkilo/henkilo-tyyne.json")
+    else if (oid.equals("1.2.246.562.24.58099330694")) getResourceJson("/mock-data/henkilo/henkilo-matti.json")
     else getResourceJson("/mock-data/henkilo/henkilo-empty.json")
   }
 
