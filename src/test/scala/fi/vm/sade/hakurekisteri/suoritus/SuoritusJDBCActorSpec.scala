2a6daa2c723dbabd69b110f8780953836f2bc24c
Tommi Helander <tommi.helander@gofore.com>
2017-01-20 10:15:42 +0200

    Add tests for SuoritusJDBCActor

--- src/test/scala/fi/vm/sade/hakurekisteri/suoritus/SuoritusJDBCActorSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/suoritus/SuoritusJDBCActorSpec.scala
@@ -11,15 +11,14 @@ import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriDriver.api._
 import fi.vm.sade.hakurekisteri.rest.support.JDBCJournal
 import fi.vm.sade.hakurekisteri.tools.ItPostgres
 import org.joda.time.LocalDate
-import org.scalatest.BeforeAndAfterEach
-import org.scalatra.test.scalatest.ScalatraFunSuite
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FlatSpec, Matchers}
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.duration.{Duration, _}
+import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 
-class SuoritusJDBCActorSpec extends ScalatraFunSuite with BeforeAndAfterEach {
+class SuoritusJDBCActorSpec extends FlatSpec with BeforeAndAfterEach with  BeforeAndAfterAll with Matchers {
   val logger = LoggerFactory.getLogger(getClass)
 
   val linkedOid1: String = MockOppijaNumeroRekisteri.linkedTestPersonOids.head
@@ -39,18 +38,41 @@ class SuoritusJDBCActorSpec extends ScalatraFunSuite with BeforeAndAfterEach {
     run(suoritusrekisteri ? originalSuoritus)
   }
 
-  override protected def afterEach(): Unit = {
+  override protected def afterAll(): Unit = {
     Await.result(system.terminate(), 15.seconds)
     database.close()
   }
 
+  behavior of "SuoritusJDBCActor"
 
-  test("x") {
-    run(suoritusrekisteri ? originalSuoritus.copy(henkilo = linkedOid2, tila = "VALMIS"))
-    val henkilo1Tilat = run(database.run(sql"select tila from suoritus where henkilo_oid = $linkedOid1".as[String]))
-    val henkilo2Tilat = run(database.run(sql"select tila from suoritus where henkilo_oid = $linkedOid2".as[String]))
+  it should "update suoritus with linked person oid retains original person oid in database" in {
+    val original = run((suoritusrekisteri ? SuoritusQuery(henkilo = Some(linkedOid2))).mapTo[Seq[VirallinenSuoritus]]).head
+    val newCopy = originalSuoritus.copy(henkilo = linkedOid2, tila = "VALMIS")
+    println("original: " + original)
+    println("Kopio: " + newCopy)
+    run(suoritusrekisteri ? newCopy)
+    val henkilo1Tilat = run(database.run(sql"select tila from suoritus where henkilo_oid = $linkedOid1 and current".as[String]))
+    val henkilo2Tilat = run(database.run(sql"select tila from suoritus where henkilo_oid = $linkedOid2 and current".as[String]))
     henkilo1Tilat should have length 1
     henkilo2Tilat should have length 0
+  }
+
+  it should "update suoritus with original person oid updates suoritus" in {
+    run(suoritusrekisteri ? originalSuoritus.copy(tila = "VALMIS"))
+    val henkilo1Tilat = run(database.run(sql"select tila from suoritus where henkilo_oid = $linkedOid1 and current".as[String]))
+    henkilo1Tilat should have length 1
+    henkilo1Tilat.head should equal("VALMIS")
+  }
+
+  it should "create new suoritus when updating field included in deduplication" in {
+    val s: VirallinenSuoritus = run((suoritusrekisteri ? SuoritusQuery(henkilo = Some(linkedOid2))).mapTo[Seq[VirallinenSuoritus]]).head
+    run(suoritusrekisteri ? s.copy(komo = "uusikomo"))
+    val henkilo1Komot = run(database.run(sql"select komo from suoritus where henkilo_oid = $linkedOid1 and current".as[String]))
+    val henkilo2Komot = run(database.run(sql"select komo from suoritus where henkilo_oid = $linkedOid2 and current".as[String]))
+    henkilo1Komot should have length 1
+    henkilo2Komot should have length 1
+    henkilo1Komot.head should equal("komo")
+    henkilo2Komot.head should equal("uusikomo")
   }
 
   private def run[T](f: Future[T]): T = Await.result(f, atMost = timeout.duration)
