f903a68f6997e71f7fc1870238cd37dd32a9059c
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-01-26 19:11:26 +0200

    'siirtotiedoston jonotus' js libs

---
+++ src/main/scala/fi/vm/sade/hakurekisteri/web/jonotus/JonotusResource.scala
@@ -0,0 +1,42 @@
+package fi.vm.sade.hakurekisteri.web.jonotus
+
+import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriJsonSupport
+import fi.vm.sade.hakurekisteri.web.rest.support.{Security, SecuritySupport}
+import org.scalatra.SessionSupport
+import org.scalatra._
+import org.scalatra.atmosphere._
+import org.slf4j.LoggerFactory
+import scalate.ScalateSupport
+import org.scalatra.json.{JValueResult, JacksonJsonSupport}
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import org.json4s.jackson.Serialization.{write}
+import org.json4s._
+
+case class Sijoitus(sijoitus: Int)
+
+class JonotusResource(implicit val security: Security) extends ScalatraServlet
+  with ScalateSupport with JValueResult
+  with JacksonJsonSupport with SessionSupport
+  with AtmosphereSupport with SecuritySupport {
+
+  private val logger = LoggerFactory.getLogger(classOf[JonotusResource])
+
+  atmosphere("/") {
+    new AtmosphereClient {
+      def receive = {
+        case Connected =>
+          logger.info(s"User connected $currentUser!")
+        case Disconnected(disconnector, couldContainErrors) =>
+          couldContainErrors.foreach(logger.error("Disconnected with exception!",_))
+        case JsonMessage(json) => send(write(Sijoitus(54)))
+          logger.info(s"User $currentUser sent message $json")
+        case event =>
+          logger.error(s"Unexpected event in 'siirtotiedoston jonotus' $event")
+      }
+    }
+  }
+
+  override protected implicit def jsonFormats: Formats = HakurekisteriJsonSupport.format
+
+}
