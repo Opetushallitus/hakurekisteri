866b1f26d0977513e5fbec457bb3aa70b773be58
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-01-31 13:37:46 +0200

    atmosphere replaced with polling frontend

--- src/main/scala/fi/vm/sade/hakurekisteri/web/jonotus/AsiakirjaResource.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/web/jonotus/AsiakirjaResource.scala
@@ -1,38 +1,31 @@
 package fi.vm.sade.hakurekisteri.web.jonotus
 
-import java.io.OutputStream
-
 import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
-import fi.vm.sade.hakurekisteri.hakija.{ExcelUtilV2, JSONHakijat}
 import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriJsonSupport
 import fi.vm.sade.hakurekisteri.web.HakuJaValintarekisteriStack
-import fi.vm.sade.hakurekisteri.web.hakija.{HakijaResourceSupport, HakijaSwaggerApi}
+import fi.vm.sade.hakurekisteri.web.hakija.HakijaResourceSupport
 import fi.vm.sade.hakurekisteri.web.rest.support._
-import org.scalatra.FutureSupport
-import org.scalatra.json.JacksonJsonSupport
-import org.scalatra.swagger.Swagger
 
-import scala.concurrent.ExecutionContext
-import scala.reflect.ClassTag
-
-class AsiakirjaResource(jono: Siirtotiedostojono)(implicit system: ActorSystem, sw: Swagger, val security: Security, val ct: ClassTag[JSONHakijat])
-  extends HakuJaValintarekisteriStack with HakurekisteriJsonSupport with JacksonJsonSupport with FutureSupport with SecuritySupport with ExcelSupport[JSONHakijat] with DownloadSupport with QueryLogging with HakijaResourceSupport  {
+class AsiakirjaResource(jono: Siirtotiedostojono)(implicit system: ActorSystem, val security: Security)
+  extends HakuJaValintarekisteriStack with HakurekisteriJsonSupport with SecuritySupport with DownloadSupport with HakijaResourceSupport {
 
   override val logger: LoggingAdapter = Logging.getLogger(system, this)
 
-  override protected implicit def executor: ExecutionContext = system.dispatcher
-
-  override val streamingRender: (OutputStream, JSONHakijat) => Unit = (out, hakijat) => {
-    ExcelUtilV2.write(out, hakijat)
-  }
-
-  get("/asiakirja/:id") {
+  get("/:id") {
     (currentUser, params.get("id")) match {
       case (Some(user), Some(id)) =>
         logger.info(s"User $user downloaded file $id")
+        jono.getAsiakirjaWithId(id) match {
+          case Some((format, bytes)) =>
+            contentType = getContentType(format)
+            setContentDisposition(format, response, "hakijat")
+            response.outputStream.write(bytes)
+          case _ =>
+            halt(status = 404, body=s"$id not found!")
+        }
       case _ =>
-        logger.error(s"No such file or user $currentUser not authorized!")
+        halt(status = 401, body=s"User not authorized!")
     }
   }
 }
