e632037d49c9762a3c6644a7c7336b32fd604ddf
teo.mertanen <teo.mertanen@gofore.com>
2017-04-25 16:47:08 +0300

    get suoritukset locally and pass them to valintalaskentakooste directly

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/kooste/KoosteService.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/kooste/KoosteService.scala
@@ -5,12 +5,13 @@ import akka.actor.ActorSystem
 import akka.event.Logging
 import fi.vm.sade.hakurekisteri.integration.hakemus.FullHakemus
 import fi.vm.sade.hakurekisteri.integration.VirkailijaRestClient
+import fi.vm.sade.hakurekisteri.oppija.Oppija
 
 import scala.concurrent.Future
 
 trait IKoosteService {
-  def getSuoritukset(hakuOid: String, hakemus: FullHakemus): Future[Map[String,String]]
-  def getSuoritukset(hakuOid: String, hakemukset: Seq[FullHakemus]): Future[Map[String,Map[String,String]]]
+  def getSuoritukset(hakuOid: String, oppija: Oppija, hakemus: FullHakemus): Future[Map[String,String]]
+  def getSuoritukset(hakuOid: String, oppijat: Seq[Oppija], hakemukset: Seq[FullHakemus]): Future[Map[String,Map[String,String]]]
 }
 
 class KoosteService(restClient: VirkailijaRestClient, pageSize: Int = 200)
@@ -21,20 +22,20 @@ class KoosteService(restClient: VirkailijaRestClient, pageSize: Int = 200)
 
   private val logger = Logging.getLogger(system, this)
 
-  def getSuoritukset(hakuOid: String, hakemus: FullHakemus): Future[Map[String,String]] = {
+  def getSuoritukset(hakuOid: String, oppija: Oppija, hakemus: FullHakemus): Future[Map[String,String]] = {
       val opiskelijaOid = hakemus.personOid.get
       restClient.postObject[FullHakemus, Map[String,String]]("valintalaskentakoostepalvelu.suorituksetByOpiskelijaOid", hakuOid, opiskelijaOid)(200, hakemus)
   }
 
-  def getSuoritukset(hakuOid: String, hakemukset: Seq[FullHakemus]): Future[Map[String,Map[String,String]]] = {
-    val hakemusHakijat: Seq[HakemusHakija] = hakemukset.map(h => HakemusHakija(h.personOid.get, h))
+  def getSuoritukset(hakuOid: String, oppijat: Seq[Oppija], hakemukset: Seq[FullHakemus]): Future[Map[String,Map[String,String]]] = {
+    val hakemusHakijat: Seq[HakemusHakija] = (hakemukset.zip(oppijat)).map{case (h,o) => HakemusHakija(o, h)}
     restClient.postObject[Seq[HakemusHakija], Map[String,Map[String,String]]]("valintalaskentakoostepalvelu.suorituksetByOpiskelijaOid", hakuOid)(200, hakemusHakijat)
   }
 
-  case class HakemusHakija(opiskelijaOid: String, hakemus: FullHakemus)
+  case class HakemusHakija(oppija: Oppija, hakemus: FullHakemus)
 }
 
 class KoosteServiceMock extends IKoosteService {
-  override def getSuoritukset(hakuOid: String, hakemus: FullHakemus): Future[Map[String,String]] = Future.successful(Map[String,String]())
-  override def getSuoritukset(hakuOid: String, hakemukset: Seq[FullHakemus]): Future[Map[String,Map[String,String]]] = Future.successful(Map[String,Map[String,String]]())
+  override def getSuoritukset(hakuOid: String, oppija: Oppija, hakemus: FullHakemus): Future[Map[String,String]] = Future.successful(Map[String,String]())
+  override def getSuoritukset(hakuOid: String, oppijat: Seq[Oppija], hakemukset: Seq[FullHakemus]): Future[Map[String,Map[String,String]]] = Future.successful(Map[String,Map[String,String]]())
 }
