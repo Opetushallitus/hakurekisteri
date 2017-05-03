f5a151e1b3a37adde1e00ff378d98e05e025e7c2
Joni Mäkelä <joni.makela@gofore.com>
2017-05-03 11:52:24 +0300

    Added validation for post path - if ensikertailaisuudet param is true, hakuOid also has to be defined. Otherwise the server will respond with bad request status.

--- src/main/scala/fi/vm/sade/hakurekisteri/web/oppija/OppijaResource.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/web/oppija/OppijaResource.scala
@@ -98,14 +98,18 @@ class OppijaResource(val rekisterit: Registers, val hakemusService: IHakemusServ
     }
     val hakuOid: Option[String] = params.get("haku")
 
-    new AsyncResult() {
-      override implicit def timeout: Duration = 500.seconds
+    if (ensikertalaisuudet && hakuOid.getOrElse("").isEmpty) {
+      BadRequest(null,null,"Haku has to be defined if ensikertalaisuudet is true")
+    } else {
+      new AsyncResult() {
+        override implicit def timeout: Duration = 500.seconds
 
-      private val oppijat = fetchOppijat(henkilot, ensikertalaisuudet, HakemusQuery(hakuOid))
+        private val oppijat = fetchOppijat(henkilot, ensikertalaisuudet, HakemusQuery(hakuOid))
 
-      logQuery(Map("henkilot" -> henkilot, "haku" -> hakuOid), t0, oppijat)
+        logQuery(Map("henkilot" -> henkilot, "haku" -> hakuOid), t0, oppijat)
 
-      override val is: Future[_] = oppijat
+        override val is: Future[_] = oppijat
+      }
     }
   }
 
