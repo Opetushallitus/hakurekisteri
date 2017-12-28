658d4d10dbdb220876d5a6d41edb35914b02ea9a
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2017-12-28 14:40:14 +0200

    Format
    No functional changes.

--- src/test/scala/fi/vm/sade/hakurekisteri/tools/ExcelTools.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/tools/ExcelTools.scala
@@ -1,30 +1,23 @@
 package fi.vm.sade.hakurekisteri.tools
 
-
 import org.apache.poi.ss.{usermodel => poi}
 
 import scala.io.Source
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 
 trait ExcelTools {
-
-
-
   import scala.language.implicitConversions
 
+  case class WorkbookData(sheets: (String, String)*) {
 
+    case class RichWorkbook(workbook: poi.Workbook) {
 
-
-  case class WorkbookData(sheets: (String, String)*)  {
-
-    case class RichWorkbook(workbook:poi.Workbook) {
-
-
-      def readData(data:String): Array[Array[String]] =
+      def readData(data: String): Array[Array[String]] =
         Source.fromString(data.trim.stripMargin).getLines().toArray.map(_.split("\\|").map(_.trim))
 
-      def addSheet(name:String)(data:String): poi.Workbook = addSheet(name, readData(data))
-      def addSheet(name:String, stringTable: Array[Array[String]]): poi.Workbook = {
+      def addSheet(name: String)(data: String): poi.Workbook = addSheet(name, readData(data))
+
+      def addSheet(name: String, stringTable: Array[Array[String]]): poi.Workbook = {
         val sheet: poi.Sheet = workbook.createSheet(name)
         for (
           (row, index) <- stringTable.zipWithIndex
@@ -38,9 +31,9 @@ trait ExcelTools {
       }
     }
 
-    implicit def wb2rwb(workbook:poi.Workbook): RichWorkbook = RichWorkbook(workbook)
+    implicit def wb2rwb(workbook: poi.Workbook): RichWorkbook = RichWorkbook(workbook)
 
-    val toExcel:poi.Workbook = {
+    val toExcel: poi.Workbook = {
       val result = new HSSFWorkbook()
       for (
         (name, data) <- sheets
@@ -48,5 +41,4 @@ trait ExcelTools {
       result
     }
   }
-
 }
