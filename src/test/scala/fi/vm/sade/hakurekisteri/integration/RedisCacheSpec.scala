a2a4f0c94802e273875b93b92b4b4bc0f4b809ca
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-02-21 11:14:26 +0200

    BUG-1645 : Remove duplicate retrievals
    A proof of concept kind of solution for the
    Redis cache.
    Not in use yet in the client actors.
    Needs some refactoring and maybe fixing some details.

--- src/test/scala/fi/vm/sade/hakurekisteri/integration/RedisCacheSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/RedisCacheSpec.scala
@@ -4,13 +4,17 @@ import akka.actor.ActorSystem
 import fi.vm.sade.hakurekisteri.integration.cache.CacheFactory
 import fi.vm.sade.scalaproperties.OphProperties
 import fi.vm.sade.utils.tcp.PortChecker
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, Ignore, Matchers}
+import org.mockito.Mockito.{times, verify, when}
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 import redis.embedded.RedisServer
 
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
-class RedisCacheSpec extends FlatSpec with Matchers with ActorSystemSupport with BeforeAndAfterAll with DispatchSupport {
+class RedisCacheSpec extends FlatSpec with Matchers with ActorSystemSupport with BeforeAndAfterAll with DispatchSupport with MockitoSugar {
 
   val port = PortChecker.findFreeLocalPort
   val redisServer = new RedisServer(port)
@@ -27,6 +31,10 @@ class RedisCacheSpec extends FlatSpec with Matchers with ActorSystemSupport with
   val cacheKey = "foo"
   val cacheEntry = "bar"
   val cacheEntryF = Future.successful(cacheEntry)
+
+  val concurrencyTestLoopCount: Int = 5
+  val concurrencyTestParallelRequestCount: Int = 10
+  val concurrencyTestResultsWaitDuration: Duration = 10.seconds
 
   it should "add an entry to cache" in {
    withSystem(
@@ -102,6 +110,34 @@ class RedisCacheSpec extends FlatSpec with Matchers with ActorSystemSupport with
         cache4 contains(cacheKey) should be(true)
       }
     )
+  }
+
+  it should "only do one backend call per missing value with loader accepting get API" in {
+    1.to(concurrencyTestLoopCount).foreach { counter =>
+      withSystem(
+         implicit system => {
+           val cache = redisCacheFactory.getInstance[String,String](3.minutes.toMillis, getClass, s"prefixLoaderApi$counter")
+
+           val mockLoader: String => Future[Option[String]] = mock[String => Future[Option[String]]]
+           when(mockLoader.apply(cacheKey)).thenAnswer(new Answer[Future[Option[String]]] {
+             override def answer(invocation: InvocationOnMock): Future[Option[String]] = {
+               Thread.sleep(10)
+               Future.successful(Some(cacheEntry))
+             }
+           })
+
+           val results = 1.to(concurrencyTestParallelRequestCount).par.map(_ => cache.get(cacheKey, mockLoader)).map(Await.result(_, concurrencyTestResultsWaitDuration))
+           results should have size concurrencyTestParallelRequestCount
+           results.foreach(_ should be(Some(cacheEntry)))
+
+           cache.contains(cacheKey) should be(true)
+
+           Await.result(cache.get(cacheKey), 1.second) should be (cacheEntry)
+
+           verify(mockLoader, times(1)).apply(cacheKey)
+         }
+       )
+    }
   }
 
   override def afterAll() = {
