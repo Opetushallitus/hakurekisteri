da4f7b3af3e31392217ff0371fe89aa081328969
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-02-21 15:18:16 +0200

    BUG-1645 : Make cache lock max time configurable

--- src/test/scala/fi/vm/sade/hakurekisteri/integration/RedisCacheSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/RedisCacheSpec.scala
@@ -23,6 +23,7 @@ class RedisCacheSpec extends FlatSpec with Matchers with ActorSystemSupport with
     .addDefault("suoritusrekisteri.cache.redis.enabled", "true")
     .addDefault("suoritusrekisteri.cache.redis.host", "localhost")
     .addDefault("suoritusrekisteri.cache.redis.numberOfWaitersToLog", "5")
+    .addDefault("suoritusrekisteri.cache.redis.cacheItemLockMaxDurationSeconds", "6")
     .addDefault("suoritusrekisteri.cache.redis.port", s"${port}"))(system)
 
   override def beforeAll() = {
