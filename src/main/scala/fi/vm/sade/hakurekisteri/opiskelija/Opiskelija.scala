16dc014c502a2b5863dcffe0f69f3c216080d372
Mikko Siukola <mikko.siukola@cybercom.com>
2018-03-05 17:18:22 +0200

    Testataan historiankorjausta tuotannossa, ysikorjaukset vain logille (ei poistoja). Haetaan runsaan päivän verran dataa feb 27-feb28.

--- src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/Opiskelija.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/opiskelija/Opiskelija.scala
@@ -9,7 +9,7 @@ import org.joda.time.DateTime
 case class Opiskelija(oppilaitosOid: String, luokkataso: String, luokka: String, henkiloOid: String, alkuPaiva: DateTime, loppuPaiva: Option[DateTime] = None, source: String) extends UUIDResource[Opiskelija] {
 
   if (loppuPaiva.isDefined)
-    require(!loppuPaiva.get.isBefore(alkuPaiva), "loppuPaiva must be after alkuPaiva. Alku: "+alkuPaiva+", loppu: "+loppuPaiva)
+    require(!loppuPaiva.get.isBefore(alkuPaiva), "HenkiloOid: " + henkiloOid + " : loppuPaiva must be after alkuPaiva. Alku: "+alkuPaiva+", loppu: "+loppuPaiva)
 
   override def identify(identity: UUID): Opiskelija with Identified[UUID] = new IdentifiedOpiskelija(this, identity)
 
