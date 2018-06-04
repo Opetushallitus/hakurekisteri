1c8b2cc7910941610821f474eca9bbf275ddf39d
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-06-04 15:49:45 +0300

    BUG-1771 : Store myonnetty only when after valmistuminen

--- src/test/scala/fi/vm/sade/hakurekisteri/integration/koski/ArvosanaMyonnettyParserSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/koski/ArvosanaMyonnettyParserSpec.scala
@@ -17,7 +17,32 @@ class ArvosanaMyonnettyParserSpec extends FlatSpec with ShouldMatchers with Haku
     val biologia = osaSuoritukset.find(_.koulutusmoduuli.tunniste.exists(_.koodiarvo == "BI"))
     biologia shouldBe defined
     val determinedDate = ArvosanaMyonnettyParser.findArviointipäivä(biologia.get, "personOid", "aine", new LocalDate(1970, 1, 1))
-    determinedDate should be(new LocalDate(2018, 12, 31))
+    determinedDate should be(Some(new LocalDate(2018, 12, 31)))
+  }
+
+  it should "only take arvosana specific myonto dates when they are after suoritus valmistuminen date" in {
+    val osaSuoritukset: Seq[KoskiOsasuoritus] = readOsasuoritukset("henkilo_from_koski.json")
+
+    val suorituksenValmistumisPvm: LocalDate = new LocalDate(2018, 6, 2)
+
+    val biologia = osaSuoritukset.find(_.koulutusmoduuli.tunniste.exists(_.koodiarvo == "BI"))
+    biologia shouldBe defined
+    val biologiaDate = ArvosanaMyonnettyParser.findArviointipäivä(biologia.get, "personOid", "aine", suorituksenValmistumisPvm)
+    biologiaDate should be(Some(new LocalDate(2018, 12, 31)))
+
+    val terveystieto = osaSuoritukset.find(_.koulutusmoduuli.tunniste.exists(_.koodiarvo == "TE"))
+    terveystieto shouldBe defined
+    val terveystietoDate = ArvosanaMyonnettyParser.findArviointipäivä(terveystieto.get, "personOid", "terveystieto", suorituksenValmistumisPvm)
+    terveystietoDate should be(None)
+  }
+
+  it should "not store arvosana specific myonto dates when they are null in Koski" in {
+    val osaSuoritukset: Seq[KoskiOsasuoritus] = readOsasuoritukset("henkilo_without_arviointipvm_from_koski.json")
+
+    val ensimmainenVierasKieli = osaSuoritukset.find(_.koulutusmoduuli.tunniste.exists(_.koodiarvo == "BI"))
+    ensimmainenVierasKieli shouldBe defined
+    val vieraanKielenMyontoPvm = ArvosanaMyonnettyParser.findArviointipäivä(ensimmainenVierasKieli.get, "personOid", "bilsa", new LocalDate(2018, 6, 19))
+    vieraanKielenMyontoPvm should be(None)
   }
 
   private def readOsasuoritukset(fileNameInSamePackage: String) = {
