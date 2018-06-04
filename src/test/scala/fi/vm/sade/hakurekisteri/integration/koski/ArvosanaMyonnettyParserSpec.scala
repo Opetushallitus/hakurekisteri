6d17e00b2a0aecd91e3e940d25af3b00b284d00f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-06-04 15:30:14 +0300

    BUG-1771 : Use classpath, extract methods

--- src/test/scala/fi/vm/sade/hakurekisteri/integration/koski/ArvosanaMyonnettyParserSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/koski/ArvosanaMyonnettyParserSpec.scala
@@ -2,26 +2,37 @@ package fi.vm.sade.hakurekisteri.integration.koski
 
 
 import fi.vm.sade.hakurekisteri.rest.support.HakurekisteriJsonSupport
+import org.apache.commons.io.IOUtils
 import org.joda.time.LocalDate
 import org.json4s.jackson.JsonMethods
 import org.scalatest.{FlatSpec, ShouldMatchers}
+import org.springframework.core.io.ClassPathResource
+
+import scala.collection.JavaConverters._
 
 class ArvosanaMyonnettyParserSpec extends FlatSpec with ShouldMatchers with HakurekisteriJsonSupport with JsonMethods {
-  private val jsonString: String = scala.io.Source.fromFile("src/test/resources/fi/vm/sade/hakurekisteri/integration/koski/henkilo_from_koski.json").getLines().mkString
-
   it should "parse dates for arvosanas" in {
-    val testDataJson = parse(jsonString)
-    val koskiHenkiloContainer = testDataJson.extract[KoskiHenkiloContainer]
-
-    val osaSuoritukset: Seq[KoskiOsasuoritus] = for {
-      opiskeluoikeus <- koskiHenkiloContainer.opiskeluoikeudet
-      suoritus <- opiskeluoikeus.suoritukset
-      osasuoritus <- suoritus.osasuoritukset
-    } yield osasuoritus
+    val osaSuoritukset: Seq[KoskiOsasuoritus] = readOsasuoritukset("henkilo_from_koski.json")
 
     val biologia = osaSuoritukset.find(_.koulutusmoduuli.tunniste.exists(_.koodiarvo == "BI"))
     biologia shouldBe defined
     val determinedDate = ArvosanaMyonnettyParser.findArviointipäivä(biologia.get, "personOid", "aine", new LocalDate(1970, 1, 1))
     determinedDate should be(new LocalDate(2018, 12, 31))
+  }
+
+  private def readOsasuoritukset(fileNameInSamePackage: String) = {
+    val jsonString: String = readFileFromClasspath(fileNameInSamePackage)
+    val koskiHenkiloContainer = parse(jsonString).extract[KoskiHenkiloContainer]
+
+    for {
+      opiskeluoikeus <- koskiHenkiloContainer.opiskeluoikeudet
+      suoritus <- opiskeluoikeus.suoritukset
+      osasuoritus <- suoritus.osasuoritukset
+    } yield osasuoritus
+  }
+
+  private def readFileFromClasspath(fileNameInSamePackage: String) = {
+    val classPathResource = new ClassPathResource(fileNameInSamePackage, getClass)
+    IOUtils.readLines(classPathResource.getInputStream, "UTF-8").asScala.mkString
   }
 }
