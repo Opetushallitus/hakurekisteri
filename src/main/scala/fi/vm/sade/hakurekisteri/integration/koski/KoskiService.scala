6dce37c379417a2153e409f35d449fb9da676da6
Mikko Siukola <mikko.siukola@cybercom.com>
2019-01-25 16:37:48 +0200

    Haetaan vähän vanhempiakin muutoksia

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiService.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiService.scala
@@ -78,7 +78,7 @@ class KoskiService(virkailijaRestClient: VirkailijaRestClient,
         //val timestamp: Option[String] = if (!cursor.isDefined) Some(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm")
         //  .format(new Date(Platform.currentTime - TimeUnit.DAYS.toMillis(180))))
         //else None
-        val timestamp: Option[String] = if (!cursor.isDefined) Some("2018-06-01T00:00:00+02:00") else None
+        val timestamp: Option[String] = if (!cursor.isDefined) Some("2015-06-01T00:00:00+02:00") else None
         val params = SearchParamsWithCursor(timestamp, cursor)
         logger.info("refreshChangedOppijasFromKoski active, making call with params: {}", params)
         fetchChangedOppijas(params).onComplete {
