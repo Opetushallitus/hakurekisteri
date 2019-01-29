7ae502186d934a352253d49923d89cc3196c9772
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-01-29 14:28:38 +0200

    OK-227 : Set hourly cron temporarily

--- src/main/scala/support/Integrations.scala
+++ src/main/scala/support/Integrations.scala
@@ -214,7 +214,8 @@ class BaseIntegrations(rekisterit: Registers,
 
   val koskiCronJob = OphUrlProperties.getProperty("suoritusrekisteri.koski.update.cronJob")
   quartzScheduler.scheduleJob(lambdaJob(koskiService.updateAktiivisetHaut()),
-    newTrigger().startNow().withSchedule(cronSchedule("0 0 0 * * ?")).build())
+    // every day at midnight: newTrigger().startNow().withSchedule(cronSchedule("0 0 0 * * ?")).build())
+      newTrigger().startNow().withSchedule(cronSchedule("0 0 * * * ?")).build())
   //TODO: This if for dev purposes. Schedule real trigger cron when done.
     //newTrigger().startNow().withSchedule(cronSchedule(koskiCronJob)).build())
   override val hakemusBasedPermissionChecker: HakemusBasedPermissionCheckerActorRef = new HakemusBasedPermissionCheckerActorRef(system.actorOf(Props(new HakemusBasedPermissionCheckerActor(hakuAppPermissionCheckerClient, ataruPermissionCheckerClient, organisaatiot))))
