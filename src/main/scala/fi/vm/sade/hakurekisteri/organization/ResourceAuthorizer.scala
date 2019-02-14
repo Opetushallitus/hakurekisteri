441746e1b4418b435600f786f8645bfb6a22a774
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-02-14 14:58:16 +0200

    SEC-68 : Log strange admin user case
    This appeared in production but I have not been able to reproduce it in
    my development environment yet.

--- src/main/scala/fi/vm/sade/hakurekisteri/organization/ResourceAuthorizer.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/organization/ResourceAuthorizer.scala
@@ -2,11 +2,12 @@ package fi.vm.sade.hakurekisteri.organization
 
 import dispatch.Defaults.executor // TODO: make our own execution context
 import fi.vm.sade.hakurekisteri.rest.support.User
+import fi.vm.sade.utils.slf4j.Logging
 
 import scala.concurrent.Future
 
 class ResourceAuthorizer[A](filterOppijaOidsForHakemusBasedReadAccess: (User, Set[String]) => Future[Set[String]],
-                            authorizationSubjectFinder: AuthorizationSubjectFinder[A])(implicit m: Manifest[A]) {
+                            authorizationSubjectFinder: AuthorizationSubjectFinder[A])(implicit m: Manifest[A]) extends Logging {
   def authorizedResources(resources: Seq[A], user: User, action: String)(organizationAuthorizer: OrganizationAuthorizer): Future[Seq[A]] = {
     subjectFinder(resources).map {
       _.map {
@@ -16,6 +17,11 @@ class ResourceAuthorizer[A](filterOppijaOidsForHakemusBasedReadAccess: (User, Se
       val entriesNotAuthorizedByOrganization = xs.filter(_._3 == false)
       val oppijaOidsForHakemusBasedAccess: Future[Set[String]] = if (entriesNotAuthorizedByOrganization.nonEmpty && action == "READ") {
         val uniqueOppijaOids: Set[String] = entriesNotAuthorizedByOrganization.flatMap(_._2.oppijaOid).toSet
+        if (user.isAdmin) {
+          logger.warn(s"Some data fell back to hakemus based read access check for admin user ${user.username} ! Looks like a bug.")
+          logger.warn(s"About to invoke hakemus based read access check for ${uniqueOppijaOids.size} " +
+            s"oids when checking READ access for user ${user.username} and ${entriesNotAuthorizedByOrganization.map(x => x._1 + " : " + x._2).mkString(", ")}")
+        }
         filterOppijaOidsForHakemusBasedReadAccess(user, uniqueOppijaOids)
       } else {
         Future.successful(Set())
