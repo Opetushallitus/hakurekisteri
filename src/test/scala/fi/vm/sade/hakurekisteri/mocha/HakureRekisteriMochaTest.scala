b1c8b4e66d3dec2c1d2048e70aa8d7a275f3d395
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-03-29 12:42:08 +0200

    BUG-1965 : Fix mocha test timeout

--- src/test/scala/fi/vm/sade/hakurekisteri/mocha/HakureRekisteriMochaTest.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/mocha/HakureRekisteriMochaTest.scala
@@ -1,14 +1,23 @@
 package fi.vm.sade.hakurekisteri.mocha
 
+import java.util.concurrent.TimeUnit.MINUTES
+
 import fi.vm.sade.hakurekisteri.CleanSharedTestJettyBeforeEach
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.concurrent.duration.Duration
 import scala.language.postfixOps
 import scala.sys.process._
 
 class HakureRekisteriMochaTest extends FlatSpec with CleanSharedTestJettyBeforeEach with Matchers {
+  private val totalMochaTestsMaxDuration: Duration = Duration(10, MINUTES)
+
   "Mocha tests" should "pass" in {
-    val pb = Seq("node_modules/mocha-headless-chrome/bin/start", "-f", "http://localhost:" + port + "/test/runner.html")
+    val pb = Seq("node_modules/mocha-headless-chrome/bin/start",
+      "-t",
+      totalMochaTestsMaxDuration.toMillis.toString,
+      "-f",
+      "http://localhost:" + port + "/test/runner.html")
     val res = pb.!
     res should be(0)
   }
