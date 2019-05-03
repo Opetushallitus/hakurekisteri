000b3e49f945465c2eef209ef125a2cb81e0076a
Mikko Siukola <mikko.siukola@cybercom.com>
2019-05-03 12:33:52 +0300

    Get todistusvuosi from koostedata if available.
    If better information not available,
    show an empty field instead of the
    previously-shown 1970.

--- src/main/scala/fi/vm/sade/hakurekisteri/hakija/representation/XMLHakija.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/hakija/representation/XMLHakija.scala
@@ -108,7 +108,12 @@ object XMLHakemus {
 
   def resolveYear(suoritus: VirallinenSuoritus):Option[String] = suoritus match {
     case VirallinenSuoritus("ulkomainen", _,  _, _, _, _, _, _,  _, _, _, _) => None
-    case VirallinenSuoritus(_, _, _,date, _, _, _,_,  _, _, _, _)  => Some(date.getYear.toString)
+    case VirallinenSuoritus(_, _, _,date, _, _, _,_,  _, _, _, _)  =>
+      val year = date.getYear.toString
+      if (!"1970".equals(year))
+        Some(date.getYear.toString)
+      else
+        None
   }
 
   private[hakija] def apply(hakija: Hakija, opiskelutieto: Option[Opiskelija], lahtokoulu: Option[Organisaatio], toiveet: Seq[XMLHakutoive], osaaminen: Option[XMLOsaaminen]): XMLHakemus =
