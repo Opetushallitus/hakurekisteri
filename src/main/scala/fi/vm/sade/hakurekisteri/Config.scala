939c173fa32f9eaf60f25ced0dc9b03f7ead8c66
oph-petar <petar@phz.fi>
2019-09-06 10:31:56 +0300

    Set default archive batch size to 100000

--- src/main/scala/fi/vm/sade/hakurekisteri/Config.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/Config.scala
@@ -108,7 +108,7 @@ class DefaultConfig extends Config {
   override val postgresPassword = properties.getOrElse("suoritusrekisteri.db.password", "postgres")
   override val archiveNonCurrentAfterDays = properties.getOrElse("suoritusrekisteri.db.archiveNonCurrentAfterDays", "180")
   override val archiveTotalTimeoutMinutes = properties.getOrElse("suoritusrekisteri.db.archiveTotalTimeoutMinutes", "180")
-  override val archiveBatchSize = properties.getOrElse("suoritusrekister.db.archiveBatchSize" ,"10000")
+  override val archiveBatchSize = properties.getOrElse("suoritusrekister.db.archiveBatchSize" ,"100000")
   override val slowQuery: Long = java.lang.Long.parseLong(getPropertyOrCrash("suoritusrekisteri.db.slowquery.millis", "configuration key missing: suoritusrekisteri.db.slowquery.millis"))
   override val reallySlowQuery: Long = java.lang.Long.parseLong(getPropertyOrCrash("suoritusrekisteri.db.slowquery.millis", "configuration key missing: suoritusrekisteri.db.reallyslowquery.millis"))
   override val maxDbLogLineLength: Int = java.lang.Integer.parseInt(getPropertyOrCrash("suoritusrekisteri.db.max.log.line.length", "configuration key missing: suoritusrekisteri.db.max.log.line.length"))
