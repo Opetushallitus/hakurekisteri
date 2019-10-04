338f5714c39fddf778292135253f125b58f3693c
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-10-04 13:36:10 +0300

    KSK-4 : Test fix.

--- src/test/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiDataHandlerTest.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiDataHandlerTest.scala
@@ -2592,7 +2592,7 @@ class KoskiDataHandlerTest extends FlatSpec with BeforeAndAfterEach with BeforeA
     Await.result(koskiDatahandler.processHenkilonTiedotKoskesta(henkilo,PersonOidsWithAliases(henkilo.henkilö.oid.toSet), new KoskiSuoritusHakuParams(saveLukio = false, saveAmmatillinen = false)), 5.seconds)
 
     var suoritus: Seq[String] = run(database.run(sql"select lahde_arvot from suoritus where henkilo_oid = '1.2.246.562.24.75034821549' and komo = '1.2.246.562.13.62959769647'".as[String]))
-    suoritus.head should equal("{\"vuosiluokkiin sitomaton opetus\":\"true\"}")
+    suoritus.head should equal("{\"vuosiluokkiin sitomaton opetus\":\"false\"}")
 
     json = scala.io.Source.fromFile(jsonDir + "koskidata_suoritus_update_after.json").mkString
     henkilo = parse(json).extract[KoskiHenkiloContainer]
@@ -2602,7 +2602,7 @@ class KoskiDataHandlerTest extends FlatSpec with BeforeAndAfterEach with BeforeA
     Await.result(koskiDatahandler.processHenkilonTiedotKoskesta(henkilo,PersonOidsWithAliases(henkilo.henkilö.oid.toSet), new KoskiSuoritusHakuParams(saveLukio = false, saveAmmatillinen = false)), 5.seconds)
 
     suoritus = run(database.run(sql"select lahde_arvot from suoritus where henkilo_oid = '1.2.246.562.24.75034821549' and komo = '1.2.246.562.13.62959769647'".as[String]))
-    suoritus.head should equal("{\"vuosiluokkiin sitomaton opetus\":\"false\"}")
+    suoritus.head should equal("{\"vuosiluokkiin sitomaton opetus\":\"true\"}")
   }
 
   def getPerusopetusPäättötodistus(arvosanat: Seq[SuoritusArvosanat]): Option[SuoritusArvosanat] = {
