a7c3d684fecd7e41df2117f0c53fea1fed3ccb19
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-10-04 13:05:15 +0300

    KSK-4 : Fixed sql.

--- src/test/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiDataHandlerTest.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiDataHandlerTest.scala
@@ -2591,7 +2591,7 @@ class KoskiDataHandlerTest extends FlatSpec with BeforeAndAfterEach with BeforeA
 
     Await.result(koskiDatahandler.processHenkilonTiedotKoskesta(henkilo,PersonOidsWithAliases(henkilo.henkilö.oid.toSet), new KoskiSuoritusHakuParams(saveLukio = false, saveAmmatillinen = false)), 5.seconds)
 
-    var suoritus: Seq[String] = run(database.run(sql"select lahde_arvot from suoritus where henkilo_oid = '1.2.246.562.24.75034821549' and where komo = '1.2.246.562.13.62959769647'".as[String]))
+    var suoritus: Seq[String] = run(database.run(sql"select lahde_arvot from suoritus where henkilo_oid = '1.2.246.562.24.75034821549' and komo = '1.2.246.562.13.62959769647'".as[String]))
     suoritus.head should equal("{\"vuosiluokkiin sitomaton opetus\":\"true\"}")
 
     json = scala.io.Source.fromFile(jsonDir + "koskidata_suoritus_update_after.json").mkString
