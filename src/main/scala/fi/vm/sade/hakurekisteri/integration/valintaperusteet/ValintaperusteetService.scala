4c184a48ecb6f4b6121f1a80307cf6753ef6f9ce
Mikko Siukola <mikko.siukola@cybercom.com>
2019-11-27 11:54:19 +0200

    Get valintatapajonon tyyppis in batches in case there are over 5000

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/valintaperusteet/ValintaperusteetService.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/valintaperusteet/ValintaperusteetService.scala
@@ -3,6 +3,7 @@ package fi.vm.sade.hakurekisteri.integration.valintaperusteet
 import akka.actor.ActorSystem
 import akka.event.Logging
 import fi.vm.sade.hakurekisteri.integration.VirkailijaRestClient
+import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.concurrent.Future
 
@@ -27,12 +28,17 @@ trait IValintaperusteetService {
 }
 
 class ValintaperusteetService(restClient: VirkailijaRestClient)(implicit val system: ActorSystem) extends IValintaperusteetService {
+
   private val logger = Logging.getLogger(system, this)
+
+  private val MAX_VALINTATAPAJONOT_BATCH_SIZE = 5000
 
   override def getValintatapajonot(jonoOids: Set[String]): Future[Seq[ValintatapajononTiedot]] = {
     if (jonoOids.nonEmpty) {
-      logger.info("Getting jonotietos from valintaperusteet for jonos: " + jonoOids)
-      restClient.postObject[Set[String], Seq[ValintatapajononTiedot]]("valintaperusteet.valintatapajonosByOids")(200, jonoOids)
+      val batches: Seq[Set[String]] = jonoOids.grouped(MAX_VALINTATAPAJONOT_BATCH_SIZE).toSeq
+      logger.info("Getting jonotietos from valintaperusteet for jonos: " + jonoOids + s" in ${batches.size}")
+      Future.sequence(batches.map(oidBatch => restClient.postObject[Set[String], Seq[ValintatapajononTiedot]]("valintaperusteet.valintatapajonosByOids")(200, oidBatch)))
+        .map(_.flatten.toSeq)
     } else {
       logger.info("Empty list of jonoOids provided for getValintatapajonot.")
       Future.successful(Seq.empty)
