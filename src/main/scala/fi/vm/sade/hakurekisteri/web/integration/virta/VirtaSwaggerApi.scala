ffa2518648e340e042fb20a863d11e815bd7b999
Mikko Siukola <mikko.siukola@cybercom.com>
2020-07-07 13:03:11 +0300

    BUG-2161 Add some of VirtaResource to swagger

---
+++ src/main/scala/fi/vm/sade/hakurekisteri/web/integration/virta/VirtaSwaggerApi.scala
@@ -0,0 +1,39 @@
+package fi.vm.sade.hakurekisteri.web.integration.virta
+
+import fi.vm.sade.hakurekisteri.integration.virta.VirtaStatus
+import fi.vm.sade.hakurekisteri.web.rest.support.{ModelResponseMessage, OldSwaggerSyntax}
+import org.scalatra.swagger.{DataType, Model, SwaggerSupport}
+
+trait VirtaSwaggerApi extends SwaggerSupport with VirtaSwaggerModel {
+
+  registerModel(virtaStatusModel)
+
+  val process = apiOperation[VirtaStatus]("käynnistäVirtaProsessonti")
+    .summary("Käynnistää VIRTA-prosessionnin")
+    .description("Käynnistää virta-prosessionnin, pitkä versio.")
+    .tags("virta")
+
+  val refreshOppija = apiOperation[VirtaStatus]("päivitäOppijanTiedot")
+    .summary("Päivittää yhden oppijan tiedot VIRTA-järjestelmästä.")
+    .description("Päivittää yhden oppijan tiedot VIRTA-järjestelmästä.")
+    .parameter(pathParam[String]("oppijaOid")
+      .description("oppijanumero").required)
+    .tags("virta")
+
+  val statusQuery = apiOperation[VirtaStatus]("status")
+    .summary("Palauttaa VIRTA-integraation tilan.")
+    .description("Palauttaa VIRTA-integraation tilan.")
+    .tags("virta")
+}
+
+trait VirtaSwaggerModel extends OldSwaggerSyntax {
+
+  val virtaStatusFields = Seq(
+    ModelField("lastProcessDone", "Koska viimeisin käsittely on valmistunut", DataType.DateTime, required = false),
+    ModelField("processing", "Onko käsittely käynnissä", DataType.Boolean, required = false),
+    ModelField("queueLength", "Jonon pituus", DataType.Long),
+    ModelField("status", "Tila", DataType.String)
+  )
+
+  def virtaStatusModel: Model = Model("VirtaStatus", "VirtaStatus", virtaStatusFields.map(t => (t.name, t)).toMap)
+}
