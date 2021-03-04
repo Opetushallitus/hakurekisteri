e561213a10f0fb18507d2b8f3ea47e994f31846a
Mikko Siukola <mikko.siukola@cybercom.com>
2021-03-04 14:37:36 +0200

    OY-2879 format

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/hakemus/HakemusService.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/hakemus/HakemusService.scala
@@ -542,17 +542,19 @@ class HakemusService(
           AtaruSearchParams(None, None, None, None, Some(formattedDate))
         )
       } yield hakuappApplications.toList ::: ataruApplications
-      allApplications.flatMap(aa => fetchPersonAliases(aa.filter(h => h.applicationSystemId.length != 35))).onComplete {
-        case Success((hakemukset, personOidsWithAliases)) =>
-          Try(triggerHakemukset(hakemukset, personOidsWithAliases)) match {
-            case Failure(e) => logger.error(e, "Exception in trigger!")
-            case _          =>
-          }
-          processModifiedHakemukset(lastChecked, refreshFrequency)
-        case Failure(t) =>
-          logger.error(t, "Fetching modified hakemukset failed, retrying")
-          processModifiedHakemukset(modifiedAfter, refreshFrequency)
-      }
+      allApplications
+        .flatMap(aa => fetchPersonAliases(aa.filter(h => h.applicationSystemId.length != 35)))
+        .onComplete {
+          case Success((hakemukset, personOidsWithAliases)) =>
+            Try(triggerHakemukset(hakemukset, personOidsWithAliases)) match {
+              case Failure(e) => logger.error(e, "Exception in trigger!")
+              case _          =>
+            }
+            processModifiedHakemukset(lastChecked, refreshFrequency)
+          case Failure(t) =>
+            logger.error(t, "Fetching modified hakemukset failed, retrying")
+            processModifiedHakemukset(modifiedAfter, refreshFrequency)
+        }
     })
   }
 
