fd5da1cef6660a3db43b553b167f4df06a24afce
Jussi Jartamo <jussi.jartamo@gofore.com>
2021-04-20 02:52:31 +0300

    aikaleima optional

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/pistesyotto/PistesyottoService.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/pistesyotto/PistesyottoService.scala
@@ -3,7 +3,7 @@ package fi.vm.sade.hakurekisteri.integration.pistesyotto
 import fi.vm.sade.hakurekisteri.integration.{VirkailijaRestClient}
 import scala.concurrent.{Future}
 
-case class Pistetieto(aikaleima: String, tunniste: String, arvo: Any, osallistuminen: String)
+case class Pistetieto(aikaleima: Option[String], tunniste: String, arvo: Any, osallistuminen: String)
 case class PistetietoWrapper(hakemusOID: String, pisteet: Seq[Pistetieto])
 
 class PistesyottoService(restClient: VirkailijaRestClient) {
