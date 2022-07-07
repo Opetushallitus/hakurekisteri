6e6e09702cb6fbcef58b887a499c639ca926190c
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-07-07 13:16:20 +0300

    OY-3714__batch-one-kouta-hakukohde-fetch-to-many

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/kouta/KoutaInternalActor.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/kouta/KoutaInternalActor.scala
@@ -8,24 +8,11 @@ import akka.util.Timeout
 import com.github.blemale.scaffeine.Scaffeine
 import com.google.common.base.{Supplier, Suppliers}
 import fi.vm.sade.hakurekisteri.Config
-import fi.vm.sade.hakurekisteri.integration.haku.{
-  GetHautQuery,
-  RestHaku,
-  RestHakuAika,
-  RestHakuResult
-}
-import fi.vm.sade.hakurekisteri.integration.hakukohde.{
-  Hakukohde,
-  HakukohdeQuery,
-  HakukohteenKoulutuksetQuery
-}
+import fi.vm.sade.hakurekisteri.batch.support.BatchOneApiCallAsMany
+import fi.vm.sade.hakurekisteri.integration.haku.{GetHautQuery, RestHaku, RestHakuAika, RestHakuResult}
+import fi.vm.sade.hakurekisteri.integration.hakukohde.{Hakukohde, HakukohdeQuery, HakukohteenKoulutuksetQuery}
 import fi.vm.sade.hakurekisteri.integration.koodisto.{GetKoodi, Koodi, KoodistoActorRef}
-import fi.vm.sade.hakurekisteri.integration.tarjonta.{
-  GetHautQueryFailedException,
-  HakukohteenKoulutukset,
-  Hakukohteenkoulutus,
-  TarjontaKoodi
-}
+import fi.vm.sade.hakurekisteri.integration.tarjonta.{GetHautQueryFailedException, HakukohteenKoulutukset, Hakukohteenkoulutus, TarjontaKoodi}
 import fi.vm.sade.hakurekisteri.integration.{ExecutorUtil, OphUrlProperties, VirkailijaRestClient}
 import org.joda.time.{LocalDate, LocalDateTime}
 import support.TypedAskableActorRef
@@ -52,6 +39,12 @@ class KoutaInternalActor(
   private val hakukohdeCache = Scaffeine()
     .expireAfterWrite(30.minutes)
     .buildAsyncFuture[String, KoutaInternalHakukohde](getHakukohdeFromKoutaInternalForReal)
+
+  private val koutaInternalHakukohdeBatcher = new BatchOneApiCallAsMany[KoutaInternalHakukohde](
+    poolSize = 2,
+    poolName = "KoutaInternalHakukohdeBatcher",
+    getHakukohdeFromKoutaInternalBatched,
+    oidsAtMostPerSingleApiCall = 500)
 
   val hakuCache = Suppliers.memoizeWithExpiration(
     () =>
@@ -210,8 +203,14 @@ class KoutaInternalActor(
   private def getHakukohdeFromKoutaInternal(hakukohdeOid: String): Future[KoutaInternalHakukohde] =
     hakukohdeCache.get(hakukohdeOid)
 
-  private def getHakukohdeFromKoutaInternalForReal(hakukohdeOid: String) = restClient
-    .readObject[KoutaInternalHakukohde]("kouta-internal.hakukohde", hakukohdeOid)(200, 3)
+  private def getHakukohdeFromKoutaInternalBatched(hakukohdeOids: Set[String]): Future[List[KoutaInternalHakukohde]] = {
+    restClient
+      .postObjectWithCodes[Set[String], List[KoutaInternalHakukohde]](
+        "kouta-internal.hakukohde.batch", Seq(200), 2, hakukohdeOids, basicAuth = false)
+  }
+
+  private def getHakukohdeFromKoutaInternalForReal(hakukohdeOid: String): Future[KoutaInternalHakukohde] =
+    koutaInternalHakukohdeBatcher.batch(hakukohdeOid, hk => hakukohdeOid.equals(hk.oid))
 
   private def getToteutusFromKoutaInternal(toteutusOid: String): Future[KoutaInternalToteutus] =
     restClient
