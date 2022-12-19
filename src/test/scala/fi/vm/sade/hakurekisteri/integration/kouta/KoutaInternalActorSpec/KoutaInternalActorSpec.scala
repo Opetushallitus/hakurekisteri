4016d51108adc1e5e7df01578d771d5c89c6e7dc
Tomi Katajanheimo <tomi@katajanheimo.com>
2022-12-19 13:38:53 +0200

    Fix spotless issues

--- src/test/scala/fi/vm/sade/hakurekisteri/integration/kouta/KoutaInternalActorSpec/KoutaInternalActorSpec.scala
+++ src/test/scala/fi/vm/sade/hakurekisteri/integration/kouta/KoutaInternalActorSpec/KoutaInternalActorSpec.scala
@@ -7,10 +7,21 @@ import fi.vm.sade.hakurekisteri.acceptance.tools.HakeneetSupport
 import fi.vm.sade.hakurekisteri.integration.hakukohde.HakukohteenKoulutuksetQuery
 import fi.vm.sade.hakurekisteri.integration.koodisto.KoodistoActorRef
 import fi.vm.sade.hakurekisteri.integration.kouta.KoutaInternalActor
-import fi.vm.sade.hakurekisteri.integration.tarjonta.{HakukohteenKoulutukset, Hakukohteenkoulutus, TarjontaKoodi}
-import fi.vm.sade.hakurekisteri.integration.{CapturingAsyncHttpClient, DispatchSupport, Endpoint, LocalhostProperties, ServiceConfig, VirkailijaRestClient}
+import fi.vm.sade.hakurekisteri.integration.tarjonta.{
+  HakukohteenKoulutukset,
+  Hakukohteenkoulutus,
+  TarjontaKoodi
+}
+import fi.vm.sade.hakurekisteri.integration.{
+  CapturingAsyncHttpClient,
+  DispatchSupport,
+  Endpoint,
+  LocalhostProperties,
+  ServiceConfig,
+  VirkailijaRestClient
+}
 import fi.vm.sade.utils.slf4j.Logging
-import org.mockito.Mockito
+import org.mockito.Mockito.when
 import org.scalatest.concurrent.Waiters
 import org.scalatest.mockito.MockitoSugar
 import org.scalatra.test.scalatest.ScalatraFunSuite
@@ -18,31 +29,46 @@ import org.scalatra.test.scalatest.ScalatraFunSuite
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
-class KoutaInternalActorSpec extends ScalatraFunSuite
-  with HakeneetSupport
-  with MockitoSugar
-  with DispatchSupport
-  with Waiters
-  with LocalhostProperties
-  with Logging {
+class KoutaInternalActorSpec
+    extends ScalatraFunSuite
+    with HakeneetSupport
+    with MockitoSugar
+    with DispatchSupport
+    with Waiters
+    with LocalhostProperties
+    with Logging {
   private val endPoint = mock[Endpoint]
   private val koutaInternalClient = new VirkailijaRestClient(
     ServiceConfig(serviceUrl = "http://localhost/oppijanumerorekisteri-service"),
     aClient = Some(new CapturingAsyncHttpClient(endPoint))
   )
-  private val koodistoMock = KoodistoActorRef(system.actorOf(Props(
-    new MockedKoodistoActor()
-  )))
-  private val koutaInternalActor = system.actorOf(Props(
-    new KoutaInternalActor(koodistoMock, koutaInternalClient, new MockConfig())
-  ))
+  private val koodistoMock = KoodistoActorRef(
+    system.actorOf(
+      Props(
+        new MockedKoodistoActor()
+      )
+    )
+  )
+  private val koutaInternalActor = system.actorOf(
+    Props(
+      new KoutaInternalActor(koodistoMock, koutaInternalClient, new MockConfig())
+    )
+  )
 
   test("koutaInternalActor handles koulutukseen johtamaton koulutus") {
-    Mockito.when(endPoint.request(forPattern(".*/kouta-internal/hakukohde/findbyoids")))
+    when(endPoint.request(forPattern(".*/kouta-internal/hakukohde/findbyoids")))
       .thenReturn((200, List(), getJson("koutaHakukohde")))
-    Mockito.when(endPoint.request(forPattern(".*/kouta-internal/toteutus/1.2.246.562.17.00000000000000011399")))
+    when(
+      endPoint.request(
+        forPattern(".*/kouta-internal/toteutus/1.2.246.562.17.00000000000000011399")
+      )
+    )
       .thenReturn((200, List(), getJson("koutaToteutus")))
-    Mockito.when(endPoint.request(forPattern(".*/kouta-internal/koulutus/1.2.246.562.13.00000000000000003172")))
+    when(
+      endPoint.request(
+        forPattern(".*/kouta-internal/koulutus/1.2.246.562.13.00000000000000003172")
+      )
+    )
       .thenReturn((200, List(), getJson("koutaKoulutus")))
     val hakukohdeOid = "1.2.246.562.20.00000000000000027357"
     val hakukohteenKoulutukset: HakukohteenKoulutukset = Await.result(
