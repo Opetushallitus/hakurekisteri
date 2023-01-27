9a4e683298a2e33b88c8c52c51bbd4fa6186b486
Mikko Siukola <mikko.siukola@cybercom.com>
2023-01-27 12:34:33 +0200

    OY-3918 WIP Get keskiarvo from valintalaskenta

---
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/valintalaskentatulos/ValintalaskentaTulosService.scala
@@ -0,0 +1,32 @@
+package fi.vm.sade.hakurekisteri.integration.valintalaskentatulos
+
+import akka.actor.ActorSystem
+import fi.vm.sade.hakurekisteri.integration.VirkailijaRestClient
+
+import scala.concurrent.Future
+
+trait IValintalaskentaTulosService {
+  def getLaskennanTulosForHakemusOids(hakuOid: String, hakemusOids: Seq[String]): Future[Seq[LaskennanTulosHakemukselle]]
+}
+
+case class LaskennanTulosHakemukselle(hakemusOid: String)
+
+class ValintalaskentaTulosService(restClient: VirkailijaRestClient)(implicit val system: ActorSystem
+) extends IValintalaskentaTulosService {
+
+  override def getLaskennanTulosForHakemusOids(hakuOid: String, hakemusOids: Seq[String]): Future[Seq[LaskennanTulosHakemukselle]] = {
+    if (hakemusOids.isEmpty) {
+      Future.successful(Seq.empty)
+    } else {
+      restClient.postObject[Seq[String], Seq[LaskennanTulosHakemukselle]](
+        "valintalaskentatulos.laskennantulos.hakemuksille",
+        hakuOid
+      )(200, hakemusOids)
+    }
+  }
+}
+
+class ValintalaskentaTulosServiceMock extends IValintalaskentaTulosService {
+  override def getLaskennanTulosForHakemusOids(hakuOid: String, hakemusOids: Seq[String]): Future[Seq[LaskennanTulosHakemukselle]] =
+    Future.successful(Seq.empty)
+}
