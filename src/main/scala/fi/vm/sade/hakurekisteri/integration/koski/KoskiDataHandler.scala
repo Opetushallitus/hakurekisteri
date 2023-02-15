f50862c6b2431473804d245a9d2bcc716db1c499
August Kilponen <august.kilponen@gmail.com>
2023-02-15 12:15:08 +0200

    Lisätty debug-lokitusta

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiDataHandler.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiDataHandler.scala
@@ -584,11 +584,6 @@ class KoskiDataHandler(
         )
       }
 
-      if (henkilöOid.equals("1.2.246.562.24.38994425611")) {
-        logger.info(s"Henkilö: $henkilöOid | tallennettavatSuoritukset: $tallennettavatSuoritukset")
-        logger.info(s"Henkilö: $henkilöOid | fetchedSuoritukset: $fetchedSuoritukset")
-      }
-
       // Tarkistetaan onko henkilön Koskessa tulevissa suorituksissa tai jo kannassa olevissa
       // suorituksissa valmis ja vahvistettu perusopetuksen suoritus.
       //
@@ -614,6 +609,12 @@ class KoskiDataHandler(
         tallennettavatSuoritukset = tallennettavatSuoritukset.filterNot(s =>
           Oids.perusopetuksenOppiaineenOppimaaraOid.contains(s.suoritus.komo)
         )
+      }
+
+      if (henkilöOid.equals("1.2.246.562.24.38994425611")) {
+        logger.info(s"Henkilö: $henkilöOid | tallennettavatSuoritukset: $tallennettavatSuoritukset")
+        logger.info(s"Henkilö: $henkilöOid | fetchedSuoritukset: $fetchedSuoritukset")
+        logger.info(s"Henkilö: $henkilöOid | hasValmisPerusopetuksenSuoritus: $hasValmisPerusopetuksenSuoritus")
       }
 
       checkAndDeleteIfSuoritusDoesNotExistAnymoreInKoski(
