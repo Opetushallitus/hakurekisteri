68fc4795b781dee6d8d7883d35ac169623315468
Väinö Ala-Härkönen <vae@wonna.fi>
2024-02-01 17:07:39 +0200

    Remove redundant comment

--- src/main/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiDataHandler.scala
+++ src/main/scala/fi/vm/sade/hakurekisteri/integration/koski/KoskiDataHandler.scala
@@ -142,7 +142,6 @@ class KoskiDataHandler(
         )
         return false
       }
-      // Valmiit TUVA-suoritukset, l. väh. 19 opintoviikkoa halutaan säilyttää aina, keskeneräiset tältä lukuvuodelta.
       if (!suoritus.laajuusVahintaan(19) && KoskiUtil.isBeforeTuvaStartDate(lasnaDate.get)) {
         logger.info(
           s"Filtteröitiin henkilöltä $henkiloOid keskeneräinen tuva-suoritus, joka on alkanut ennen viime vuoden elokuun ensimmäistä päivää."
