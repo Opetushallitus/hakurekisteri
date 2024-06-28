397d300748a35a5b9b7d952dd834eab985c0654d
Mikko Siukola <mikko.siukola@cybercom.com>
2024-06-28 14:17:28 +0300

    OK-567 Fix table name

--- suoritusrekisteri/src/main/resources/db/migration/V23__add_siirtotiedosto_table.sql
+++ suoritusrekisteri/src/main/resources/db/migration/V23__add_siirtotiedosto_table.sql
@@ -11,15 +11,15 @@ create table if not exists siirtotiedosto (
                                                PRIMARY KEY (id)
 );
 
-COMMENT ON column siirtotiedostot.window_start IS 'Siirtotiedosto-operaation aikaikkunan alkuhetki (unixtime)';
-COMMENT ON column siirtotiedostot.window_end IS 'Siirtotiedosto-operaation aikaikkunan loppuhetki (unixtime)';
-COMMENT ON column siirtotiedostot.run_start IS 'Siirtotiedosto-operaation suorituksen alkuaika';
-COMMENT ON column siirtotiedostot.run_end IS 'Siirtotiedosto-operaation suorituksen loppuaika';
-COMMENT ON column siirtotiedostot.info IS 'Tietoja tallennetuista entiteeteistä, mm. lukumäärät';
-COMMENT ON column siirtotiedostot.error_message IS 'null, jos mikään ei mennyt vikaan';
+COMMENT ON column siirtotiedosto.window_start IS 'Siirtotiedosto-operaation aikaikkunan alkuhetki (unixtime)';
+COMMENT ON column siirtotiedosto.window_end IS 'Siirtotiedosto-operaation aikaikkunan loppuhetki (unixtime)';
+COMMENT ON column siirtotiedosto.run_start IS 'Siirtotiedosto-operaation suorituksen alkuaika';
+COMMENT ON column siirtotiedosto.run_end IS 'Siirtotiedosto-operaation suorituksen loppuaika';
+COMMENT ON column siirtotiedosto.info IS 'Tietoja tallennetuista entiteeteistä, mm. lukumäärät';
+COMMENT ON column siirtotiedosto.error_message IS 'null, jos mikään ei mennyt vikaan';
 
 alter table siirtotiedosto owner to oph;
 
 --todo, what are the initial values? This is just something to get the poc started.
-insert into siirtotiedostot(id, uuid, window_start, window_end, run_start, run_end, info, success, error_message)
+insert into siirtotiedosto(id, uuid, window_start, window_end, run_start, run_end, info, success, error_message)
 values (1, '57be2612-ba79-429e-a93e-c38346f1d62d',  0, 1719401507582, now(), now(), '{"entityTotals": {}}'::jsonb, true, null) on conflict do nothing;
