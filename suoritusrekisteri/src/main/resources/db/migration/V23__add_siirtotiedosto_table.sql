8f31d1c5c6be835c4f53091f599e4126575f2255
Mikko Siukola <mikko.siukola@cybercom.com>
2024-07-01 14:32:10 +0300

    OK-567 Update migration, add sequence for siirtotiedosto ids

--- suoritusrekisteri/src/main/resources/db/migration/V23__add_siirtotiedosto_table.sql
+++ suoritusrekisteri/src/main/resources/db/migration/V23__add_siirtotiedosto_table.sql
@@ -1,4 +1,4 @@
-create table if not exists siirtotiedosto (
+CREATE TABLE IF NOT EXISTS siirtotiedosto (
                                                id serial,
                                                uuid varchar,
                                                window_start bigint not null,
@@ -11,15 +11,15 @@ create table if not exists siirtotiedosto (
                                                PRIMARY KEY (id)
 );
 
-COMMENT ON column siirtotiedosto.window_start IS 'Siirtotiedosto-operaation aikaikkunan alkuhetki (unixtime)';
-COMMENT ON column siirtotiedosto.window_end IS 'Siirtotiedosto-operaation aikaikkunan loppuhetki (unixtime)';
-COMMENT ON column siirtotiedosto.run_start IS 'Siirtotiedosto-operaation suorituksen alkuaika';
-COMMENT ON column siirtotiedosto.run_end IS 'Siirtotiedosto-operaation suorituksen loppuaika';
-COMMENT ON column siirtotiedosto.info IS 'Tietoja tallennetuista entiteeteistä, mm. lukumäärät';
-COMMENT ON column siirtotiedosto.error_message IS 'null, jos mikään ei mennyt vikaan';
+CREATE SEQUENCE IF NOT EXISTS siirtotiedosto_id_seq START 0;
 
-alter table siirtotiedosto owner to oph;
+COMMENT ON COLUMN siirtotiedosto.window_start IS 'Siirtotiedosto-operaation aikaikkunan alkuhetki (unixtime)';
+COMMENT ON COLUMN siirtotiedosto.window_end IS 'Siirtotiedosto-operaation aikaikkunan loppuhetki (unixtime)';
+COMMENT ON COLUMN siirtotiedosto.run_start IS 'Siirtotiedosto-operaation suorituksen alkuaika';
+COMMENT ON COLUMN siirtotiedosto.run_end IS 'Siirtotiedosto-operaation suorituksen loppuaika';
+COMMENT ON COLUMN siirtotiedosto.info IS 'Tietoja tallennetuista entiteeteistä, mm. lukumäärät';
+COMMENT ON COLUMN siirtotiedosto.error_message IS 'null, jos mikään ei mennyt vikaan';
 
 --todo, what are the initial values? This is just something to get the poc started.
-insert into siirtotiedosto(id, uuid, window_start, window_end, run_start, run_end, info, success, error_message)
-values (1, '57be2612-ba79-429e-a93e-c38346f1d62d',  0, 1719401507582, now(), now(), '{"entityTotals": {}}'::jsonb, true, null) on conflict do nothing;
+INSERT INTO siirtotiedosto(id, uuid, window_start, window_end, run_start, run_end, info, success, error_message)
+VALUES (nextval('siirtotiedosto_id_seq'), '57be2612-ba79-429e-a93e-c38346f1d62d',  0, 1719401507582, now(), now(), '{"entityTotals": {}}'::jsonb, true, null) ON CONFLICT DO NOTHING;
